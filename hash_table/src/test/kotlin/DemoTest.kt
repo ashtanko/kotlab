48b96d3521ff56f2f5a28c99748d711b74bca46f
Alexey Shtanko <adamluissean@gmail.com>
2019-08-23 16:02:54 +0300

    Add dummy unit test for check new Actions CI

---
+++ hash_table/src/test/kotlin/DemoTest.kt
@@ -0,0 +1,12 @@
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class DemoTest {
+    @Test
+    fun `dummy FAILED`() {
+        Assert.assertTrue(false)
+    }
+}
