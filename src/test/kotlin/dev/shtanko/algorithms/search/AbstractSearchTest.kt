7998509e34290be869c2e1e92fda706e0abafbe5
Alexey Shtanko <shtankopro@gmail.com>
2020-05-30 12:09:26 +0300

    Add test cases

--- src/test/kotlin/dev/shtanko/algorithms/search/AbstractSearchTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/search/AbstractSearchTest.kt
@@ -9,4 +9,17 @@ abstract class AbstractSearchTest<out T : AbstractSearchStrategy<Int>>(private v
     fun `empty test`() {
         assertEquals(-1, strategy.perform(emptyArray(), 1))
     }
+
+    @Test
+    fun `single element test`() {
+        assertEquals(0, strategy.perform(arrayOf(1), 1))
+        assertEquals(-1, strategy.perform(arrayOf(1), 2))
+    }
+
+    @Test
+    fun `two elements test`() {
+        assertEquals(0, strategy.perform(arrayOf(4, 8), 4))
+        assertEquals(1, strategy.perform(arrayOf(4, 8), 8))
+        assertEquals(-1, strategy.perform(arrayOf(4, 8), 15))
+    }
 }
