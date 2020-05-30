0dc9c7a9d0ca6b94b976fe316a42722ecf0ac8cf
Alexey Shtanko <shtankopro@gmail.com>
2020-05-30 12:19:20 +0300

    Add javadoc

--- src/main/kotlin/dev/shtanko/algorithms/search/LinearSearch.kt
+++ src/main/kotlin/dev/shtanko/algorithms/search/LinearSearch.kt
@@ -1,5 +1,10 @@
 package dev.shtanko.algorithms.search
 
+/**
+ * The time complexity of above algorithm is O(n).
+ * Linear search is rarely used practically because other search algorithms such as the binary search algorithm and
+ * hash tables allow significantly faster searching comparison to Linear search.
+ */
 class LinearSearch<T> : AbstractSearchStrategy<T> {
 
     override fun perform(arr: Array<T>, element: T): Int {
