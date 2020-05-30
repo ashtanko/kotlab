c828b9e4c458078d590aa16643d6e0d5bf592a33
Alexey Shtanko <shtankopro@gmail.com>
2020-05-30 12:15:34 +0300

    Add test cases

--- src/test/kotlin/dev/shtanko/algorithms/search/AbstractSearchTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/search/AbstractSearchTest.kt
@@ -12,14 +12,28 @@ abstract class AbstractSearchTest<out T : AbstractSearchStrategy<Int>>(private v
 
     @Test
     fun `single element test`() {
-        assertEquals(0, strategy.perform(arrayOf(1), 1))
-        assertEquals(-1, strategy.perform(arrayOf(1), 2))
+        val arr = arrayOf(1)
+        assertEquals(0, strategy.perform(arr, 1))
+        assertEquals(-1, strategy.perform(arr, 2))
     }
 
     @Test
     fun `two elements test`() {
-        assertEquals(0, strategy.perform(arrayOf(4, 8), 4))
-        assertEquals(1, strategy.perform(arrayOf(4, 8), 8))
-        assertEquals(-1, strategy.perform(arrayOf(4, 8), 15))
+        val arr = arrayOf(4, 8)
+        assertEquals(0, strategy.perform(arr, 4))
+        assertEquals(1, strategy.perform(arr, 8))
+        assertEquals(-1, strategy.perform(arr, 15))
+    }
+
+    @Test
+    fun `six elements test`() {
+        val arr = arrayOf(4, 8, 15, 16, 23, 42)
+        assertEquals(0, strategy.perform(arr, 4))
+        assertEquals(1, strategy.perform(arr, 8))
+        assertEquals(2, strategy.perform(arr, 15))
+        assertEquals(3, strategy.perform(arr, 16))
+        assertEquals(4, strategy.perform(arr, 23))
+        assertEquals(5, strategy.perform(arr, 42))
+        assertEquals(-1, strategy.perform(arr, 56))
     }
 }
