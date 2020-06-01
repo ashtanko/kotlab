a68e1dcffcb3a6f10b9cdc6d2c1cce6ffaeb801e
Alexey Shtanko <shtankopro@gmail.com>
2020-06-01 18:12:17 +0300

    Add javadoc

--- src/main/kotlin/dev/shtanko/datastructures/Stack.kt
+++ src/main/kotlin/dev/shtanko/datastructures/Stack.kt
@@ -1,5 +1,15 @@
 package dev.shtanko.datastructures
 
+/**
+ * A stack is a basic data structure that can be logically thought of as a linear structure represented by a real
+ * physical stack or pile, a structure where insertion and deletion of items takes place at one end called top of
+ * the stack. The basic concept can be illustrated by thinking of your data set as a stack of plates or books where
+ * you can only take the top item off the stack in order to remove things from it. This structure is used all
+ * throughout programming. The basic implementation of a stack is also called a LIFO (Last In First Out) to demonstrate
+ * the way it accesses data, since as we will see there are various variations of stack implementations.
+ * There are basically three operations that can be performed on stacks. They are 1) inserting an item into
+ * a stack (push). 2) deleting an item from the stack (pop). 3) displaying the contents of the stack (peek or top).
+ */
 class Stack<T> : Collection<T> {
     private var head: Node<T>? = null
 
