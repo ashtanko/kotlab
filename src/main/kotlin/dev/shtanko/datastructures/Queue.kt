a08f0b00b8bb6a671bf9295ab4c0f1822ad48e1b
Alexey Shtanko <shtankopro@gmail.com>
2020-06-01 15:33:27 +0300

    Add javadoc

--- src/main/kotlin/dev/shtanko/datastructures/Queue.kt
+++ src/main/kotlin/dev/shtanko/datastructures/Queue.kt
@@ -1,5 +1,11 @@
 package dev.shtanko.datastructures
 
+/**
+ * @link https://www.geeksforgeeks.org/queue-data-structure/
+ * A Queue is a linear structure which follows a particular order in which the operations are performed.
+ * The order is First In First Out (FIFO). A good example of a queue is any queue of consumers for
+ * a resource where the consumer that came first is served first.
+ */
 class Queue<T> : Collection<T> {
 
     private var head: Node<T>? = null
