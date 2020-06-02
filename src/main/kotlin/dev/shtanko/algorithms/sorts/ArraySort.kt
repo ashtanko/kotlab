61de19e2826c23fdd1c5a5d61739c793490d98bb
Alexey Shtanko <shtankopro@gmail.com>
2020-06-02 13:23:28 +0300

    Small refactor

--- src/main/kotlin/dev/shtanko/algorithms/sorts/ArraySort.kt
+++ src/main/kotlin/dev/shtanko/algorithms/sorts/ArraySort.kt
@@ -1,9 +1,7 @@
 package dev.shtanko.algorithms.sorts
 
-import java.util.*
-
 class ArraySort : AbstractSortStrategy {
     override fun <T : Comparable<T>> perform(arr: Array<T>) {
-        Arrays.sort(arr)
+        arr.sort()
     }
 }
