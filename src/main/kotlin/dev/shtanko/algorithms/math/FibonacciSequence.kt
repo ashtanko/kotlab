522daa086104f5cf33c676e8e34c784f000966f0
Alexey Shtanko <shtankopro@gmail.com>
2020-06-02 13:43:21 +0300

    Fix multiline if-else statements without braces

--- src/main/kotlin/dev/shtanko/algorithms/math/FibonacciSequence.kt
+++ src/main/kotlin/dev/shtanko/algorithms/math/FibonacciSequence.kt
@@ -1,7 +1,8 @@
 package dev.shtanko.algorithms.math
 
 fun Int.toFibonacciSequence(): Int {
-    if (this <= 1)
+    if (this <= 1) {
         return this
+    }
     return (this - 1).toFibonacciSequence() + (this - 2).toFibonacciSequence()
 }
