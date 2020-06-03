f1a79baa13f8df402312149445a8981630b375af
Alexey Shtanko <shtankopro@gmail.com>
2020-06-03 10:31:12 +0300

    Add search in range example

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ElementPosition.kt
@@ -0,0 +1,45 @@
+package dev.shtanko.algorithms.leetcode
+
+fun IntArray.searchRange(target: Int): IntArray {
+    var high = this.size - 1
+    var low = 0
+    val rs = intArrayOf(-1, -1)
+
+    // base case
+    if (this.isEmpty()) {
+        return rs
+    }
+
+    // left side
+    while (low < high) {
+        val mid = low + (high - low) / 2
+        if (target > this[mid]) {
+            low = mid + 1
+        } else {
+            high = mid
+        }
+    }
+    if (target == this[low]) {
+        rs[0] = low
+    } else {
+        rs[0] = -1
+    }
+
+    // right side
+    high = this.size - 1
+    while (low < high) {
+        val mid = (low + (high - low) / 2) + 1
+        if (target < this[mid]) {
+            high = mid - 1
+        } else {
+            low = mid
+        }
+    }
+    if (target == this[high]) {
+        rs[1] = high
+    } else {
+        rs[1] = -1
+    }
+
+    return rs
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/ElementPositionTest.kt
@@ -0,0 +1,32 @@
+package dev.shtanko.algorithms.leetcode
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
+class ElementPositionTest {
+
+    @Test
+    fun `empty test`() {
+        val arr = intArrayOf()
+        val target = 4
+        val actual = arr.searchRange(target)
+        assertArrayEquals(intArrayOf(-1, -1), actual)
+    }
+
+    @Test
+    fun `simple test`() {
+        val arr = intArrayOf(5, 7, 7, 8, 8, 10)
+        val target = 8
+        val actual = arr.searchRange(target)
+        assertArrayEquals(intArrayOf(3, 4), actual)
+    }
+
+    @Test
+    fun `simple test 2`() {
+        val arr = intArrayOf(5, 7, 7, 8, 8, 10)
+        val target = 6
+        val actual = arr.searchRange(target)
+        assertArrayEquals(intArrayOf(-1, -1), actual)
+    }
+
+}
