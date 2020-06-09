b1e4f13f6a12877ca76100291e7de11d28f1670a
Alexey Shtanko <shtankopro@gmail.com>
2020-06-09 14:39:17 +0300

    Add javadoc

--- src/main/kotlin/dev/shtanko/algorithms/sorts/PancakeSort.kt
+++ src/main/kotlin/dev/shtanko/algorithms/sorts/PancakeSort.kt
@@ -2,6 +2,11 @@ package dev.shtanko.algorithms.sorts
 
 import dev.shtanko.algorithms.extensions.flip
 
+/**
+ * Pancake sorting is the colloquial term for the mathematical problem of sorting a disordered stack of pancakes in
+ * order of size when a spatula can be inserted at any point in the stack and used to flip all pancakes above it.
+ * A pancake number is the minimum number of flips required for a given number of pancakes.
+ */
 class PancakeSort : AbstractSortStrategy {
     override fun <T : Comparable<T>> perform(arr: Array<T>) {
         for (i in arr.indices) {
