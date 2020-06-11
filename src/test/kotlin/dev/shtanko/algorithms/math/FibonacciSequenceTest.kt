29d0bee7ec4b2774130b33733631f533fd3f7f23
Alexey Shtanko <shtankopro@gmail.com>
2020-06-11 16:22:31 +0300

    Add math examples

--- src/test/kotlin/dev/shtanko/algorithms/math/FibonacciSequenceTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/math/FibonacciSequenceTest.kt
@@ -19,4 +19,24 @@ class FibonacciSequenceTest {
         assertEquals(34, 9.toFibonacciSequence())
         assertEquals(55, 10.toFibonacciSequence())
     }
+
+    @Test
+    fun `iterative test`() {
+        assertEquals(0, Fibonacci.ITERATIVE.invoke(0))
+        assertEquals(1, Fibonacci.ITERATIVE.invoke(1))
+        assertEquals(1, Fibonacci.ITERATIVE.invoke(2))
+        assertEquals(34, Fibonacci.ITERATIVE.invoke(9))
+        assertEquals(34, Fibonacci.ITERATIVE.invoke(9))
+        assertEquals(55, Fibonacci.ITERATIVE.invoke(10))
+    }
+
+    @Test
+    fun `recursive test`() {
+        assertEquals(0, Fibonacci.RECURSIVE.invoke(0))
+        assertEquals(1, Fibonacci.RECURSIVE.invoke(1))
+        assertEquals(1, Fibonacci.RECURSIVE.invoke(2))
+        assertEquals(34, Fibonacci.RECURSIVE.invoke(9))
+        assertEquals(34, Fibonacci.RECURSIVE.invoke(9))
+        assertEquals(55, Fibonacci.RECURSIVE.invoke(10))
+    }
 }
