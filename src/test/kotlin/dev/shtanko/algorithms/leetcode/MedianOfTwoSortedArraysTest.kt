2dfa6e889f1b70d38e85714e73d0dc78ca1f89a0
Alexey Shtanko <shtankopro@gmail.com>
2020-06-15 19:28:59 +0300

    Add test cases

--- src/test/kotlin/dev/shtanko/algorithms/leetcode/MedianOfTwoSortedArraysTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MedianOfTwoSortedArraysTest.kt
@@ -1,5 +1,6 @@
 package dev.shtanko.algorithms.leetcode
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MedianOfTwoSortedArraysTest {
@@ -8,6 +9,13 @@ class MedianOfTwoSortedArraysTest {
     fun `simple test`() {
         val pair = intArrayOf(1, 3) to intArrayOf(2)
         val median = pair.findMedianSortedArrays()
-        println(median)
+        assertEquals(2.0, median)
+    }
+
+    @Test
+    fun `simple test 2`() {
+        val pair = intArrayOf(1, 2) to intArrayOf(3, 4)
+        val median = pair.findMedianSortedArrays()
+        assertEquals(2.5, median)
     }
 }
