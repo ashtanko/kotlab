6ac84c0df57d41aca6238be1fff3ad77ad5fb970
Alexey Shtanko <shtankopro@gmail.com>
2020-06-30 09:51:39 +0300

    Add extensions

--- src/main/kotlin/dev/shtanko/algorithms/extensions/Array.kt
+++ src/main/kotlin/dev/shtanko/algorithms/extensions/Array.kt
@@ -18,10 +18,26 @@ fun <T> Array<T>.reverse() {
         this.swap(i, n - 1 - i)
 }
 
+fun <T> Array<T>.reverse2() {
+    var i = 0
+    var j = size - 1
+    while (i < j) {
+        swap(i, j)
+        i++
+        j--
+    }
+}
+
 fun <T> Array<T>.flip(left: Int, right: Int) {
     var l = left
     var r = right
     while (l <= r) {
         swap(l++, r--)
     }
+}
+
+fun CharArray.swap(i: Int, j: Int) {
+    val tmp = this[i]
+    this[i] = this[j]
+    this[j] = tmp
 }
--- src/test/kotlin/dev/shtanko/algorithms/extensions/ArrayExtensionTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/extensions/ArrayExtensionTest.kt
@@ -29,10 +29,31 @@ class ArrayExtensionTest {
     }
 
     @Test
+    fun `two pointers technique reverse test`() {
+        val arr = arrayOf(4, 8, 15, 16, 23, 42)
+        arr.reverse2()
+        assertArrayEquals(arrayOf(42, 23, 16, 15, 8, 4), arr)
+    }
+
+    @Test
     fun `flip test`() {
         val arr = arrayOf(4, 8, 15, 16, 23, 42)
         arr.flip(0, arr.size - 1)
         val expected = arrayOf(42, 23, 16, 15, 8, 4)
         assertArrayEquals(expected, arr)
+    }
+
+    @Test
+    fun `char swap test`() {
+        val arr = charArrayOf('A', 'B')
+        arr.swap(1, 0)
+        assertArrayEquals(charArrayOf('B', 'A'), arr)
+    }
+
+    @Test
+    fun `int array swap test`() {
+        val arr = intArrayOf(4, 8)
+        arr.swap(1, 0)
+        assertArrayEquals(intArrayOf(8, 4), arr)
     }
 }
