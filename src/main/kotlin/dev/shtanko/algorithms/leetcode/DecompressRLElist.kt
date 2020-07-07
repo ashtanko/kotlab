0fad87803b33bc3b836b5aef34857ec55be4a634
Alexey Shtanko <shtankopro@gmail.com>
2020-07-07 10:56:21 +0300

    Add leetcode solution

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DecompressRLElist.kt
@@ -0,0 +1,20 @@
+package dev.shtanko.algorithms.leetcode
+
+import java.util.ArrayList
+
+fun IntArray.decompressRLElist(): IntArray {
+    val list: MutableList<Int> = ArrayList()
+    var i = 0
+    while (i < size) {
+        for (j in 0 until this[i]) {
+            list.add(this[i + 1])
+        }
+        i += 2
+    }
+
+    val res = IntArray(list.size)
+    for (j in list.indices) {
+        res[j] = list[j]
+    }
+    return res
+}
