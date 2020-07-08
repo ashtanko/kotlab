bd9574e2a83235c2dedbc779e243561b8824d94c
Alexey Shtanko <shtankopro@gmail.com>
2020-07-08 12:46:26 +0300

    Update build file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
 
     defaultConfig {
         applicationId = "dev.shtanko.algorithms"
         minSdkVersion(21)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
 
@@ -19,13 +19,28 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            // TODO configure proguard
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+    }
+
+    lintOptions {
+        // Eliminates UnusedResources false positives for resources used in DataBinding layouts
+        isCheckGeneratedSources = true
+        // Running lint over the debug variant is enough
+        isCheckReleaseBuilds = false
+        // See lint.xml for rules configuration
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    // To avoid the compile error: "Cannot inline bytecode built with JVM target 1.8
+    // into bytecode that is being built with JVM target 1.6"
+    kotlinOptions {
+        val options = this as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+        options.jvmTarget = "1.8"
     }
 }
 
