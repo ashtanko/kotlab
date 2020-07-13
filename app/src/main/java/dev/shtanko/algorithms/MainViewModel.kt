d7790bd457d963edb95d2b7b7b7cdbebd4d800dc
Alexey Shtanko <shtankopro@gmail.com>
2020-07-13 13:12:24 +0300

    Add MainViewModel

---
+++ app/src/main/java/dev/shtanko/algorithms/MainViewModel.kt
@@ -0,0 +1,5 @@
+package dev.shtanko.algorithms
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()
---
+++ app/src/test/java/dev/shtanko/algorithms/MainViewModelTest.kt
@@ -0,0 +1,6 @@
+package dev.shtanko.algorithms
+
+class MainViewModelTest {
+
+    private lateinit var testee: MainViewModel
+}
