375969fdcb306d25062a2933eb3cdc7208442718
Alexey Shtanko <shtankopro@gmail.com>
2020-08-08 21:57:40 +0300

    Small fixes

--- src/main/kotlin/dev/shtanko/algorithms/leetcode/MonotonicArray.kt
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MonotonicArray.kt
@@ -3,9 +3,10 @@ package dev.shtanko.algorithms.leetcode
 fun IntArray.isMonotonic(): Boolean {
     val n = size
     if (n == 1) return true
-    val up = (this[n - 1] - this[0]) > 0
+    val up = this[n - 1] - this[0] > 0
     for (i in 0 until n - 1) {
-        if (this[i + 1] != this[i] && (this[i + 1] - this[i] > 0) != up) {
+        val local = this[i + 1] - this[i] > 0
+        if (this[i + 1] != this[i] && local != up) {
             return false
         }
     }
