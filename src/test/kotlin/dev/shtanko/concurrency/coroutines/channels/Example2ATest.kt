ebb203935fc20c984c726f0522302ace450cf48a
Alexey Shtanko <shtankopro@gmail.com>
2020-10-04 08:54:58 +0300

    Make all test pass

--- src/test/kotlin/dev/shtanko/concurrency/coroutines/channels/Example2ATest.kt
+++ src/test/kotlin/dev/shtanko/concurrency/coroutines/channels/Example2ATest.kt
@@ -3,7 +3,7 @@ package dev.shtanko.concurrency.coroutines.channels
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import kotlin.system.measureTimeMillis
 
@@ -11,7 +11,7 @@ class Example2ATest {
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `simple test`() = runBlockingTest {
+    fun `simple test`() = runBlocking {
         val s = listOf(7, 2, 8, -9, 4, 0)
         val c = Channel<Int>()
         go { sum(s.subList(s.size / 2, s.size), c) }
--- src/test/kotlin/dev/shtanko/concurrency/coroutines/channels/Example2Test.kt
+++ src/test/kotlin/dev/shtanko/concurrency/coroutines/channels/Example2Test.kt
@@ -3,14 +3,14 @@ package dev.shtanko.concurrency.coroutines.channels
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 
 class Example2Test {
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `simple test`() = runBlockingTest {
+    fun `simple test`() = runBlocking {
         val s = listOf(7, 2, 8, -9, 4, 0)
         val c = Channel<Int>()
         go { sum(s.subList(s.size / 2, s.size), c) }
