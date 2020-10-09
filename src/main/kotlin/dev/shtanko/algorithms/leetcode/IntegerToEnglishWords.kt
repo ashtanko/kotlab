dc0b068bae2750dcc4d906e2df061f62bf377303
Alexey Shtanko <shtankopro@gmail.com>
2020-10-09 16:42:48 +0300

    Add Integer To English Words solution

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/IntegerToEnglishWords.kt
@@ -0,0 +1,68 @@
+package dev.shtanko.algorithms.leetcode
+
+class IntegerToEnglishWords {
+
+    companion object {
+        private const val TWENTY = 20
+        private const val HUNDRED = 100
+        private const val THOUSAND = 1000
+        private const val MILLION = 1_000_000
+        private const val BILLION = 1_000_000_000
+
+        private const val ZERO_STRING = "Zero"
+        private const val HUNDRED_STRING = "Hundred"
+        private const val THOUSAND_STRING = "Thousand"
+        private const val MILLION_STRING = "Million"
+        private const val BILLION_STRING = "Billion"
+        private const val HUNDRED_FORMAT = "%s %s"
+        private const val THOUSAND_FORMAT = "%s %s %s"
+        private const val MILLION_FORMAT = "%s %s %s"
+        private const val BILLION_FORMAT = "%s %s %s"
+        private const val FORMAT = "%s %s %s"
+        private const val BLANK = ' '
+    }
+
+    private val belowTen = arrayOf("", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine")
+    private val belowTwenty = arrayOf(
+        "Ten",
+        "Eleven",
+        "Twelve",
+        "Thirteen",
+        "Fourteen",
+        "Fifteen",
+        "Sixteen",
+        "Seventeen",
+        "Eighteen",
+        "Nineteen"
+    )
+    private val belowHundred =
+        arrayOf("", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety")
+
+    fun numberToWords(num: Int): String {
+        return if (num == 0) ZERO_STRING else helper(num)
+    }
+
+    private fun helper(num: Int): String {
+        val result: String = when {
+            num < DECIMAL -> belowTen[num]
+            num < TWENTY -> belowTwenty[num - DECIMAL]
+            num < HUNDRED -> String.format(HUNDRED_FORMAT, belowHundred[num / DECIMAL], helper(num % DECIMAL))
+            num < THOUSAND -> String.format(
+                THOUSAND_FORMAT, helper(num / HUNDRED),
+                HUNDRED_STRING,
+                helper(num % HUNDRED)
+            )
+            num < MILLION -> String.format(
+                MILLION_FORMAT,
+                helper(num / THOUSAND),
+                THOUSAND_STRING,
+                helper(num % THOUSAND)
+            )
+            num < BILLION -> String.format(
+                BILLION_FORMAT, helper(num / MILLION), MILLION_STRING, helper(num % MILLION)
+            )
+            else -> String.format(FORMAT, helper(num / BILLION), BILLION_STRING, helper(num % BILLION))
+        }
+        return result.trim { it <= BLANK }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/IntegerToEnglishWordsTest.kt
@@ -0,0 +1,33 @@
+package dev.shtanko.algorithms.leetcode
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class IntegerToEnglishWordsTest {
+
+    companion object {
+        @JvmStatic
+        fun dataProvider(): List<Pair<Int, String>> {
+            return listOf(
+                0 to "Zero",
+                1 to "One",
+                22 to "Twenty Two",
+                123 to "One Hundred Twenty Three",
+                444 to "Four Hundred Forty Four",
+                12_345 to "Twelve Thousand Three Hundred Forty Five",
+                1_234_567 to "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven",
+                1_234_567_891 to "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+            )
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    fun `simple test`(testCase: Pair<Int, String>) {
+        val num = testCase.first
+        val expected = testCase.second
+        val actual = IntegerToEnglishWords().numberToWords(num)
+        assertEquals(expected, actual)
+    }
+}
