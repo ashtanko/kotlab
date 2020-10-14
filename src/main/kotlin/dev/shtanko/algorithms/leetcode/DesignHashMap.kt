642a061d6719d776be325a5558dc012982ac7f72
Alexey Shtanko <shtankopro@gmail.com>
2020-10-14 10:11:39 +0300

    Design hash map

--- src/main/kotlin/dev/shtanko/algorithms/leetcode/DesignHashMap.kt
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DesignHashMap.kt
@@ -1,3 +1,49 @@
 package dev.shtanko.algorithms.leetcode
 
-// TODO
+internal class DesignHashMap {
+    private val nodes = Array<ListNode?>(10_000) { null }
+
+    fun put(key: Int, value: Int) {
+        val i = idx(key)
+        if (nodes[i] == null) {
+            nodes[i] = ListNode(-1, -1)
+        }
+        val prev = find(nodes[i], key)
+        if (prev?.next == null) {
+            prev?.next = ListNode(key, value)
+        } else {
+            prev.next?.value = value
+        }
+    }
+
+    fun get(key: Int): Int {
+        val i = idx(key)
+        if (nodes[i] == null) return -1
+        val node = find(nodes[i], key)
+        return if (node?.next == null) -1 else node.next?.value ?: -1
+    }
+
+    fun remove(key: Int) {
+        val i = idx(key)
+        if (nodes[i] == null) return
+        val prev = find(nodes[i], key)
+        if (prev?.next == null) return
+        prev.next = prev.next?.next
+    }
+
+    private fun idx(key: Int): Int = Integer.hashCode(key) % nodes.size
+
+    private fun find(bucket: ListNode?, key: Int): ListNode? {
+        var node: ListNode? = bucket
+        var prev: ListNode? = null
+        while (node != null && node.key != key) {
+            prev = node
+            node = node.next
+        }
+        return prev
+    }
+
+    internal class ListNode(val key: Int, var value: Int) {
+        var next: ListNode? = null
+    }
+}
--- src/test/kotlin/dev/shtanko/algorithms/leetcode/DesignHashMapTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/DesignHashMapTest.kt
@@ -1,3 +1,20 @@
 package dev.shtanko.algorithms.leetcode
 
-// TODO
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DesignHashMapTest {
+
+    @Test
+    fun `hash map behavioral test`() {
+        val map = DesignHashMap()
+        map.put(1, 1)
+        map.put(2, 2)
+        assertEquals(1, map.get(1))
+        assertEquals(-1, map.get(3))
+        map.put(2, 1)
+        assertEquals(1, map.get(2))
+        map.remove(2)
+        assertEquals(-1, map.get(2))
+    }
+}
