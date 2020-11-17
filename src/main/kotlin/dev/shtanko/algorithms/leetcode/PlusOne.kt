eabeed139b4976d51d4fe42bea9129763ab2e2b9
Alexey Shtanko <shtankopro@gmail.com>
2020-11-17 17:45:22 +0200

    Add Plus One solution

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/PlusOne.kt
@@ -0,0 +1,22 @@
+package dev.shtanko.algorithms.leetcode
+
+class PlusOne {
+
+    companion object {
+        private const val MAX_DIGIT_RESTRICTION = 9
+    }
+
+    fun perform(digits: IntArray): IntArray {
+        val carry = 1
+        for (i in digits.size - 1 downTo 0) {
+            digits[i] += carry
+            if (digits[i] <= MAX_DIGIT_RESTRICTION) {
+                return digits
+            }
+            digits[i] = 0
+        }
+        val ret = IntArray(digits.size + 1)
+        ret[0] = 1
+        return ret
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/PlusOneTest.kt
@@ -0,0 +1,30 @@
+package dev.shtanko.algorithms.leetcode
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class PlusOneTest {
+    companion object {
+
+        @JvmStatic
+        fun dataProvider(): Stream<Arguments> = Stream.of(
+            Arguments.of(intArrayOf(1, 2, 3), intArrayOf(1, 2, 4)),
+            Arguments.of(intArrayOf(4, 3, 2, 1), intArrayOf(4, 3, 2, 2)),
+            Arguments.of(intArrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 0), intArrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 1)),
+            Arguments.of(intArrayOf(0), intArrayOf(1)),
+            Arguments.of(intArrayOf(0, 0), intArrayOf(0, 1)),
+            Arguments.of(intArrayOf(), intArrayOf(1))
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    fun `simple test`(arr: IntArray, expected: IntArray) {
+        val actual = PlusOne().perform(arr)
+        println(actual.toList())
+        assertArrayEquals(expected, actual)
+    }
+}
