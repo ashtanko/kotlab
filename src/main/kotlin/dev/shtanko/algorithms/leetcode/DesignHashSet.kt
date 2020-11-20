2c6c5380d63076e6b6e88819f196c8459c39e6ba
Alexey Shtanko <shtankopro@gmail.com>
2020-11-20 17:17:46 +0200

    Implement Hash Set

--- src/main/kotlin/dev/shtanko/algorithms/leetcode/DesignHashSet.kt
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DesignHashSet.kt
@@ -1,3 +1,102 @@
 package dev.shtanko.algorithms.leetcode
 
-// TODO
+interface DesignHashSet {
+    fun add(key: Int)
+    fun remove(key: Int)
+    fun contains(key: Int): Boolean
+}
+
+class DesignHashSetImpl : DesignHashSet {
+
+    companion object {
+        private const val DEFAULT_CAPACITY = 32
+    }
+
+    private var capacity = DEFAULT_CAPACITY
+    private var cnt = 0
+    var list = arrayOfNulls<DesignList>(capacity)
+
+    init {
+        for (i in 0 until capacity) {
+            list[i] = DesignList()
+        }
+    }
+
+    override fun add(key: Int) {
+        if (contains(key)) return
+        if (cnt < capacity) {
+            addTail(key)
+        } else { // rehash
+            capacity *= 2
+            val copy = list
+            val newList = arrayOfNulls<DesignList>(capacity)
+            for (i in 0 until capacity) {
+                newList[i] = DesignList()
+            }
+            list = newList
+            for (i in copy.indices) {
+                var head: DesignListNode? = copy[i]!!.head
+                while (head!!.next != null && head.next?.value != null) {
+                    addTail(head.next?.value!!)
+                    head = head.next
+                }
+            }
+            addTail(key)
+        }
+        cnt++
+    }
+
+    override fun remove(key: Int) {
+        if (!contains(key)) return
+        cnt--
+        val idx = key % capacity
+        var head: DesignListNode? = list[idx]!!.head
+        while (head!!.next != null && head.next?.value != null) {
+            if (head.next?.value == key) {
+                val next = head.next
+                next!!.next?.pre = head
+                head.next = next.next
+                next.pre = null
+                next.next = null
+                break
+            }
+            head = head.next
+        }
+    }
+
+    override fun contains(key: Int): Boolean {
+        val idx = key % capacity
+        var head: DesignListNode? = list[idx]!!.head
+        while (head!!.next != null && head.next?.value != null) {
+            if (head.next?.value == key) return true
+            head = head.next
+        }
+        return false
+    }
+
+    private fun addTail(key: Int) {
+        val idx = key % capacity
+        val tail = list[idx]!!.tail
+        val pre: DesignListNode = tail.pre ?: return // TODO
+        val cur = DesignListNode(key)
+        pre.next = cur
+        cur.pre = pre
+        cur.next = tail
+        tail.pre = cur
+    }
+}
+
+class DesignList {
+    var head = DesignListNode()
+    var tail = DesignListNode()
+
+    init {
+        head.next = tail
+        tail.pre = head
+    }
+}
+
+data class DesignListNode(var value: Int? = null) {
+    var next: DesignListNode? = null
+    var pre: DesignListNode? = null
+}
--- src/test/kotlin/dev/shtanko/algorithms/leetcode/DesignHashSetTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/DesignHashSetTest.kt
@@ -1,5 +1,21 @@
 package dev.shtanko.algorithms.leetcode
 
+import org.junit.Assert.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
 class DesignHashSetTest {
-    // TODO
+
+    @Test
+    fun `hash set test`() {
+        val hashSet: DesignHashSet = DesignHashSetImpl()
+        hashSet.add(1)
+        hashSet.add(2)
+        assertTrue(hashSet.contains(1))
+        assertFalse(hashSet.contains(3))
+        hashSet.add(2)
+        assertTrue(hashSet.contains(2))
+        hashSet.remove(2)
+        assertFalse(hashSet.contains(2))
+    }
 }
