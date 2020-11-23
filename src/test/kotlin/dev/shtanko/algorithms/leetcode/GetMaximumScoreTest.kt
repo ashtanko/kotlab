6d27b60369314ef492be041ad28ef374c5411d40
Alexey Shtanko <shtankopro@gmail.com>
2020-11-23 17:07:53 +0200

    Add Get Maximum Score solution

---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/GetMaximumScoreTest.kt
@@ -0,0 +1,34 @@
+package dev.shtanko.algorithms.leetcode
+
+import junit.framework.Assert.assertEquals
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+class GetMaximumScoreTest {
+
+    class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(2, 4, 5, 8, 10),
+                intArrayOf(4, 6, 8, 9),
+                30
+            ),
+            Arguments.of(
+                intArrayOf(1, 3, 5, 7, 9),
+                intArrayOf(3, 5, 100),
+                109
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `get the maximum score test`(nums1: IntArray, nums2: IntArray, expected: Int) {
+        val actual = GetMaximumScore().maxSum(nums1, nums2)
+        assertEquals(expected, actual)
+    }
+}
