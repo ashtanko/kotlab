4adfab02d5b05c1f2b463cd226dd936e165bef47
Alexey Shtanko <shtankopro@gmail.com>
2020-11-25 11:13:59 +0200

    Add yet another Valid Parentheses solution

--- src/main/kotlin/dev/shtanko/algorithms/leetcode/ValidParentheses.kt
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ValidParentheses.kt
@@ -17,3 +17,30 @@ fun String.isValidParentheses(): Boolean {
     }
     return stack.isEmpty()
 }
+
+class ValidParentheses(private val mappings: MutableMap<Char, Char> = HashMap()) {
+
+    init {
+        mappings[')'] = '('
+        mappings['}'] = '{'
+        mappings[']'] = '['
+    }
+
+    fun perform(s: String): Boolean {
+        // Initialize a stack to be used in the algorithm.
+        val stack = Stack<Char>()
+
+        for (character: Char in s) {
+            // If the current character is a closing bracket.
+            if (mappings.containsKey(character)) {
+                if (stack.isEmpty() || stack.pop() != mappings[character]) return false
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(character)
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty()
+    }
+}
--- src/test/kotlin/dev/shtanko/algorithms/leetcode/ValidParenthesesTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/ValidParenthesesTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import java.util.TreeMap
 
 class ValidParenthesesTest {
 
@@ -16,7 +17,6 @@ class ValidParenthesesTest {
                 false to "(]",
                 false to "([)]",
                 true to "{[]}"
-
             )
         }
     }
@@ -25,6 +25,19 @@ class ValidParenthesesTest {
     @MethodSource("dataProvider")
     fun `is valid parentheses test`(testCase: Pair<Boolean, String>) {
         val actual = testCase.second.isValidParentheses()
+        if (testCase.first) {
+            assertTrue(actual)
+        } else {
+            assertFalse(actual)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    fun `is valid parentheses mapping test`(testCase: Pair<Boolean, String>) {
+        val s = testCase.second
+        val actual = ValidParentheses(TreeMap()).perform(s)
+
         if (testCase.first) {
             assertTrue(actual)
         } else {
