a63839ecbe7a85943540943dd343b5bf1ca6b572
Alexey Shtanko <shtankopro@gmail.com>
2020-12-04 12:17:54 +0200

    Refactor sort test cases

--- src/test/kotlin/dev/shtanko/algorithms/sorts/TestObject.kt
+++ src/test/kotlin/dev/shtanko/algorithms/sorts/TestObject.kt
@@ -2,6 +2,12 @@ package dev.shtanko.algorithms.sorts
 
 data class TestObject(val id: Int, val name: String) : Comparable<TestObject> {
 
+    companion object {
+        fun empty(): TestObject {
+            return TestObject(0, "")
+        }
+    }
+
     override fun compareTo(other: TestObject): Int {
 
         val n = this.name.compareTo(other.name)
