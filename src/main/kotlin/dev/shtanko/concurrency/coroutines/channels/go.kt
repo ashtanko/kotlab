24c15aafeba4b557d668f646d9fcfd92bb2ab3fa
ashtanko <shtankopro@gmail.com>
2020-12-10 13:05:26 +0200

    Clean up

--- src/main/kotlin/dev/shtanko/concurrency/coroutines/channels/go.kt
+++
@@ -1,8 +0,0 @@
-package dev.shtanko.concurrency.coroutines.channels
-
-import dev.shtanko.concurrency.coroutines.context.CommonPool
-import dev.shtanko.concurrency.coroutines.run.runBlocking
-
-fun mainBlocking(block: suspend () -> Unit) = runBlocking(CommonPool, block)
-
-fun go(block: suspend () -> Unit) = CommonPool.runParallel(block)
