14aa0199aaa95d41b669b647031822078193cd3b
ashtanko <shtankopro@gmail.com>
2020-12-24 18:54:33 +0200

    Add solution of All Paths From Source to Target problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/AllPathsSourceTarget.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2020 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.ArrayList
+
+import java.util.LinkedList
+
+/**
+ * All Paths From Source to Target.
+ * @link https://leetcode.com/problems/all-paths-from-source-to-target/
+ */
+interface AllPathsSourceTarget {
+    fun perform(graph: Array<IntArray>): List<List<Int>>
+}
+
+/**
+ * Approach 1: Backtracking.
+ * Time Complexity: O(2^N*N)
+ * Space Complexity: O(2^N*N)
+ */
+class AllPathsSourceBacktracking : AllPathsSourceTarget {
+
+    private var target = 0
+    private lateinit var graph: Array<IntArray>
+    private val results: MutableList<List<Int>> = ArrayList()
+
+    override fun perform(graph: Array<IntArray>): List<List<Int>> {
+        this.graph = graph
+        target = graph.size - 1
+        this.graph = graph
+        // adopt the LinkedList for fast access to the tail element.
+        // adopt the LinkedList for fast access to the tail element.
+        val path = LinkedList<Int>()
+        path.addLast(0)
+        // kick of the backtracking, starting from the source (node 0)
+        // kick of the backtracking, starting from the source (node 0)
+        this.backtrack(0, path)
+        return results
+    }
+
+    private fun backtrack(currNode: Int, path: LinkedList<Int>) {
+        if (currNode == target) {
+            // Note: one should make a deep copy of the path
+            results.add(ArrayList(path))
+            return
+        }
+        // explore the neighbor nodes one after another.
+        for (nextNode in graph[currNode]) {
+            // mark the choice, before backtracking.
+            path.addLast(nextNode)
+            this.backtrack(nextNode, path)
+            // remove the previous choice, to try the next choice
+            path.removeLast()
+        }
+    }
+}
+
+/**
+ * Top-Down Dynamic Programming.
+ */
+class AllPathsSourceDP : AllPathsSourceTarget {
+
+    private var target = 0
+    private lateinit var graph: Array<IntArray>
+    private val memo: HashMap<Int, List<List<Int>>> = HashMap()
+
+    override fun perform(graph: Array<IntArray>): List<List<Int>> {
+        this.target = graph.size - 1
+        this.graph = graph
+        return allPathsToTarget(0) ?: emptyList()
+    }
+
+    private fun allPathsToTarget(currNode: Int): List<List<Int>>? {
+        // memoization. check the result in the cache first
+        if (memo.containsKey(currNode)) return memo[currNode]
+        val results: MutableList<List<Int>> = ArrayList()
+        // base case
+        if (currNode == target) {
+            val path = ArrayList<Int>()
+            path.add(target)
+            results.add(path)
+            return results
+        }
+
+        // iterate through the paths starting from each neighbor.
+        for (nextNode in graph[currNode]) {
+            for (path in allPathsToTarget(nextNode)!!) {
+                val newPath = ArrayList<Int>()
+                newPath.add(currNode)
+                newPath.addAll(path)
+                results.add(newPath)
+            }
+        }
+        memo[currNode] = results
+        return results
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/AllPathsSourceTargetTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class AllPathsSourceTargetTest<out T : AllPathsSourceTarget>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(intArrayOf(1), intArrayOf()),
+                listOf(listOf(0, 1))
+            ),
+            Arguments.of(
+                arrayOf(intArrayOf(1, 2, 3), intArrayOf(2), intArrayOf(3), intArrayOf()),
+                listOf(listOf(0, 1, 2, 3), listOf(0, 2, 3), listOf(0, 3))
+            ),
+            Arguments.of(
+                arrayOf(intArrayOf(1, 3), intArrayOf(2), intArrayOf(3), intArrayOf()),
+                listOf(listOf(0, 1, 2, 3), listOf(0, 3))
+            ),
+            Arguments.of(
+                arrayOf(intArrayOf(1, 2), intArrayOf(3), intArrayOf(3), intArrayOf()),
+                listOf(listOf(0, 1, 3), listOf(0, 2, 3))
+            ),
+            Arguments.of(
+                arrayOf(intArrayOf(4, 3, 1), intArrayOf(3, 2, 4), intArrayOf(3), intArrayOf(4), intArrayOf()),
+                listOf(listOf(0, 4), listOf(0, 3, 4), listOf(0, 1, 3, 4), listOf(0, 1, 2, 3, 4), listOf(0, 1, 4))
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `all paths source target test`(graph: Array<IntArray>, expected: List<List<Int>>) {
+        val actual = strategy.perform(graph)
+        assertThat(actual, equalTo(expected))
+    }
+}
+
+internal class AllPathsSourceBacktrackingTest :
+    AllPathsSourceTargetTest<AllPathsSourceBacktracking>(AllPathsSourceBacktracking())
+
+internal class AllPathsSourceDPTest :
+    AllPathsSourceTargetTest<AllPathsSourceDP>(AllPathsSourceDP())
