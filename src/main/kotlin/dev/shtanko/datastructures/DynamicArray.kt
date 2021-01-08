b12d68a2a2c1bb8e069008045021ea24ee195a90
ashtanko <shtankopro@gmail.com>
2021-01-08 20:49:17 +0200

    Add DynamicArray implementation
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/datastructures/DynamicArray.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.datastructures
+
+class DynamicArray<T>(initialCapacity: Int) {
+    private var data: Array<Any?>
+    private var size: Int = 0
+    private var initialCapacity: Int
+
+    // Init over constructor
+    init {
+        data = arrayOfNulls(initialCapacity)
+        this.initialCapacity = initialCapacity
+    }
+
+    fun get(index: Int): Any? = data[index]
+
+    fun set(index: Int, value: T) {
+        data[index] = value
+        size++
+    }
+
+    fun insert(index: Int, value: T) {
+        // Check size
+        if (size == initialCapacity) {
+            resize()
+        }
+        // Copy up | Shift
+        for (i in size downTo index) {
+            data[i + 1] = data[i]
+        }
+
+        // Insert
+        data[index] = value
+        size++
+    }
+
+    fun print() {
+        data.forEach { println("data[i] = $it") }
+    }
+
+    fun add(value: T) {
+        if (size == initialCapacity) {
+            resize()
+        }
+        data[size] = value
+        size++
+    }
+
+    fun delete(index: Int) {
+        // Copy down
+        for (i in index until size - 1) data[i] = data[i + 1]
+
+        // Clear if last element in array
+        if (index == size) data[index] = null else data[size - 1] = null
+
+        size--
+    }
+
+    fun size() = size
+
+    fun isEmpty() = size == 0
+
+    fun contains(value: T): Boolean {
+        data.forEach {
+            if (it == value) return true
+        }
+        return false
+    }
+
+    private fun resize() {
+        val newCapacity = if (initialCapacity == 0) 1 else initialCapacity * 2
+        val newData = arrayOfNulls<Any?>(newCapacity)
+        if (initialCapacity >= 0) System.arraycopy(data, 0, newData, 0, initialCapacity)
+        // manual copy array
+        /* for (int i = 0; i < initialCapacity; i++) {
+            newData[i] = data[i];
+        }*/
+        data = newData
+        initialCapacity = newCapacity
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/datastructures/DynamicArrayTest.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.datastructures
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+internal class DynamicArrayTest {
+
+    @Test
+    fun `Should Set Value Then Get By Index`() {
+        val array = DynamicArray<String>(3)
+
+        assertThat(array.isEmpty()).isTrue
+
+        array.set(0, "Kotlin")
+        array.set(1, "is really")
+        array.set(2, "awesome")
+
+        assertThat(array.isEmpty()).isFalse
+
+        assertThat(array.size()).isEqualTo(3)
+        assertThat(array.get(0)).isEqualTo("Kotlin")
+        assertThat(array.get(1)).isEqualTo("is really")
+        assertThat(array.get(2)).isEqualTo("awesome")
+    }
+
+    @Test
+    fun `Should Insert Into Array`() {
+        val array = DynamicArray<Int>(4)
+        array.set(0, 0)
+        array.set(1, 1000)
+        array.set(2, 2000)
+        array.set(3, 3000)
+
+        array.insert(1, 999)
+
+        assertThat(array.size()).isEqualTo(5)
+        assertThat(array.get(0)).isEqualTo(0)
+        assertThat(array.get(1)).isEqualTo(999)
+        assertThat(array.get(2)).isEqualTo(1000)
+        assertThat(array.get(3)).isEqualTo(2000)
+        assertThat(array.get(4)).isEqualTo(3000)
+    }
+
+    @Test
+    @Disabled
+    fun `Should Insert Then Add To Array`() {
+        val array = DynamicArray<String>(0)
+        array.insert(0, "insert 0")
+        array.insert(1, "insert 1")
+
+        array.add("add")
+
+        assertThat(array.size()).isEqualTo(3)
+    }
+
+    @Test
+    fun `Should Add To Array`() {
+        val array = DynamicArray<String>(0)
+
+        array.add("add")
+        array.add("add2")
+
+        assertThat(array.size()).isEqualTo(2)
+    }
+
+    @Test
+    fun `Should Delete From Array`() {
+        val array = DynamicArray<String>(0)
+
+        array.add("will be deleted")
+        array.add("won't be deleted")
+        array.add("won't be deleted2")
+
+        array.delete(0)
+
+        assertThat(array.size()).isEqualTo(2)
+        assertThat(array.get(0)).isEqualTo("won't be deleted")
+        assertThat(array.get(1)).isEqualTo("won't be deleted2")
+    }
+
+    @Test
+    fun `Should Verify If Contains Same Value`() {
+        val array = DynamicArray<String>(0)
+
+        array.add("text2")
+        array.add("text1")
+
+        assertThat(array.contains("text1")).isTrue
+        assertThat(array.contains("unknown")).isFalse
+    }
+
+    @Test
+    fun `Resize Capacity Test`() {
+        val array = DynamicArray<String>(3)
+
+        array.add("text2")
+        array.add("text1")
+        array.add("text3")
+        array.add("text4")
+
+        assertThat(array.size()).isEqualTo(4)
+    }
+}
