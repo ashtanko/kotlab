e9600085441212c3383cf7eb329f6c36c4f81af3
ashtanko <shtankopro@gmail.com>
2021-01-09 16:57:55 +0200

    Add solution of Counting Elements problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CountingElements.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * Counting Elements
+ * @link https://leetcode.com/problems/counting-elements/
+ */
+interface CountingElements {
+    fun perform(arr: IntArray): Int
+}
+
+/**
+ * Approach 1: Search with Array
+ * Time complexity : O(N^2)
+ * Space complexity : O(1)
+ */
+class CESearchWithArray : CountingElements {
+    override fun perform(arr: IntArray): Int {
+        var count = 0
+        for (x in arr) {
+            if (x.plus(1).isInArray(arr)) {
+                count++
+            }
+        }
+        return count
+    }
+
+    /**
+     * @param arr
+     * Check is target in the array
+     */
+    private fun Int.isInArray(arr: IntArray): Boolean {
+        for (x in arr) {
+            if (x == this) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+/**
+ * Approach 2: Search with HashSet
+ * Time complexity : O(N).
+ * Space complexity : O(N).
+ */
+class CESearchingWithHashSet : CountingElements {
+    override fun perform(arr: IntArray): Int {
+        val set = arr.toHashSet()
+        var count = 0
+        for (x in arr) {
+            val value = x.plus(1)
+            if (set.contains(value)) {
+                count++
+            }
+        }
+        return count
+    }
+}
+
+/**
+ * Approach 3: Search with Sorted Array
+ * Time complexity : O(NlogN).
+ * Space complexity : varies from O(N) to O(1).
+ */
+class CESearchingWithSortedArray : CountingElements {
+    override fun perform(arr: IntArray): Int {
+        arr.sort()
+        var count = 0
+        var runLength = 1
+        for (i in 1 until arr.size) {
+            if (arr[i - 1] != arr[i]) {
+                if (arr[i - 1] + 1 == arr[i]) {
+                    count += runLength
+                }
+                runLength = 0
+            }
+            runLength++
+        }
+        return count
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/CountingElementsTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class CountingElementsTest<out T : CountingElements>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(),
+                0
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3),
+                2
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 3, 3, 5, 5, 7, 7),
+                0
+            ),
+            Arguments.of(
+                intArrayOf(1, 3, 2, 3, 5, 0),
+                3
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 2, 2),
+                2
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 2),
+                2
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `counting elements test`(arr: IntArray, expected: Int) {
+        val actual = strategy.perform(arr)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class CESearchWithArrayTest : CountingElementsTest<CESearchWithArray>(CESearchWithArray())
+internal class CESearchingWithHashSetTest : CountingElementsTest<CESearchingWithHashSet>(CESearchingWithHashSet())
+internal class CESearchingWithSortedArrayTest :
+    CountingElementsTest<CESearchingWithSortedArray>(CESearchingWithSortedArray())
