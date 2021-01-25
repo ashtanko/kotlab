eb9ea4b4849b30194801033b943f6f30828a53b0
ashtanko <shtankopro@gmail.com>
2021-01-26 00:16:48 +0200

    Small refactor
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- scripts/git-hooks/pre-commit.sh
+++ scripts/git-hooks/pre-commit.sh
@@ -5,7 +5,7 @@ JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export JAVA_HOME
 
 OUTPUT="/tmp/analysis-result"
-./gradlew detekt ktlint lintDevDebug spotlessCheck --daemon > ${OUTPUT}
+./gradlew detekt ktlintCheck spotlessCheck spotlessApply --profile --daemon > ${OUTPUT}
 EXIT_CODE=$?
 if [ ${EXIT_CODE} -ne 0 ]; then
     cat ${OUTPUT}
