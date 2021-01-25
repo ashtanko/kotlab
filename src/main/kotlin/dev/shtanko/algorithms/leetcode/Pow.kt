8941f2f5519705e34751c2545fbb6375df1fa77d
ashtanko <shtankopro@gmail.com>
2021-01-25 12:48:39 +0200

    Add solution of Pow problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/Pow.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 50. Pow(x, n)
+ * @link https://leetcode.com/problems/powx-n/
+ */
+interface Pow {
+    fun perform(x: Double, n: Int): Double
+}
+
+/**
+ * Approach 1: Brute Force
+ * Time complexity : O(n).
+ * Space complexity : O(1).
+ */
+class PowBruteForce : Pow {
+    override fun perform(x: Double, n: Int): Double {
+        var a = n.toLong()
+        var b = x
+        if (a < 0) {
+            b = 1 / x
+            a = -a
+        }
+        var ans = 1.0
+        for (i in 0 until a) {
+            ans *= b
+        }
+        return ans
+    }
+}
+
+/**
+ * Approach 2: Fast Power Algorithm Recursive
+ * Time complexity : O(log n).
+ * Space complexity : O(log n).
+ */
+class PowFastRecursive : Pow {
+    override fun perform(x: Double, n: Int): Double {
+        var a = n.toLong()
+        var b = x
+        if (a < 0) {
+            b = 1 / x
+            a = -a
+        }
+
+        return fastPow(b, a)
+    }
+
+    private fun fastPow(x: Double, n: Long): Double {
+        if (n == 0L) {
+            return 1.0
+        }
+        val half = fastPow(x, n / 2)
+        return if (n % 2 == 0L) {
+            half * half
+        } else {
+            half * half * x
+        }
+    }
+}
+
+/**
+ * Approach 3: Fast Power Algorithm Iterative
+ * Time complexity : O(log n).
+ * Space complexity : O(1).
+ */
+class PowFastIterative : Pow {
+    override fun perform(x: Double, n: Int): Double {
+        var a = n.toLong()
+        var b = x
+        if (a < 0) {
+            b = 1 / x
+            a = -a
+        }
+
+        var ans = 1.0
+        var currentProduct = b
+
+        var i: Long = a
+        while (i > 0) {
+            if (i % 2 == 1L) {
+                ans *= currentProduct
+            }
+            currentProduct *= currentProduct
+            i /= 2
+        }
+
+        return ans
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/PowTest.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class PowTest<out T : Pow>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                2.00000,
+                10,
+                1024.00000
+            ),
+            Arguments.of(
+                2.10000,
+                3,
+                9.261000000000001
+                // 9.26100
+            ),
+            Arguments.of(
+                2.00000,
+                -2,
+                0.25000
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `pow test`(x: Double, n: Int, expected: Double) {
+        val actual = strategy.perform(x, n)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class PowBruteForceTest : PowTest<PowBruteForce>(PowBruteForce())
+internal class PowFastRecursiveTest : PowTest<PowFastRecursive>(PowFastRecursive())
+internal class PowFastIterativeTest : PowTest<PowFastIterative>(PowFastIterative())
