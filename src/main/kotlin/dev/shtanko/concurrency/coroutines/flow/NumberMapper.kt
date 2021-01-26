cbb7ba50d50374e9ecccb25ddd4528a6e6119755
ashtanko <shtankopro@gmail.com>
2021-01-26 14:31:13 +0200

    Add flow example
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/concurrency/coroutines/flow/NumberMapper.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.concurrency.coroutines.flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+
+/**
+ * Map collection of n to collection of string
+ */
+object NumberMapper {
+
+    private const val DELAY_MILLIS = 100L
+
+    /**
+     *  @return [Flow]
+     */
+    suspend fun map(n: Int): List<String> {
+        return (0..n)
+            .asFlow()
+            .filter {
+                it % 2 == 0
+            }
+            .map { delay(DELAY_MILLIS); "$it" }
+            .toList()
+    }
+}
