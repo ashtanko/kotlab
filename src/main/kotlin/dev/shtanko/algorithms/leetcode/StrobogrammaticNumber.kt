4cbd8f15c47070fe652956a2deda8f739b500ea1
ashtanko <shtankopro@gmail.com>
2021-01-27 17:03:16 +0200

    Add solution of Strobogrammatic Number problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/StrobogrammaticNumber.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 246. Strobogrammatic Number
+ */
+interface StrobogrammaticNumber {
+    fun perform(num: String): Boolean
+}
+
+/**
+ * Approach 1: Make a Rotated Copy
+ * Time complexity : O(N).
+ * Space complexity : O(N).
+ */
+class StrobogrammaticRotated : StrobogrammaticNumber {
+    override fun perform(num: String): Boolean {
+        val rotatedDigits = charArrayOf('0', '1', '\u0000', '\u0000', '\u0000', '\u0000', '9', '\u0000', '8', '6')
+        val rotatedStringBuilder = StringBuilder()
+        for (i in num.length - 1 downTo 0) {
+            val c: Char = num[i]
+            val charIndex = Character.getNumericValue(c)
+            rotatedStringBuilder.append(rotatedDigits[charIndex])
+        }
+
+        val rotatedString = rotatedStringBuilder.toString()
+        return num == rotatedString
+    }
+}
+
+/**
+ * Approach 2: Two Pointers
+ * Time complexity : O(N).
+ * Space complexity : O(1).
+ */
+class StrobogrammaticTwoPointers : StrobogrammaticNumber {
+    override fun perform(num: String): Boolean {
+        val rotatedDigits: Map<Char, Char> = hashMapOf('0' to '0', '1' to '1', '6' to '9', '8' to '8', '9' to '6')
+
+        var left = 0
+        var right: Int = num.length - 1
+        while (left <= right) {
+            val leftChar: Char = num[left]
+            val rightChar: Char = num[right]
+            if (!rotatedDigits.containsKey(leftChar) || rotatedDigits[leftChar] != rightChar) {
+                return false
+            }
+            left++
+            right--
+        }
+
+        return true
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/StrobogrammaticNumberTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class StrobogrammaticNumberTest<out T : StrobogrammaticNumber>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "",
+                true
+            ),
+            Arguments.of(
+                "1",
+                true
+            ),
+            Arguments.of(
+                "69",
+                true
+            ),
+            Arguments.of(
+                "88",
+                true
+            ),
+            Arguments.of(
+                "962",
+                false
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `is strobogrammatic number test`(num: String, expected: Boolean) {
+        val actual = strategy.perform(num)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class StrobogrammaticRotatedTest : StrobogrammaticNumberTest<StrobogrammaticRotated>(StrobogrammaticRotated())
+internal class StrobogrammaticTwoPointersTest :
+    StrobogrammaticNumberTest<StrobogrammaticTwoPointers>(StrobogrammaticTwoPointers())
