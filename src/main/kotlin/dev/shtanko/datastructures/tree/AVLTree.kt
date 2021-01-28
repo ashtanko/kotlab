1a3aafcd726a72aa7c346396b77e64ccaeda64c5
ashtanko <shtankopro@gmail.com>
2021-01-28 18:47:16 +0200

    AVL tree refactor
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- src/main/kotlin/dev/shtanko/datastructures/tree/AVLTree.kt
+++ src/main/kotlin/dev/shtanko/datastructures/tree/AVLTree.kt
@@ -18,152 +18,132 @@ package dev.shtanko.datastructures.tree
 
 import org.slf4j.LoggerFactory
 
+/**
+ * AVL Tree
+ * @link https://www.geeksforgeeks.org/avl-tree-set-1-insertion/
+ */
 class AVLTree {
 
     companion object {
         private val LOGGER = LoggerFactory.getLogger(AVLTree::class.java)
-        private const val PRINT_FORMAT = "%s"
-        private const val TWO = 2
     }
 
     data class Node(
         val key: Int,
-        var balance: Int? = null,
-        var height: Int? = null,
+        var height: Int = 1,
         var left: Node? = null,
-        var right: Node? = null,
-        var parent: Node? = null
+        var right: Node? = null
     )
 
-    private var root: Node? = null
+    var root: Node? = null
 
-    fun insert(key: Int): Boolean {
-        if (root == null) {
-            root = Node(key = key, parent = null)
-        } else {
-            var n = root
-            var parent: Node?
-            while (true) {
-                if (n?.key == key) {
-                    return false
-                }
-                parent = n
-                val goLeft = if (n == null) false else n.key > key
+    fun insert(node: Node?, key: Int): Node? {
 
-                n = if (goLeft) {
-                    n?.left
-                } else {
-                    n?.right
-                }
-                if (n == null) {
-                    if (goLeft) {
-                        parent?.left = Node(key = key, parent = parent)
-                    } else {
-                        parent?.right = Node(key = key, parent = parent)
-                    }
-                    reBalance(parent)
-                    break
-                }
-            }
+        /* 1.  Perform the normal BST insertion */
+        if (node == null) return Node(key)
+        when {
+            key < node.key -> node.left = insert(node.left, key)
+            key > node.key -> node.right =
+                insert(node.right, key)
+            // Duplicate keys not allowed
+            else -> return node
         }
 
-        return true
-    }
+        /* 2. Update height of this ancestor node */node.height = 1 + max(
+            height(node.left),
+            height(node.right)
+        )
 
-    private fun reBalance(n: Node?) {
-        var node = n
-        setBalance(node)
-        if (node?.balance == -TWO) {
-            node = if (height(node.left?.left) >= height(node.left?.right)) rotateRight(n) else rotateLeftThenRight(n)
-        } else if (n?.balance == TWO) {
-            node = if (height(n.right?.right) >= height(n.right?.left)) rotateLeft(n) else rotateRightThenLeft(n)
+        /* 3. Get the balance factor of this ancestor
+              node to check whether this node became
+              unbalanced */
+        val balance = getBalance(node)
+
+        // If this node becomes unbalanced, then there
+        // are 4 cases Left Left Case
+        if (balance > 1 && key < node.left!!.key) return rightRotate(node)
+
+        // Right Right Case
+        if (balance < -1 && key > node.right!!.key) return leftRotate(node)
+
+        // Left Right Case
+        if (balance > 1 && key > node.left!!.key) {
+            node.left = leftRotate(node.left!!)
+            return rightRotate(node)
         }
-        if (n?.parent != null) {
-            reBalance(n.parent)
-        } else {
-            root = node
+
+        // Right Left Case
+        if (balance < -1 && key < node.right!!.key) {
+            node.right = rightRotate(node.right!!)
+            return leftRotate(node)
         }
+
+        /* return the (unchanged) node pointer */
+        return node
     }
 
-    fun printBalance() {
-        printBalance(root)
-    }
-
-    private fun rotateLeft(a: Node?): Node? {
-        val right = a?.right
-
-        right?.parent = a?.parent
-
-        a?.right = right?.left
-        if (a?.right != null) {
-            a.right?.parent = a
-        }
-        right?.left = a
-        a?.parent = right
-        return rotateTree(a, right)
-    }
-
-    private fun rotateRight(a: Node?): Node? {
-        val left = a?.left
-
-        left?.parent = a?.parent
-
-        a?.left = left?.right
-        if (a?.left != null) {
-            a.left?.parent = a
-        }
-        left?.right = a
-        a?.parent = left
-        return rotateTree(a, left)
-    }
-
-    private fun rotateTree(a: Node?, b: Node?): Node? {
-        if (b?.parent != null) {
-            if (b.parent?.right == a) {
-                b.parent?.right = b
-            } else {
-                b.parent?.left = b
-            }
-        }
-        setBalance(a, b)
-        return b
-    }
-
-    private fun rotateLeftThenRight(n: Node?): Node? {
-        n?.left = rotateLeft(n?.left)
-        return rotateRight(n)
-    }
-
-    private fun rotateRightThenLeft(n: Node?): Node? {
-        n?.right = rotateRight(n?.right)
-        return rotateLeft(n)
-    }
-
-    private fun printBalance(n: Node?) {
-        if (n != null) {
-            printBalance(n.left)
-            LOGGER.info(String.format(PRINT_FORMAT, n.balance))
-            printBalance(n.right)
+    // A utility function to print preorder traversal
+    // of the tree.
+    // The function also prints height of every node
+    fun preOrder(node: Node?) {
+        if (node != null) {
+            LOGGER.info("${node.key}")
+            preOrder(node.left)
+            preOrder(node.right)
         }
     }
 
-    private fun setBalance(vararg nodes: Node?) {
-        for (node in nodes) {
-            reHeight(node)
-            node?.balance = height(node?.right) - height(node?.left)
-        }
+    // Get Balance factor of node N
+    private fun getBalance(n: Node?): Int {
+        return if (n == null) 0 else height(n.left) - height(n.right)
     }
 
-    private fun reHeight(node: Node?) {
-        if (node == null) {
-            node?.height = 1 + height(node?.left).coerceAtLeast(height(node?.right))
-        }
+    // A utility function to left rotate subtree rooted with x
+    // See the diagram given above.
+    private fun leftRotate(x: Node): Node? {
+        val y = x.right
+        val t2 = y?.left
+
+        // Perform rotation
+        y?.left = x
+        x.right = t2
+
+        //  Update heights
+        x.height = max(height(x.left), height(x.right)) + 1
+        y?.height = max(height(y?.left), height(y?.right)) + 1
+
+        // Return new root
+        return y
     }
 
+    // A utility function to right rotate subtree rooted with y
+    // See the diagram given above.
+    private fun rightRotate(y: Node): Node? {
+        val x = y.left
+        val t2 = x?.right
+
+        // Perform rotation
+        x?.right = y
+        y.left = t2
+
+        // Update heights
+        y.height = max(height(y.left), height(y.right)) + 1
+        x?.height = max(height(x?.left), height(x?.right)) + 1
+
+        // Return new root
+        return x
+    }
+
+    // A utility function to get maximum of two integers
+    fun max(a: Int, b: Int): Int {
+        return if (a > b) a else b
+    }
+
+    // A utility function to get the height of the tree
     private fun height(n: Node?): Int {
         if (n == null) {
-            return -1
+            return 0
         }
-        return n.height ?: -1
+        return n.height
     }
 }
--- src/test/kotlin/dev/shtanko/datastructures/tree/AVLTreeTest.kt
+++ src/test/kotlin/dev/shtanko/datastructures/tree/AVLTreeTest.kt
@@ -16,16 +16,92 @@
 
 package dev.shtanko.datastructures.tree
 
+import dev.shtanko.patterns.utils.InMemoryAppender
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class AVLTreeTest {
 
+    private lateinit var appender: InMemoryAppender
+
+    @BeforeEach
+    fun setUp() {
+        appender = InMemoryAppender()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        appender.stop()
+    }
+
+    /* The constructed AVL Tree would be
+             30
+            /  \
+          20   40
+         /  \    \
+        10  25   50
+    */
     @Test
-    internal fun `empty test`() {
+    internal fun `insert items test`() {
         val tree = AVLTree()
-        for (i in 1 until 10) {
-            tree.insert(i)
-        }
-        tree.printBalance()
+        tree.root = tree.insert(tree.root, 10)
+        tree.root = tree.insert(tree.root, 20)
+        tree.root = tree.insert(tree.root, 30)
+        tree.root = tree.insert(tree.root, 40)
+        tree.root = tree.insert(tree.root, 50)
+        tree.root = tree.insert(tree.root, 25)
+        tree.preOrder(tree.root)
+
+        assertEquals("50", appender.lastMessage)
+        assertEquals(6, appender.logSize)
+    }
+
+    @Test
+    internal fun `insert duplicate keys`() {
+        val tree = AVLTree()
+        tree.root = tree.insert(tree.root, 10)
+        tree.root = tree.insert(tree.root, 10)
+        tree.preOrder(tree.root)
+
+        assertEquals("10", appender.lastMessage)
+        assertEquals(1, appender.logSize)
+    }
+
+    @Test
+    internal fun `insert items in random order test`() {
+        val tree = AVLTree()
+        tree.root = tree.insert(tree.root, 12)
+        tree.root = tree.insert(tree.root, 8)
+        tree.root = tree.insert(tree.root, 18)
+        tree.root = tree.insert(tree.root, 17)
+        tree.root = tree.insert(tree.root, 11)
+        tree.root = tree.insert(tree.root, 5)
+        tree.root = tree.insert(tree.root, 4)
+        tree.preOrder(tree.root)
+
+        assertEquals("17", appender.lastMessage)
+        assertEquals(7, appender.logSize)
+    }
+
+    @Test
+    internal fun `insert items 2 test`() {
+        val tree = AVLTree()
+        tree.root = tree.insert(tree.root, 10)
+        tree.root = tree.insert(tree.root, 22)
+        tree.root = tree.insert(tree.root, 12)
+        tree.root = tree.insert(tree.root, 20)
+        tree.root = tree.insert(tree.root, 25)
+        tree.root = tree.insert(tree.root, 28)
+        tree.root = tree.insert(tree.root, 30)
+        tree.root = tree.insert(tree.root, 48)
+        tree.root = tree.insert(tree.root, 38)
+        tree.root = tree.insert(tree.root, 40)
+        tree.root = tree.insert(tree.root, 36)
+        tree.preOrder(tree.root)
+
+        assertEquals("40", appender.lastMessage)
+        assertEquals(11, appender.logSize)
     }
 }
