f988d3c3a804311a3fedcd1ca0b30592c8d7664a
ashtanko <shtankopro@gmail.com>
2021-01-29 12:31:55 +0200

    Add solution of Super Palindromes problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/SuperPalindromes.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import dev.shtanko.algorithms.extensions.isPalindrome
+
+enum class SPNumType(val value: Int) {
+    EVEN(1),
+    ODD(2)
+}
+
+/**
+ * 906. Super Palindromes
+ * @link https://leetcode.com/problems/super-palindromes/
+ */
+object SuperPalindromes {
+    private const val MAGIC = 100_000
+
+    fun superPalindromesInRange(left: String, right: String): Int {
+        val l = left.toLong()
+        val r = right.toLong()
+        var ans = 0
+        countLen(l, r, ans, SPNumType.ODD) {
+            ans++
+        }
+        countLen(l, r, ans, SPNumType.EVEN) {
+            ans++
+        }
+        return ans
+    }
+
+    private fun countLen(l: Long, r: Long, a: Int, numType: SPNumType, increase: () -> Unit) {
+        for (k in 1 until MAGIC) {
+            val sb = StringBuilder(k.toString())
+            for (i in sb.length - numType.value downTo 0) {
+                sb.append(sb[i])
+            }
+            var v = sb.toString().toLong()
+            v *= v
+            if (v > r) break
+            if (v >= l && v.isPalindrome()) increase()
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/SuperPalindromesTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal class SuperPalindromesTest {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "0",
+                "0",
+                0
+            ),
+            Arguments.of(
+                "-4",
+                "-1000",
+                0
+            ),
+            Arguments.of(
+                "1",
+                "1",
+                1
+            ),
+            Arguments.of(
+                "4",
+                "1000",
+                4
+            ),
+            Arguments.of(
+                "1",
+                "2",
+                1
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `super palindromes test`(left: String, right: String, expected: Int) {
+        val actual = SuperPalindromes.superPalindromesInRange(left, right)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
