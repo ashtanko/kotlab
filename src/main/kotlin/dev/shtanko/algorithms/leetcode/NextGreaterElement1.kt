87ad9fc585d18765a081b804fa2374a82083e38d
ashtanko <shtankopro@gmail.com>
2021-02-07 19:45:22 +0200

    Add solution of Next Greater Element I problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/NextGreaterElement1.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.Stack
+
+interface NextGreaterElement1 {
+    fun perform(nums1: IntArray, nums2: IntArray): IntArray
+}
+
+class NGBruteForce : NextGreaterElement1 {
+    override fun perform(nums1: IntArray, nums2: IntArray): IntArray {
+        val res = IntArray(nums1.size)
+        var j: Int
+        for (i in nums1.indices) {
+            var found = false
+            j = 0
+            while (j < nums2.size) {
+                if (found && nums2[j] > nums1[i]) {
+                    res[i] = nums2[j]
+                    break
+                }
+                if (nums2[j] == nums1[i]) {
+                    found = true
+                }
+                j++
+            }
+            if (j == nums2.size) {
+                res[i] = -1
+            }
+        }
+        return res
+    }
+}
+
+class NGBetterForce : NextGreaterElement1 {
+    override fun perform(nums1: IntArray, nums2: IntArray): IntArray {
+        val hash: HashMap<Int, Int> = HashMap()
+        val res = IntArray(nums1.size)
+        var j: Int
+        for (i in nums2.indices) {
+            hash[nums2[i]] = i
+        }
+        for (i in nums1.indices) {
+            j = hash[nums1[i]]!! + 1
+            while (j < nums2.size) {
+                if (nums1[i] < nums2[j]) {
+                    res[i] = nums2[j]
+                    break
+                }
+                j++
+            }
+            if (j == nums2.size) {
+                res[i] = -1
+            }
+        }
+        return res
+    }
+}
+
+class NGStack : NextGreaterElement1 {
+    override fun perform(nums1: IntArray, nums2: IntArray): IntArray {
+        val stack: Stack<Int> = Stack()
+        val map: HashMap<Int, Int> = HashMap()
+        val res = IntArray(nums1.size)
+        for (i in nums2.indices) {
+            while (!stack.empty() && nums2[i] > stack.peek()) map[stack.pop()] = nums2[i]
+            stack.push(nums2[i])
+        }
+        while (!stack.empty()) map[stack.pop()] = -1
+        for (i in nums1.indices) {
+            res[i] = map[nums1[i]]!!
+        }
+        return res
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/NextGreaterElement1Test.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class NextGreaterElement1Test<out T : NextGreaterElement1>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(4, 1, 2),
+                intArrayOf(1, 3, 4, 2),
+                intArrayOf(-1, 3, -1),
+            ),
+            Arguments.of(
+                intArrayOf(2, 4),
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(3, -1),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `next greater element test`(nums1: IntArray, nums2: IntArray, expected: IntArray) {
+        val actual = strategy.perform(nums1, nums2)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class NGBruteForceTest : NextGreaterElement1Test<NGBruteForce>(NGBruteForce())
+internal class NGBetterForceTest : NextGreaterElement1Test<NGBetterForce>(NGBetterForce())
+internal class NGStackTest : NextGreaterElement1Test<NGStack>(NGStack())
