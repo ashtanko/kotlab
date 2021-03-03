e0f19b093fa2481a98ce7240abb5c7f39f69f0f8
ashtanko <shtankopro@gmail.com>
2021-03-03 18:10:44 +0200

    Solution of Single Row Keyboard problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/SingleRowKeyboard.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.abs
+
+interface SingleRowKeyboard {
+    fun perform(keyboard: String, word: String): Int
+}
+
+class SingleRowKeyboardNaive : SingleRowKeyboard {
+    override fun perform(keyboard: String, word: String): Int {
+        val map = IntArray(LIMIT)
+        for (i in keyboard.indices) {
+            map[keyboard[i] - 'a'] = i
+        }
+        var res = 0
+        var prev = 0
+        for (w in word) {
+            val idx = map[w - 'a']
+            res += abs(prev - idx)
+            prev = idx
+        }
+
+        return res
+    }
+
+    companion object {
+        private const val LIMIT = 26
+    }
+}
