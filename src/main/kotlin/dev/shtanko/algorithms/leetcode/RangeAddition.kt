e8d278001fcd0abe82d59b884773f32545181826
ashtanko <shtankopro@gmail.com>
2021-03-03 17:27:52 +0200

    Solutions of Range Addition problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/RangeAddition.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import dev.shtanko.algorithms.extensions.second
+
+/**
+ * 370. Range Addition
+ * @link https://leetcode.com/problems/range-addition/
+ */
+interface RangeAddition {
+    fun perform(length: Int, updates: Array<IntArray>): IntArray
+}
+
+/**
+ * Approach 1: Naive Approach
+ * Time complexity : O(n * k)
+ * Space complexity : O(1)
+ */
+class RangeAdditionBruteForce : RangeAddition {
+    override fun perform(length: Int, updates: Array<IntArray>): IntArray {
+        val ans = IntArray(length) { 0 }
+        for (triplet in updates) {
+            val startIndex = triplet.first()
+            val endIndex = triplet.second()
+            val inc = triplet.last()
+            for (i in startIndex..endIndex) {
+                ans[i] += inc
+            }
+        }
+        return ans
+    }
+}
+
+/**
+ * Approach 2: Range Caching
+ * Time complexity : O(n + k)
+ * Space complexity : O(1)
+ */
+class RangeAdditionCaching : RangeAddition {
+    override fun perform(length: Int, updates: Array<IntArray>): IntArray {
+        val ans = IntArray(length) { 0 }
+        for (triplet in updates) {
+            val startIndex = triplet.first()
+            val endIndex = triplet.second()
+            val inc = triplet.last()
+            ans[startIndex] += inc
+            if (endIndex < length - 1) {
+                ans[endIndex + 1] -= inc
+            }
+        }
+        var sum = 0
+        for (i in 0 until length) {
+            sum += ans[i]
+            ans[i] = sum
+        }
+        return ans
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/RangeAdditionTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class RangeAdditionTest<out T : RangeAddition>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                5,
+                arrayOf(
+                    intArrayOf(1, 3, 2),
+                    intArrayOf(2, 4, 3),
+                    intArrayOf(0, 2, -2)
+                ),
+                intArrayOf(-2, 0, 3, 5, 3)
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `range addition test`(length: Int, updates: Array<IntArray>, expected: IntArray) {
+        val actual = strategy.perform(length, updates)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class RangeAdditionBruteForceTest : RangeAdditionTest<RangeAdditionBruteForce>(RangeAdditionBruteForce())
+internal class RangeAdditionCachingTest : RangeAdditionTest<RangeAdditionCaching>(RangeAdditionCaching())
