434eeee3b06ffc031336187f4b2f5fdeec7833f1
ashtanko <shtankopro@gmail.com>
2021-03-22 17:36:40 +0200

    Solution of Vowel Spellchecker problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/VowelSpellchecker.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+interface VowelSpellchecker {
+    fun perform(wordlist: Array<String>, queries: Array<String>): Array<String>
+}
+
+class VowelSpellcheckerImpl : VowelSpellchecker {
+
+    private val wordsPerfect: MutableSet<String> = HashSet()
+    private val wordsCap: MutableMap<String, String> = HashMap()
+    private val wordsVow: MutableMap<String, String> = HashMap()
+
+    override fun perform(wordlist: Array<String>, queries: Array<String>): Array<String> {
+        for (word in wordlist) {
+            wordsPerfect.add(word)
+            val wordlow = word.toLowerCase()
+            wordsCap.putIfAbsent(wordlow, word)
+            val wordlowDV = devowel(wordlow)
+            wordsVow.putIfAbsent(wordlowDV, word)
+        }
+
+        val ans = Array(queries.size) { "" }
+        var t = 0
+        for (query in queries) ans[t++] = solve(query) ?: ""
+        return ans
+    }
+
+    private fun solve(query: String): String? {
+        if (wordsPerfect.contains(query)) return query
+        val queryL = query.toLowerCase()
+        if (wordsCap.containsKey(queryL)) return wordsCap[queryL]
+        val queryLV = devowel(queryL)
+        return if (wordsVow.containsKey(queryLV)) wordsVow[queryLV] else ""
+    }
+
+    private fun devowel(word: String): String {
+        val ans = StringBuilder()
+        for (c in word.toCharArray()) ans.append(if (isVowel(c)) '*' else c)
+        return ans.toString()
+    }
+
+    private fun isVowel(c: Char): Boolean {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/VowelSpellcheckerTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class VowelSpellcheckerTest<out T : VowelSpellchecker>(private val strategy: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf<String>(),
+                arrayOf<String>(),
+                arrayOf<String>(),
+            ),
+            Arguments.of(
+                arrayOf("KiTe", "kite", "hare", "Hare"),
+                arrayOf("kite", "Kite", "KiTe", "Hare", "HARE", "Hear", "hear", "keti", "keet", "keto"),
+                arrayOf("kite", "KiTe", "KiTe", "Hare", "hare", "", "", "KiTe", "", "KiTe"),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `spellchecker test`(wordlist: Array<String>, queries: Array<String>, expected: Array<String>) {
+        val actual = strategy.perform(wordlist, queries)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class VowelSpellcheckerImplTest : VowelSpellcheckerTest<VowelSpellcheckerImpl>(VowelSpellcheckerImpl())
