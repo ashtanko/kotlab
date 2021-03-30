64a3fe0f0a6ac7693430b483ccd735db861a37d4
ashtanko <shtankopro@gmail.com>
2021-03-30 18:08:31 +0300

    Update async test
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- src/test/kotlin/dev/shtanko/concurrency/coroutines/async/AsyncTest.kt
+++ src/test/kotlin/dev/shtanko/concurrency/coroutines/async/AsyncTest.kt
@@ -18,14 +18,45 @@ package dev.shtanko.concurrency.coroutines.async
 
 import dev.shtanko.concurrency.TestBase
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class AsyncTest : TestBase() {
+
+    @Test
+    fun `test simple`() = runTest {
+        expect(1)
+        val job: Deferred<Int> = async {
+            expect(3)
+            42
+        }
+        expect(2)
+        assertTrue(job.isActive)
+        assertThat(job.await()).isEqualTo(42)
+        assertTrue(job.isActive.not())
+        expect(4)
+        assertThat(job.await()).isEqualTo(42) // second await -- same result
+        finish(5)
+    }
+
+    @Test
+    fun `test undispatched`() = runTest {
+        expect(1)
+        val d: Deferred<Int> = async(start = CoroutineStart.UNDISPATCHED) {
+            expect(2)
+            42
+        }
+        expect(3)
+        assertTrue(!d.isActive)
+        assertEquals(d.await(), 42)
+        finish(4)
+    }
 
     @Test
     fun `starts the coroutine lazily`() = runTest {
