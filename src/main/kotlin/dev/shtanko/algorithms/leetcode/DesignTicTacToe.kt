691bc27bccf089b33bdacf02400aea61ee4662d7
ashtanko <shtankopro@gmail.com>
2021-05-23 20:27:24 +0300

    Design Tic Tac Toe
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DesignTicTacToe.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.abs
+
+/**
+ * Design Tic-Tac-Toe
+ * @link https://leetcode.com/problems/design-tic-tac-toe/
+ */
+interface DesignTicTacToe {
+    fun move(row: Int, col: Int, player: Int): Int
+}
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(n^2)
+ */
+class TTTOptimizedBruteForce(val n: Int) : DesignTicTacToe {
+
+    private val board: Array<IntArray> = Array(n) { IntArray(n) }
+
+    override fun move(row: Int, col: Int, player: Int): Int {
+        board[row][col] = player
+        // check if the player wins
+        val rowOrColumn = checkRow(row, player) || checkColumn(col, player) || row == col && checkDiagonal(player)
+        if (rowOrColumn || col == n - row - 1 && checkAntiDiagonal(player)) {
+            return player
+        }
+        // No one wins
+        return 0
+    }
+
+    private fun checkDiagonal(player: Int): Boolean {
+        for (row in 0 until n) {
+            if (board[row][row] != player) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkAntiDiagonal(player: Int): Boolean {
+        for (row in 0 until n) {
+            if (board[row][n - row - 1] != player) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkColumn(col: Int, player: Int): Boolean {
+        for (row in 0 until n) {
+            if (board[row][col] != player) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkRow(row: Int, player: Int): Boolean {
+        for (col in 0 until n) {
+            if (board[row][col] != player) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+/**
+ * Approach 2: Optimised Approach
+ * Time Complexity: O(1)
+ * Space Complexity: O(n)
+ */
+class TTTOptimised(val n: Int) : DesignTicTacToe {
+
+    private var rows: IntArray = IntArray(n)
+    private var cols: IntArray = IntArray(n)
+    private var diagonal = 0
+    private var antiDiagonal = 0
+
+    override fun move(row: Int, col: Int, player: Int): Int {
+        val currentPlayer = if (player == 1) 1 else -1
+        // update currentPlayer in rows and cols arrays
+        rows[row] += currentPlayer
+        cols[col] += currentPlayer
+        // update diagonal
+        if (row == col) {
+            diagonal += currentPlayer
+        }
+        // update anti diagonal
+        if (col == cols.size - row - 1) {
+            antiDiagonal += currentPlayer
+        }
+        val n: Int = rows.size
+        // check if the current player wins
+        val rowsOrCols = abs(rows[row]) == n || abs(cols[col]) == n
+        return if (rowsOrCols || abs(diagonal) == n || abs(antiDiagonal) == n) {
+            player
+        } else 0
+        // No one wins
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/DesignTicTacToeTest.kt
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+class DesignTicTacToeTest {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(TTTOptimizedBruteForce(3)),
+            Arguments.of(TTTOptimised(3)),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `move test`(ticTacToe: DesignTicTacToe) {
+        // |X| | |
+        // | | | |    // Player 1 makes a move at (0, 0).
+        // | | | |
+        assertThat(ticTacToe.move(0, 0, 1)).isEqualTo(0)
+
+        // |X| |O|
+        // | | | |    // Player 2 makes a move at (0, 2).
+        // | | | |
+        assertThat(ticTacToe.move(0, 2, 2)).isEqualTo(0)
+
+        // |X| |O|
+        // | | | |    // Player 1 makes a move at (2, 2).
+        // | | |X|
+        assertThat(ticTacToe.move(2, 2, 1)).isEqualTo(0)
+
+        // |X| |O|
+        // | |O| |    // Player 2 makes a move at (1, 1).
+        // | | |X|
+        assertThat(ticTacToe.move(1, 1, 2)).isEqualTo(0)
+
+        // |X| |O|
+        // | |O| |    // Player 1 makes a move at (2, 0).
+        // |X| |X|
+        assertThat(ticTacToe.move(2, 0, 1)).isEqualTo(0)
+
+        // |X| |O|
+        // |O|O| |    // Player 2 makes a move at (1, 0).
+        // |X| |X|
+        assertThat(ticTacToe.move(1, 0, 2)).isEqualTo(0)
+
+        // |X| |O|
+        // |O|O| |    // Player 1 makes a move at (2, 1).
+        // |X|X|X|
+        assertThat(ticTacToe.move(2, 1, 1)).isEqualTo(1)
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `dead heat test`(ticTacToe: DesignTicTacToe) {
+        // |X| | |
+        // | | | |    // Player 1 makes a move at (0, 0).
+        // | | | |
+        assertThat(ticTacToe.move(0, 0, 1)).isEqualTo(0)
+
+        // |X| | |
+        // |0| | |    // Player 2 makes a move at (1, 0).
+        // | | | |
+        assertThat(ticTacToe.move(1, 0, 2)).isEqualTo(0)
+
+        // |X| | |
+        // |0| | |    // Player 1 makes a move at (2, 0).
+        // |X| | |
+        assertThat(ticTacToe.move(2, 0, 1)).isEqualTo(0)
+
+        // |X| | |
+        // |0|0| |    // Player 2 makes a move at (1, 1).
+        // |X| | |
+        assertThat(ticTacToe.move(1, 1, 2)).isEqualTo(0)
+
+        // |X| | |
+        // |0|0| |    // Player 1 makes a move at (1, 2).
+        // |X| | |
+        assertThat(ticTacToe.move(1, 2, 1)).isEqualTo(0)
+
+        // |X| |0|
+        // |0|0|X|    // Player 2 makes a move at (0, 2).
+        // |X| | |
+        assertThat(ticTacToe.move(0, 2, 2)).isEqualTo(0)
+
+        // |X| |0|
+        // |0|0|X|    // Player 1 makes a move at (2, 1).
+        // |X|X| |
+        assertThat(ticTacToe.move(2, 1, 1)).isEqualTo(0)
+
+        // |X| |0|
+        // |0|0|X|    // Player 2 makes a move at (2, 2).
+        // |X|X|0|
+        assertThat(ticTacToe.move(2, 2, 2)).isEqualTo(0)
+
+        // |X|X|0|
+        // |0|0|X|    // Player 1 makes a move at (0, 1).
+        // |X|X|0|
+        assertThat(ticTacToe.move(0, 1, 1)).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `win X row 0 test`(ticTacToe: DesignTicTacToe) {
+        // |X| | |
+        // | | | |    // Player 1 makes a move at (0, 0).
+        // | | | |
+        assertThat(ticTacToe.move(0, 0, 1)).isEqualTo(0)
+
+        // |X| | |
+        // |0| | |    // Player 2 makes a move at (1, 0).
+        // | | | |
+        assertThat(ticTacToe.move(1, 0, 2)).isEqualTo(0)
+
+        // |X|X| |
+        // |0| | |    // Player 1 makes a move at (0, 1).
+        // | | | |
+        assertThat(ticTacToe.move(0, 1, 1)).isEqualTo(0)
+
+        // |X|X| |
+        // |0|0| |    // Player 2 makes a move at (0, 1).
+        // | | | |
+        assertThat(ticTacToe.move(1, 1, 2)).isEqualTo(0)
+
+        // |X|X|X|
+        // |0|0| |    // Player 1 makes a move at (0, 2).
+        // | | | |
+        assertThat(ticTacToe.move(0, 2, 1)).isEqualTo(1)
+    }
+}
