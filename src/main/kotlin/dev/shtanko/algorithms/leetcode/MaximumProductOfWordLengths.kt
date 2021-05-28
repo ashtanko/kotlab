f09436481a140ea591fd7e2af28a13754c176943
ashtanko <shtankopro@gmail.com>
2021-05-28 23:17:45 +0300

    Maximum Product of Word Lengths
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MaximumProductOfWordLengths.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 318. Maximum Product of Word Lengths
+ * @link https://leetcode.com/problems/maximum-product-of-word-lengths/
+ */
+interface MaximumProductOfWordLengths {
+    fun maxProduct(words: Array<String>): Int
+}
+
+fun Char.bitNumber(): Int {
+    return this.code - 'a'.code
+}
+
+/**
+ * Approach 1: Optimize noCommonLetters function : Bitmasks + Precomputation
+ */
+class MaxProductBitmasks : MaximumProductOfWordLengths {
+
+    override fun maxProduct(words: Array<String>): Int {
+        val n: Int = words.size
+        val masks = IntArray(n)
+        val lens = IntArray(n)
+
+        var bitmask: Int
+        for (i in 0 until n) {
+            bitmask = 0
+            for (ch in words[i].toCharArray()) {
+                // add bit number bit_number in bitmask
+                bitmask = bitmask or (1 shl ch.bitNumber())
+            }
+            masks[i] = bitmask
+            lens[i] = words[i].length
+        }
+
+        var maxVal = 0
+        for (i in 0 until n) for (j in i + 1 until n) if (masks[i] and masks[j] == 0) maxVal =
+            max(maxVal, lens[i] * lens[j])
+
+        return maxVal
+    }
+}
+
+/**
+ * Approach 2: Optimise Number of Comparisons : Bitmasks + Precomputation + Hashmap
+ */
+class MaxProductHashmap : MaximumProductOfWordLengths {
+    override fun maxProduct(words: Array<String>): Int {
+        val hashmap: MutableMap<Int, Int> = HashMap()
+
+        var bitmask = 0
+        val bitNum = 0
+        for (word in words) {
+            bitmask = 0
+            for (ch in word.toCharArray()) {
+                // add bit number bitNumber in bitmask
+                bitmask = bitmask or (1 shl ch.bitNumber())
+            }
+            // there could be different words with the same bitmask
+            // ex. ab and aabb
+            hashmap[bitmask] = max(hashmap.getOrDefault(bitmask, 0), word.length)
+        }
+
+        var maxProd = 0
+        for (x in hashmap.keys) for (y in hashmap.keys) if (x and y == 0) maxProd =
+            max(maxProd, hashmap[x]!! * hashmap[y]!!)
+
+        return maxProd
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MaximumProductOfWordLengthsTest.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+internal abstract class MaximumProductOfWordLengthsTest<out T : MaximumProductOfWordLengths>(private val solution: T) {
+    internal class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf("abcw", "baz", "foo", "bar", "xtfn", "abcdef"),
+                16
+            ),
+            Arguments.of(
+                arrayOf("a", "ab", "abc", "d", "cd", "bcd", "abcd"),
+                4
+            ),
+            Arguments.of(
+                arrayOf("a", "aa", "aaa", "aaaa"),
+                0
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `max product test`(words: Array<String>, expected: Int) {
+        val actual = solution.maxProduct(words)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+internal class MaxProductBitmasksTest : MaximumProductOfWordLengthsTest<MaxProductBitmasks>(MaxProductBitmasks())
+internal class MaxProductHashmapTest : MaximumProductOfWordLengthsTest<MaxProductHashmap>(MaxProductHashmap())
