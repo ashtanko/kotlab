5285d53bc7a17d47d1ed937216cb865f8953998d
Alexey Shtanko <shtankopro@gmail.com>
2021-07-01 12:32:10 +0300

    Bump detekt version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -18,7 +18,7 @@ object Versions {
     const val KOTLIN_VERSION = "1.5.10"
     const val LINCHECK = "2.10"
     const val RX_JAVA = "3.0.12"
-    const val DETEKT = "1.17.0"
+    const val DETEKT = "1.17.1"
     const val COROUTINES = "1.5.0"
     const val SPEK = "2.0.15"
     const val TEST = "1.2.0"
