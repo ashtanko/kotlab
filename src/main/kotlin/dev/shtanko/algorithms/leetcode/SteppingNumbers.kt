3a3c46d71d90727d79cd9f7f8bad23f16718208a
ashtanko <shtankopro@gmail.com>
2021-10-12 19:35:51 +0300

    Solution of Stepping Numbers

--- src/main/kotlin/dev/shtanko/algorithms/leetcode/SteppingNumbers.kt
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/SteppingNumbers.kt
@@ -1,4 +1,23 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.shtanko.algorithms.leetcode
+
+import java.util.LinkedList
+import java.util.Queue
 
 /**
  * 1215. Stepping Numbers
@@ -10,6 +29,24 @@ interface SteppingNumbers {
 
 class SteppingNumbersBFS : SteppingNumbers {
     override fun countSteppingNumbers(low: Int, high: Int): List<Int> {
-        TODO("Not yet implemented")
+        val res = ArrayList<Int>()
+        if (low > high) return res
+
+        val queue: Queue<Int> = LinkedList()
+        for (i in 1..9) queue.add(i)
+
+        if (low == 0) res.add(0)
+        while (!queue.isEmpty()) {
+            val p: Int = queue.poll()
+            if (p < high) {
+                val last = p % 10
+                if (last > 0) queue.add(p * 10 + last - 1)
+                if (last < 9) queue.add(p * 10 + last + 1)
+            }
+            if (p in low..high) {
+                res.add(p)
+            }
+        }
+        return res
     }
 }
