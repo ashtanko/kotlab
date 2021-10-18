48f89d70609d6c9796b55b9468f9c07f13aa1c21
ashtanko <shtankopro@gmail.com>
2021-10-18 18:20:31 +0300

    Solution of closest BST problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ClosestBST.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.Collections
+import java.util.LinkedList
+import kotlin.math.abs
+
+/**
+ * 270. Closest Binary Search Tree Value
+ * https://leetcode.com/problems/closest-binary-search-tree-value/
+ */
+interface ClosestBST {
+    fun closestValue(root: TreeNode?, target: Double): Int
+}
+
+/**
+ * Approach 1: Recursive Inorder + Linear search
+ * Time complexity : O(N)
+ * Space complexity : O(N)
+ */
+class RecursiveInorder : ClosestBST {
+    override fun closestValue(root: TreeNode?, target: Double): Int {
+        val nums = inorder(root)
+        val comparator = Comparator<Int> { o1, o2 ->
+            val first = abs(o1 - target)
+            val second = abs(o2 - target)
+            return@Comparator if (first < second) -1 else 1
+        }
+        return Collections.min(nums, comparator)
+    }
+
+    private fun inorder(root: TreeNode?): List<Int> {
+        if (root == null) return emptyList()
+        val nums = mutableListOf<Int>()
+        inorder(root.left)
+        nums.add(root.value)
+        inorder(root.right)
+        return nums
+    }
+}
+
+/**
+ * Approach 2: Iterative Inorder, O(k) time
+ */
+class IterativeInorder : ClosestBST {
+    override fun closestValue(root: TreeNode?, target: Double): Int {
+        val stack: LinkedList<TreeNode?> = LinkedList()
+        var pred = Int.MIN_VALUE
+        var node = root
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.add(node)
+                node = node.left
+            }
+            node = stack.removeLast()
+            if (pred <= target && target < node!!.value) return if (abs(
+                    pred - target
+                ) < abs(node.value - target)
+            ) pred else node.value
+            pred = node!!.value
+            node = node.right
+        }
+        return pred
+    }
+}
+
+/**
+ * Approach 3: Binary Search
+ * Time complexity : O(H)
+ * Space complexity : O(1)
+ */
+class ClosestBSTBinarySearch : ClosestBST {
+    override fun closestValue(root: TreeNode?, target: Double): Int {
+        var value: Int
+        var node = root
+        var closest: Int = node?.value ?: 0
+        while (node != null) {
+            value = node.value
+            closest = if (abs(value - target) < abs(closest - target)) value else closest
+            node = if (target < node.value) node.left else node.right
+        }
+        return closest
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/ClosestBSTTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class ClosestBSTTest<out T : ClosestBST>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(4).apply {
+                    left = TreeNode(2).apply {
+                        left = TreeNode(1)
+                        right = TreeNode(3)
+                    }
+                    right = TreeNode(5)
+                },
+                3.714286,
+                4
+            ),
+            Arguments.of(
+                TreeNode(1),
+                4.428571,
+                1
+            ),
+            Arguments.of(
+                TreeNode(22).apply {
+                    left = TreeNode(18).apply {
+                        right = TreeNode(19)
+                        left = TreeNode(9)
+                    }
+                    right = TreeNode(32).apply {
+                        right = TreeNode(45).apply {
+                            right = TreeNode(50)
+                            left = TreeNode(40)
+                        }
+                    }
+                },
+                25.5,
+                22
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `closest value test`(root: TreeNode, target: Double, expected: Int) {
+        val actual = strategy.closestValue(root, target)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class RecursiveInorderTest : ClosestBSTTest<RecursiveInorder>(RecursiveInorder())
+class IterativeInorderTest : ClosestBSTTest<IterativeInorder>(IterativeInorder())
+class ClosestBSTBinarySearchTest : ClosestBSTTest<ClosestBSTBinarySearch>(ClosestBSTBinarySearch())
