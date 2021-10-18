26ba960b075814c032b7cce6f3e23fca116ec1bb
ashtanko <shtankopro@gmail.com>
2021-10-18 18:04:21 +0300

    Solution of Validate BST problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ValidateBinarySearchTree.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.ArrayDeque
+import java.util.Deque
+import java.util.LinkedList
+
+/**
+ * 98. Validate Binary Search Tree
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ */
+interface ValidateBinarySearchTree {
+    fun isValidBST(root: TreeNode): Boolean
+}
+
+/**
+ * Approach 1: Recursive Traversal with Valid Range
+ */
+class RecursiveTraversalValidRange : ValidateBinarySearchTree {
+    override fun isValidBST(root: TreeNode): Boolean {
+        return validate(root, null, null)
+    }
+
+    private fun validate(root: TreeNode?, low: Int?, high: Int?): Boolean {
+        // Empty trees are valid BSTs.
+        if (root == null) {
+            return true
+        }
+        // The current node's value must be between low and high.
+        return if (low != null && root.value <= low || high != null && root.value >= high) {
+            false
+        } else validate(root.right, root.value, high) && validate(root.left, low, root.value)
+        // The left and right subtree must also be valid.
+    }
+}
+
+class IterativeTraversalValidRange : ValidateBinarySearchTree {
+
+    private val stack: Deque<TreeNode?> = LinkedList()
+    private val upperLimits: Deque<Int> = LinkedList()
+    private val lowerLimits: Deque<Int> = LinkedList()
+
+    override fun isValidBST(root: TreeNode): Boolean {
+        var low: Int? = null
+        var high: Int? = null
+        var value: Int
+        var node: TreeNode? = root
+        update(node, low, high)
+
+        while (!stack.isEmpty()) {
+            node = stack.poll()
+            low = lowerLimits.poll()
+            high = upperLimits.poll()
+            if (node == null) continue
+            value = node.value
+            if (low != null && value <= low) {
+                return false
+            }
+            if (high != null && value >= high) {
+                return false
+            }
+            update(node.right, value, high)
+            update(node.left, low, value)
+        }
+        return true
+    }
+
+    private fun update(root: TreeNode?, low: Int?, high: Int?) {
+        stack.add(root)
+        lowerLimits.add(low)
+        upperLimits.add(high)
+    }
+}
+
+/**
+ * Approach 3: Recursive Inorder Traversal
+ */
+class RecursiveInorderTraversal : ValidateBinarySearchTree {
+
+    private var prev: Int? = null
+
+    override fun isValidBST(root: TreeNode): Boolean {
+        return inorder(root)
+    }
+
+    private fun inorder(root: TreeNode?): Boolean {
+        if (root == null) {
+            return true
+        }
+        if (!inorder(root.left)) {
+            return false
+        }
+        if (prev != null && root.value <= prev!!) {
+            return false
+        }
+        prev = root.value
+        return inorder(root.right)
+    }
+}
+
+/**
+ * Approach 4: Iterative Inorder Traversal
+ */
+class IterativeInorderTraversal : ValidateBinarySearchTree {
+    override fun isValidBST(root: TreeNode): Boolean {
+        val stack: Deque<TreeNode> = ArrayDeque()
+        var prev: Int? = null
+        var node: TreeNode? = root
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node)
+                node = node.left
+            }
+            node = stack.pop()
+            // If next element in inorder traversal
+            // is smaller than the previous one
+            // that's not BST.
+            if (prev != null && node.value <= prev) {
+                return false
+            }
+            prev = node.value
+            node = node.right
+        }
+        return true
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/ValidateBSTTest.kt
@@ -0,0 +1,275 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class ValidateBSTTest<out T : ValidateBinarySearchTree>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(2).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(3)
+                },
+                true
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(4).apply {
+                        left = TreeNode(3)
+                        right = TreeNode(6)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(4).apply {
+                    left = TreeNode(2).apply {
+                        left = TreeNode(1)
+                        right = TreeNode(3)
+                    }
+                    right = TreeNode(5)
+                },
+                true
+            ),
+            Arguments.of(
+                TreeNode(1),
+                true
+            ),
+            Arguments.of(
+                TreeNode(6).apply {
+                    left = TreeNode(7).apply {
+                        left = TreeNode(1).apply {
+                            left = TreeNode(13).apply {
+                                left = TreeNode(11)
+                                right = TreeNode(5)
+                            }
+                        }
+                        right = TreeNode(4)
+                    }
+                    right = TreeNode(9).apply {
+                        left = TreeNode(8).apply {
+                            left = TreeNode(3)
+                            right = TreeNode(12).apply {
+                                right = TreeNode(14).apply {
+                                    left = TreeNode(10)
+                                }
+                            }
+                        }
+                        right = TreeNode(2)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(3)
+                    right = TreeNode(8).apply {
+                        left = TreeNode(2)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(8).apply {
+                    left = TreeNode(3).apply {
+                        right = TreeNode(2)
+                    }
+                    right = TreeNode(6)
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(22).apply {
+                    left = TreeNode(18).apply {
+                        right = TreeNode(19)
+                        left = TreeNode(9)
+                    }
+                    right = TreeNode(32).apply {
+                        right = TreeNode(45).apply {
+                            right = TreeNode(50)
+                            left = TreeNode(40)
+                        }
+                    }
+                },
+                true
+            ),
+            Arguments.of(
+                TreeNode(300).apply {
+                    left = TreeNode(290).apply {
+                        left = TreeNode(280).apply {
+                            left = TreeNode(271).apply {
+                                left = TreeNode(260).apply {
+                                    left = TreeNode(255).apply {
+                                        left = TreeNode(250).apply {
+                                            right = TreeNode(251)
+                                        }
+                                        right = TreeNode(256).apply {
+                                            right = TreeNode(258).apply {
+                                                left = TreeNode(257)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            right = TreeNode(282).apply {
+                                left = TreeNode(281)
+                                right = TreeNode(285)
+                            }
+                        }
+                        right = TreeNode(295)
+                    }
+                    right = TreeNode(1000).apply {
+                        left = TreeNode(900).apply {
+                            right = TreeNode(990).apply {
+                                left = TreeNode(950).apply {
+                                    right = TreeNode(980)
+                                }
+                            }
+                        }
+                        right = TreeNode(2000).apply {
+                            right = TreeNode(3000).apply {
+                                right = TreeNode(4000).apply {
+                                    left = TreeNode(3500)
+                                    right = TreeNode(5000).apply {
+                                        left = TreeNode(4300).apply {
+                                            right = TreeNode(4700).apply {
+                                                right = TreeNode(4900)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                true
+            ),
+            Arguments.of(
+                TreeNode(2).apply {
+                    left = TreeNode(7).apply {
+                        left = TreeNode(2)
+                        right = TreeNode(6).apply {
+                            left = TreeNode(5)
+                            right = TreeNode(11)
+                        }
+                    }
+                    right = TreeNode(5).apply {
+                        right = TreeNode(9).apply {
+                            left = TreeNode(4)
+                        }
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(6).apply {
+                    right = TreeNode(8).apply {
+                        left = TreeNode(7)
+                        right = TreeNode(9)
+                    }
+                },
+                true
+            ),
+            Arguments.of(
+                TreeNode(6).apply {
+                    right = TreeNode(8).apply {
+                        left = TreeNode(5)
+                        right = TreeNode(9)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(2).apply {
+                    right = TreeNode(3).apply {
+                        left = TreeNode(1)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(2),
+                true
+            ),
+            Arguments.of(
+                TreeNode(2).apply {
+                    right = TreeNode(1)
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(3).apply {
+                    left = TreeNode(4)
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(4).apply {
+                        right = TreeNode(6)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(4).apply {
+                        right = TreeNode(5)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(4).apply {
+                        right = TreeNode(6)
+                    }
+                },
+                false
+            ),
+            Arguments.of(
+                TreeNode(7).apply {
+                    left = TreeNode(5).apply {
+                        right = TreeNode(6)
+                    }
+                },
+                true
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `is valid BST test`(root: TreeNode, expected: Boolean) {
+        val actual = strategy.isValidBST(root)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class RecursiveTraversalValidRangeTest : ValidateBSTTest<RecursiveTraversalValidRange>(RecursiveTraversalValidRange())
+class IterativeTraversalValidRangeTest : ValidateBSTTest<IterativeTraversalValidRange>(IterativeTraversalValidRange())
+class RecursiveInorderTraversalTest : ValidateBSTTest<RecursiveInorderTraversal>(RecursiveInorderTraversal())
+class IterativeInorderTraversalTest : ValidateBSTTest<IterativeInorderTraversal>(IterativeInorderTraversal())
