8de1bac99f3c6c9acbfab8854bef85f32528a40a
ashtanko <shtankopro@gmail.com>
2021-10-21 17:37:55 +0300

    Solution of path sum 2 problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/PathSum2.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 113. Path Sum II
+ * @link https://leetcode.com/problems/path-sum-ii/
+ */
+interface PathSum2 {
+    fun perform(root: TreeNode?, targetSum: Int): List<List<Int>>
+}
+
+/**
+ * Approach: Depth First Traversal | Recursion
+ */
+class PathSum2DFS : PathSum2 {
+    override fun perform(root: TreeNode?, targetSum: Int): List<List<Int>> {
+        val pathsList: MutableList<List<Int>> = ArrayList()
+        val pathNodes: MutableList<Int> = ArrayList()
+        recurseTree(root, targetSum, pathNodes, pathsList)
+        return pathsList
+    }
+
+    private fun recurseTree(
+        node: TreeNode?,
+        remainingSum: Int,
+        pathNodes: MutableList<Int>,
+        pathsList: MutableList<List<Int>>
+    ) {
+        if (node == null) {
+            return
+        }
+        // Add the current node to the path's list
+        pathNodes.add(node.value)
+
+        // Check if the current node is a leaf and also, if it
+        // equals our remaining sum. If it does, we add the path to
+        // our list of paths
+        if (remainingSum == node.value && node.left == null && node.right == null) {
+            pathsList.add(ArrayList(pathNodes))
+        } else {
+            // Else, we will recurse on the left and the right children
+            recurseTree(node.left, remainingSum - node.value, pathNodes, pathsList)
+            recurseTree(node.right, remainingSum - node.value, pathNodes, pathsList)
+        }
+
+        // We need to pop the node once we are done processing ALL of it's
+        // subtrees.
+        pathNodes.removeAt(pathNodes.size - 1)
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/PathSum2Test.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class PathSum2Test<out T : PathSum2>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(4).apply {
+                        left = TreeNode(11).apply {
+                            left = TreeNode(7)
+                            right = TreeNode(2)
+                        }
+                    }
+                    right = TreeNode(8).apply {
+                        left = TreeNode(13)
+                        right = TreeNode(4).apply {
+                            left = TreeNode(5)
+                            right = TreeNode(1)
+                        }
+                    }
+                },
+                22,
+                listOf(
+                    listOf(5, 4, 11, 2),
+                    listOf(5, 8, 4, 5),
+                )
+            ),
+            Arguments.of(
+                TreeNode(1),
+                2,
+                emptyList<List<Int>>()
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(4).apply {
+                        left = TreeNode(1)
+                    }
+                    right = TreeNode(5)
+                },
+                10,
+                listOf(
+                    listOf(5, 4, 1),
+                    listOf(5, 5)
+                )
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(2)
+                    right = TreeNode(3)
+                },
+                5,
+                emptyList<List<Int>>()
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(2)
+                },
+                0,
+                emptyList<List<Int>>()
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `path sum test`(root: TreeNode?, targetSum: Int, expected: List<List<Int>>) {
+        val actual = strategy.perform(root, targetSum)
+        assertThat(actual).containsAll(expected)
+    }
+}
+
+class PathSum2DFSTest : PathSum2Test<PathSum2DFS>(PathSum2DFS())
