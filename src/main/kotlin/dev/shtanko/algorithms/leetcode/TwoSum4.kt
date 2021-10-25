24ab2fffd33b1b747c228e8f96ccfe6c6966a135
Alexey Shtanko <shtankopro@gmail.com>
2021-10-25 20:15:55 +0300

    Solution of Two Sum IV - Input is a BST problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/TwoSum4.kt
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.LinkedList
+import java.util.Queue
+
+/**
+ * 653. Two Sum IV - Input is a BST
+ * @link https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+ */
+interface TwoSum4 {
+    fun findTarget(root: TreeNode, k: Int): Boolean
+}
+
+/**
+ * Approach #1 Using HashSet
+ * Time complexity : O(n).
+ * Space complexity : O(n).
+ */
+class TwoSum4HashSet : TwoSum4 {
+    override fun findTarget(root: TreeNode, k: Int): Boolean {
+        val set: MutableSet<Int?> = HashSet()
+        return find(root, k, set)
+    }
+
+    private fun find(root: TreeNode?, k: Int, set: MutableSet<Int?>): Boolean {
+        if (root == null) return false
+        if (set.contains(k - root.value)) return true
+        set.add(root.value)
+        return find(root.left, k, set) || find(root.right, k, set)
+    }
+}
+
+/**
+ * Approach #2 Using BFS and HashSet
+ * Time complexity : O(n).
+ * Space complexity : O(n).
+ */
+class TwoSum4BFS : TwoSum4 {
+    override fun findTarget(root: TreeNode, k: Int): Boolean {
+        val set: MutableSet<Int?> = HashSet()
+        val queue: Queue<TreeNode> = LinkedList()
+        queue.add(root)
+        while (!queue.isEmpty()) {
+            if (queue.peek() != null) {
+                val node: TreeNode = queue.remove()
+                if (set.contains(k - node.value)) return true
+                set.add(node.value)
+                queue.add(node.right)
+                queue.add(node.left)
+            } else queue.remove()
+        }
+        return false
+    }
+}
+
+/**
+ * Approach #3 Using BST
+ * Time complexity : O(n).
+ * Space complexity : O(n).
+ */
+class TwoSum4BST : TwoSum4 {
+    override fun findTarget(root: TreeNode, k: Int): Boolean {
+        val list: MutableList<Int> = ArrayList()
+        inorder(root, list)
+        var l = 0
+        var r = list.size - 1
+        while (l < r) {
+            val sum = list[l] + list[r]
+            if (sum == k) return true
+            if (sum < k) l++ else r--
+        }
+        return false
+    }
+
+    private fun inorder(root: TreeNode?, list: MutableList<Int>) {
+        if (root == null) return
+        inorder(root.left, list)
+        list.add(root.value)
+        inorder(root.right, list)
+    }
+}
+
+class TwoSum4DFS : TwoSum4 {
+    override fun findTarget(root: TreeNode, k: Int): Boolean {
+        return dfs(root, root, k)
+    }
+
+    fun dfs(root: TreeNode?, cur: TreeNode?, k: Int): Boolean {
+        return if (cur == null) false else search(root, cur, k - cur.value) || dfs(root, cur.left, k) || dfs(
+            root,
+            cur.right,
+            k
+        )
+    }
+
+    fun search(root: TreeNode?, cur: TreeNode, value: Int): Boolean {
+        return if (root == null) false else root.value == value && root != cur || root.value < value && search(
+            root.right,
+            cur,
+            value
+        ) || root.value > value && search(root.left, cur, value)
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/TwoSum4Test.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class TwoSum4Test<out T : TwoSum4>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(3).apply {
+                        left = TreeNode(2)
+                        right = TreeNode(4)
+                    }
+                    right = TreeNode(6).apply {
+                        right = TreeNode(7)
+                    }
+                },
+                9,
+                true
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(3).apply {
+                        left = TreeNode(2)
+                        right = TreeNode(4)
+                    }
+                    right = TreeNode(6).apply {
+                        right = TreeNode(7)
+                    }
+                },
+                28,
+                false
+            ),
+            Arguments.of(
+                TreeNode(2).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(3)
+                },
+                4,
+                true
+            ),
+            Arguments.of(
+                TreeNode(2).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(3)
+                },
+                1,
+                false
+            ),
+            Arguments.of(
+                TreeNode(2).apply {
+                    left = TreeNode(1)
+                    right = TreeNode(3)
+                },
+                3,
+                true
+            ),
+            Arguments.of(
+                TreeNode(7).apply {
+                    left = TreeNode(5).apply {
+                        left = TreeNode(3)
+                    }
+                    right = TreeNode(9).apply {
+                        right = TreeNode(12)
+                    }
+                },
+                15,
+                true
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `find target test`(root: TreeNode, target: Int, expected: Boolean) {
+        val actual = strategy.findTarget(root, target)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class TwoSum4HashSetTest : TwoSum4Test<TwoSum4HashSet>(TwoSum4HashSet())
+class TwoSum4BFSTest : TwoSum4Test<TwoSum4BFS>(TwoSum4BFS())
+class TwoSum4BSTTest : TwoSum4Test<TwoSum4BST>(TwoSum4BST())
+class TwoSum4DFSTest : TwoSum4Test<TwoSum4DFS>(TwoSum4DFS())
