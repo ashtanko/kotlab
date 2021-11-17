ebad7a7611ff20a0404b31a67b4917de3d294794
Alexey Shtanko <shtankopro@gmail.com>
2021-11-17 16:52:29 +0200

    Solution of Beautiful Arrangement problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/BeautifulArrangement.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import dev.shtanko.algorithms.extensions.swap
+
+/**
+ * https://leetcode.com/problems/beautiful-arrangement/
+ * 526. Beautiful Arrangement
+ */
+interface BeautifulArrangement {
+    fun countArrangement(n: Int): Int
+}
+
+/**
+ * Approach #2 Better Brute Force
+ */
+class BABruteForce : BeautifulArrangement {
+    private var count = 0
+
+    override fun countArrangement(n: Int): Int {
+        val nums = IntArray(n)
+        for (i in 1..n) nums[i - 1] = i
+        return permute(nums, 0)
+    }
+
+    fun permute(nums: IntArray, l: Int): Int {
+        if (l == nums.size) {
+            count++
+        }
+        for (i in l until nums.size) {
+            nums.swap(i, l)
+            if (nums[l] % (l + 1) == 0 || (l + 1) % nums[l] == 0) {
+                permute(nums, l + 1)
+            }
+            nums.swap(i, l)
+        }
+        return count
+    }
+}
+
+/**
+ * Approach #3 Backtracking
+ */
+class BABacktracking : BeautifulArrangement {
+    private var count = 0
+
+    override fun countArrangement(n: Int): Int {
+        val visited = BooleanArray(n + 1)
+        calculate(n, 1, visited)
+        return count
+    }
+
+    fun calculate(n: Int, pos: Int, visited: BooleanArray) {
+        if (pos > n) count++
+        for (i in 1..n) {
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+                visited[i] = true
+                calculate(n, pos + 1, visited)
+                visited[i] = false
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/BeautifulArrangementTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class BeautifulArrangementTest<out T : BeautifulArrangement>(private val strategy: T) {
+    class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(0, 1),
+            Arguments.of(1, 1),
+            Arguments.of(2, 2),
+            Arguments.of(3, 3),
+            Arguments.of(4, 8),
+            Arguments.of(5, 10),
+            Arguments.of(6, 36),
+            Arguments.of(7, 41),
+            Arguments.of(8, 132),
+            Arguments.of(9, 250),
+            Arguments.of(10, 700),
+            Arguments.of(11, 750),
+            Arguments.of(12, 4010),
+            Arguments.of(13, 4237),
+            Arguments.of(14, 10680),
+            Arguments.of(15, 24679),
+            Arguments.of(16, 87328),
+            Arguments.of(17, 90478),
+            Arguments.of(18, 435812),
+            Arguments.of(19, 449586),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `count arrangement test`(n: Int, expected: Int) {
+        val actual = strategy.countArrangement(n)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class BABruteForceTest : BeautifulArrangementTest<BABruteForce>(BABruteForce())
+class BABacktrackingTest : BeautifulArrangementTest<BABacktracking>(BABacktracking())
