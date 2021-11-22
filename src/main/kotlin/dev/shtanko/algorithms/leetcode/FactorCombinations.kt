444025af0fb5151fa7ce16b88b838fa06eb6e6de
Alexey Shtanko <shtankopro@gmail.com>
2021-11-22 14:17:16 +0200

    Solution of Factor Combinations problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/FactorCombinations.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.LinkedList
+import kotlin.math.sqrt
+
+/**
+ * 254. Factor Combinations
+ */
+interface FactorCombinations {
+    fun getFactors(n: Int): List<List<Int>>
+}
+
+class FactorCombinationsImpl : FactorCombinations {
+    override fun getFactors(n: Int): List<List<Int>> {
+        val result: MutableList<List<Int>> = ArrayList()
+        helper(result, ArrayList(), n, 2)
+        return result
+    }
+
+    private fun helper(result: MutableList<List<Int>>, item: MutableList<Int>, n: Int, start: Int) {
+        if (n <= 1) {
+            if (item.size > 1) {
+                result.add(ArrayList(item))
+            }
+            return
+        }
+        for (i in start..n) {
+            if (n % i == 0) {
+                item.add(i)
+                helper(result, item, n / i, i)
+                item.removeAt(item.size - 1)
+            }
+        }
+    }
+}
+
+class FactorCombinationsImpl2 : FactorCombinations {
+    override fun getFactors(n: Int): List<List<Int>> {
+        val ret: MutableList<List<Int>> = LinkedList<List<Int>>()
+        if (n <= 3) return ret
+        val path: MutableList<Int> = LinkedList<Int>()
+        getFactors(2, n, path, ret)
+        return ret
+    }
+
+    private fun getFactors(start: Int, n: Int, path: MutableList<Int>, ret: MutableList<List<Int>>) {
+        var i = start
+        while (i <= sqrt(n.toDouble())) {
+            // The previous factor is no bigger than the next
+            if (n % i == 0 && n / i >= i) {
+                path.add(i)
+                path.add(n / i)
+                ret.add(LinkedList(path))
+                path.removeAt(path.size - 1)
+                getFactors(i, n / i, path, ret)
+                path.removeAt(path.size - 1)
+            }
+            i++
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/FactorCombinationsTest.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+abstract class FactorCombinationsTest<out T : FactorCombinations>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(0, emptyList<List<Int>>()),
+            Arguments.of(1, emptyList<List<Int>>()),
+            Arguments.of(
+                12,
+                listOf(
+                    listOf(2, 6),
+                    listOf(3, 4),
+                    listOf(2, 2, 3),
+                )
+            ),
+            Arguments.of(37, emptyList<List<Int>>()),
+            Arguments.of(
+                32,
+                listOf(
+                    listOf(2, 16),
+                    listOf(4, 8),
+                    listOf(2, 2, 8),
+                    listOf(2, 4, 4),
+                    listOf(2, 2, 2, 4),
+                    listOf(2, 2, 2, 2, 2),
+                )
+            ),
+            Arguments.of(
+                15,
+                listOf(
+                    listOf(3, 5),
+                )
+            ),
+            Arguments.of(
+                8,
+                listOf(
+                    listOf(2, 2, 2),
+                    listOf(2, 4),
+                )
+            ),
+            Arguments.of(
+                9,
+                listOf(
+                    listOf(3, 3),
+                )
+            ),
+            Arguments.of(
+                100,
+                listOf(
+                    listOf(2, 2, 5, 5),
+                    listOf(2, 2, 25),
+                    listOf(2, 5, 10),
+                    listOf(2, 50),
+                    listOf(4, 5, 5),
+                    listOf(4, 25),
+                    listOf(5, 20),
+                    listOf(10, 10),
+                )
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `get factors test`(n: Int, expected: List<List<Int>>) {
+        val actual = strategy.getFactors(n)
+        assertThat(actual).containsAll(expected)
+    }
+}
+
+class FactorCombinations1Test : FactorCombinationsTest<FactorCombinations>(FactorCombinationsImpl())
+class FactorCombinations2Test : FactorCombinationsTest<FactorCombinations>(FactorCombinationsImpl2())
