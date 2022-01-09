4801db627dd1a78da883a9c7d919f99f8c1451bd
ashtanko <shtankopro@gmail.com>
2022-01-09 20:32:52 +0200

    Solution of armstrong number

---
+++ src/test/kotlin/dev/shtanko/algorithms/math/NarcissisticNumberTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.math
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class NarcissisticNumberTest<out T : NarcissisticNumber>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(370, true),
+            Arguments.of(225, false),
+            Arguments.of(-23, false),
+            Arguments.of(153, true),
+            Arguments.of(0, true),
+            Arguments.of(12, false),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `is armstrong number test`(number: Int, expected: Boolean) {
+        val actual = strategy.isArmstrong(number)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class NarcissisticNumberImplTest : NarcissisticNumberTest<NarcissisticNumber>(NarcissisticNumberImpl())
