2279c8b45c8dddf448e0c41473a837efd69a9736
Alexey Shtanko <shtankopro@gmail.com>
2022-01-26 20:35:19 +0200

    WIP Add observer pattern example

---
+++ src/test/kotlin/dev/shtanko/patterns/behavioral/observer/example1/OrcsTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.patterns.behavioral.observer.example1
+
+import java.util.function.Supplier
+
+class OrcsTest : WeatherObserverTest<Orcs>(Supplier { Orcs() }) {
+    override fun dataProvider(): Collection<Array<Any>> {
+        return listOf(
+            arrayOf(WeatherType.SUNNY, "The orcs are facing Sunny weather now"),
+            arrayOf(WeatherType.RAINY, "The orcs are facing Rainy weather now"),
+            arrayOf(WeatherType.WINDY, "The orcs are facing Windy weather now"),
+            arrayOf(WeatherType.COLD, "The orcs are facing Cold weather now")
+        )
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/patterns/behavioral/observer/example1/WeatherObserverTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.patterns.behavioral.observer.example1
+
+import dev.shtanko.patterns.utils.InMemoryAppender
+import java.util.function.Supplier
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class WeatherObserverTest<T : WeatherObserver>(private val factory: Supplier<T>) {
+    private lateinit var appender: InMemoryAppender
+
+    @BeforeEach
+    fun setUp() {
+        appender = InMemoryAppender()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        appender.stop()
+    }
+
+    abstract fun dataProvider(): Collection<Array<Any>>
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    fun `observer test`(weather: WeatherType, response: String) {
+        val observer: T = this.factory.get()
+        assertThat(appender.logSize).isEqualTo(0)
+        observer.update(weather)
+    }
+}
