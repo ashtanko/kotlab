3209df5f9b85c8d2fdaaf255d529b937fee144f4
Oleksii Shtanko <shtankopro@gmail.com>
2022-03-04 14:32:27 +0200

    Solution of Valid Anagram problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ValidAnagram.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 242. Valid Anagram
+ * @link https://leetcode.com/problems/valid-anagram/
+ */
+interface ValidAnagram {
+    fun isAnagram(s: String, t: String): Boolean
+}
+
+class ValidAnagramHashMap : ValidAnagram {
+    override fun isAnagram(s: String, t: String): Boolean {
+        val sMap = HashMap<Char, Int>()
+        val sl: Int = s.length
+        val tl: Int = t.length
+        if (sl != tl) {
+            return false
+        }
+        for (i in 0 until sl) {
+            sMap[s[i]] = sMap.getOrDefault(s[i], 0) + 1
+            sMap[t[i]] = sMap.getOrDefault(t[i], 0) - 1
+        }
+        for (c in sMap.keys) {
+            if (sMap[c] != 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class ValidAnagramImpl : ValidAnagram {
+    override fun isAnagram(s: String, t: String): Boolean {
+        val alphabet = IntArray(ALPHABET_SIZE)
+        for (i in s.indices) {
+            alphabet[s[i] - ALPHABET_FIRST_LETTER]++
+        }
+        for (i in t.indices) {
+            alphabet[t[i] - ALPHABET_FIRST_LETTER]--
+            if (alphabet[t[i] - ALPHABET_FIRST_LETTER] < 0) {
+                return false
+            }
+        }
+        for (i in alphabet) {
+            if (i != 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        private const val ALPHABET_SIZE = 26
+        private const val ALPHABET_FIRST_LETTER = 'a'
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/ValidAnagramTest.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class ValidAnagramTest<out T : ValidAnagram>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "",
+                "",
+                true
+            ),
+            Arguments.of(
+                "q",
+                "q",
+                true
+            ),
+            Arguments.of(
+                "qwe",
+                "qwe",
+                true
+            ),
+            Arguments.of(
+                "cat",
+                "tac",
+                true
+            ),
+            Arguments.of(
+                "a",
+                "b",
+                false
+            ),
+            Arguments.of(
+                "a",
+                "",
+                false
+            ),
+            Arguments.of(
+                "ab",
+                "",
+                false
+            ),
+            Arguments.of(
+                "ab",
+                "ac",
+                false
+            ),
+            Arguments.of(
+                "anagram",
+                "nagaram",
+                true
+            ),
+            Arguments.of(
+                "rat",
+                "car",
+                false
+            ),
+            Arguments.of(
+                "restful",
+                "fluster",
+                true
+            ),
+            Arguments.of(
+                "evil",
+                "vile",
+                true
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `is anagram test`(s: String, t: String, expected: Boolean) {
+        val actual = strategy.isAnagram(s, t)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class ValidAnagramHashMapTest : ValidAnagramTest<ValidAnagram>(ValidAnagramHashMap())
+class ValidAnagramImplTest : ValidAnagramTest<ValidAnagram>(ValidAnagramImpl())
