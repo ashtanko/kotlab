6c62da6ebc9a8b8f01ee419105fdc109e8555e8a
Oleksii Shtanko <shtankopro@gmail.com>
2022-03-20 19:56:35 +0200

    Solution of Palindrome Partitioning IV problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/PalindromePartitioning4.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1745. Palindrome Partitioning IV
+ * @link https://leetcode.com/problems/palindrome-partitioning-iv/
+ */
+interface PalindromePartitioning4 {
+    fun checkPartitioning(s: String): Boolean
+}
+
+class PalindromePartitioning4DP : PalindromePartitioning4 {
+    override fun checkPartitioning(s: String): Boolean {
+        val n = s.length
+        val arr = s.toCharArray()
+        val dp = Array(n) {
+            BooleanArray(
+                n
+            )
+        }
+        for (i in n - 1 downTo 0) {
+            for (j in i until n) {
+                if (arr[i] == arr[j]) {
+                    dp[i][j] = (if (i + 1 <= j - 1) dp[i + 1][j - 1] else true)
+                } else {
+                    dp[i][j] = false
+                }
+            }
+        }
+
+        // iterate every mid and then check: left, mid and right
+        for (i in 1 until n - 1) {
+            for (j in i until n - 1) {
+                if (dp[0][i - 1] && dp[i][j] && dp[j + 1][n - 1]) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/PalindromePartitioning4Test.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class PalindromePartitioning4Test<out T : PalindromePartitioning4>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "abcbdd",
+                true
+            ),
+            Arguments.of(
+                "bcbddxy",
+                false
+            ),
+            Arguments.of(
+                "",
+                false
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `check partitioning test`(s: String, expected: Boolean) {
+        val actual = strategy.checkPartitioning(s)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class PalindromePartitioning4DPTest : PalindromePartitioning4Test<PalindromePartitioning4>(PalindromePartitioning4DP())
