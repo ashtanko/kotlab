753530e320fddb59bc3a0883ff859ff961f4cc84
Oleksii Shtanko <shtankopro@gmail.com>
2022-03-25 17:25:53 +0200

    Solution of Maximum Level Sum of a Binary Tree problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MaximumLevelSumOfBinaryTree.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.LinkedList
+import java.util.Queue
+import java.util.stream.IntStream
+
+/**
+ * 1161. Maximum Level Sum of a Binary Tree
+ * @link https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+ */
+interface MaximumLevelSumOfBinaryTree {
+    fun maxLevelSum(root: TreeNode?): Int
+}
+
+class MaximumLevelSumOfBinaryTreeBFS : MaximumLevelSumOfBinaryTree {
+    override fun maxLevelSum(root: TreeNode?): Int {
+        var max = Int.MIN_VALUE
+        var maxLevel = 1
+        val q: Queue<TreeNode> = LinkedList()
+        q.offer(root)
+        var level = 1
+        while (!q.isEmpty()) {
+            var sum = 0
+            for (sz in q.size downTo 1) {
+                val n: TreeNode = q.poll()
+                sum += n.value
+                if (n.left != null) {
+                    q.offer(n.left)
+                }
+                if (n.right != null) {
+                    q.offer(n.right)
+                }
+            }
+            if (max < sum) {
+                max = sum
+                maxLevel = level
+            }
+            ++level
+        }
+
+        return maxLevel
+    }
+}
+
+class MaximumLevelSumOfBinaryTreeDFS : MaximumLevelSumOfBinaryTree {
+    override fun maxLevelSum(root: TreeNode?): Int {
+        val list: MutableList<Int> = ArrayList()
+        dfs(root, list, 0)
+        return 1 + IntStream.range(0, list.size).reduce(0) { a, b -> if (list[a] < list[b]) b else a }
+    }
+
+    private fun dfs(n: TreeNode?, l: MutableList<Int>, level: Int) {
+        if (n == null) {
+            return
+        }
+        if (l.size == level) {
+            l.add(n.value)
+        } // never reach this level before, add first value.
+        else {
+            l[level] = l[level] + n.value
+        } // reached the level before, accumulate current value to old value.
+        dfs(n.left, l, level + 1)
+        dfs(n.right, l, level + 1)
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MaximumLevelSumOfBinaryTreeTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MaximumLevelSumOfBinaryTreeTest<out T : MaximumLevelSumOfBinaryTree>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(1).apply {
+                    right = TreeNode(0)
+                    left = TreeNode(7).apply {
+                        left = TreeNode(7)
+                        right = TreeNode(-8)
+                    }
+                },
+                2
+            ),
+            Arguments.of(
+                TreeNode(989).apply {
+                    right = TreeNode(10250).apply {
+                        left = TreeNode(98693)
+                        right = TreeNode(-89388).apply {
+                            right = TreeNode(-32127)
+                        }
+                    }
+                },
+                2
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `max level sum test`(root: TreeNode?, expected: Int) {
+        val actual = strategy.maxLevelSum(root)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MaximumLevelSumOfBinaryTreeBFSTest :
+    MaximumLevelSumOfBinaryTreeTest<MaximumLevelSumOfBinaryTree>(MaximumLevelSumOfBinaryTreeBFS())
+
+class MaximumLevelSumOfBinaryTreeDFSTest :
+    MaximumLevelSumOfBinaryTreeTest<MaximumLevelSumOfBinaryTree>(MaximumLevelSumOfBinaryTreeDFS())
