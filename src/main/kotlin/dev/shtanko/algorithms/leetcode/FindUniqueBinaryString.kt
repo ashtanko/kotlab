7f9e6d5ca4a40ec1205b22d02d850ea315a85951
Oleksii Shtanko <shtankopro@gmail.com>
2022-03-29 22:21:50 +0300

    Solution of Find Unique Binary String problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/FindUniqueBinaryString.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+
+/**
+ * 1980. Find Unique Binary String
+ * @link https://leetcode.com/problems/find-unique-binary-string/
+ */
+interface FindUniqueBinaryString {
+    fun perform(nums: Array<String>): String
+}
+
+class FindUniqueBinaryStringImpl : FindUniqueBinaryString {
+    override fun perform(nums: Array<String>): String {
+        val ans = StringBuilder()
+        for (i in nums.indices) {
+            ans.append(if (nums[i][i] == '0') '1' else '0')
+        }
+        return ans.toString()
+    }
+}
+
+class FindUniqueBinaryStringStream : FindUniqueBinaryString {
+    override fun perform(nums: Array<String>): String {
+        return IntStream.range(0, nums.size).mapToObj { i -> if (nums[i][i] == '0') "1" else "0" }
+            .collect(Collectors.joining())
+    }
+}
+
+class FindUniqueBinaryStringOneLine : FindUniqueBinaryString {
+    override fun perform(nums: Array<String>): String {
+        return (nums.indices).joinToString("") { i -> if (nums[i][i] == '0') "1" else "0" }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/FindUniqueBinaryStringTest.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class FindUniqueBinaryStringTest<out T : FindUniqueBinaryString>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf("01", "10"),
+                "11"
+            ),
+            Arguments.of(
+                arrayOf("00", "01"),
+                "10"
+            ),
+            Arguments.of(
+                arrayOf("111", "011", "001"),
+                "000"
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `find different binary string test`(nums: Array<String>, expected: String) {
+        val actual = strategy.perform(nums)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class FindUniqueBinaryStringImplTest : FindUniqueBinaryStringTest<FindUniqueBinaryString>(FindUniqueBinaryStringImpl())
+
+class FindUniqueBinaryStringStreamTest :
+    FindUniqueBinaryStringTest<FindUniqueBinaryString>(FindUniqueBinaryStringStream())
+
+class FindUniqueBinaryStringOneLineTest :
+    FindUniqueBinaryStringTest<FindUniqueBinaryString>(FindUniqueBinaryStringOneLine())
