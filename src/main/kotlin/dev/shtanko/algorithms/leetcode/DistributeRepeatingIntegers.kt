6098fd349591d6b9840b76ab87bdfafc54fcc250
Oleksii Shtanko <shtankopro@gmail.com>
2022-03-29 22:38:30 +0300

    Solution of Distribute Repeating Integers problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DistributeRepeatingIntegers.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1655. Distribute Repeating Integers
+ * @link https://leetcode.com/problems/distribute-repeating-integers/
+ */
+interface DistributeRepeatingIntegers {
+    fun canDistribute(nums: IntArray, quantity: IntArray): Boolean
+}
+
+class DistributeRepeatingIntegersBottomUp : DistributeRepeatingIntegers {
+    override fun canDistribute(nums: IntArray, quantity: IntArray): Boolean {
+        val counts: MutableMap<Int, Int> = HashMap()
+        for (num in nums) {
+            counts[num] = counts.getOrDefault(num, 0) + 1
+        }
+
+        var idx = 0
+        val arrCounts = IntArray(counts.size)
+        for (key in counts.keys) {
+            arrCounts[idx++] = counts[key] ?: 0
+        }
+        return solve(arrCounts, quantity)
+    }
+
+    private fun solve(counts: IntArray, quantity: IntArray): Boolean {
+        counts.sort()
+        quantity.sort()
+        reverse(quantity)
+        return solve(counts, quantity, 0)
+    }
+
+    private fun reverse(arr: IntArray) {
+        var i = 0
+        while (i + i < arr.size) {
+            val tmp = arr[i]
+            arr[i] = arr[arr.size - i - 1]
+            arr[arr.size - i - 1] = tmp
+            i++
+        }
+    }
+
+    private fun solve(counts: IntArray, quantity: IntArray, idx: Int): Boolean {
+        if (idx >= quantity.size) {
+            return true
+        }
+        for (i in counts.indices) {
+            if (counts[i] >= quantity[idx]) {
+                counts[i] -= quantity[idx]
+                if (solve(counts, quantity, idx + 1)) {
+                    return true
+                }
+                counts[i] += quantity[idx]
+            }
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/DistributeRepeatingIntegersTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class DistributeRepeatingIntegersTest<out T : DistributeRepeatingIntegers>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(2),
+                false
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3, 3),
+                intArrayOf(2),
+                true
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 2, 2),
+                intArrayOf(2, 2),
+                true
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `can distribute test`(nums: IntArray, quantity: IntArray, expected: Boolean) {
+        val actual = strategy.canDistribute(nums, quantity)
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class DistributeRepeatingIntegersBottomUpTest :
+    DistributeRepeatingIntegersTest<DistributeRepeatingIntegers>(DistributeRepeatingIntegersBottomUp())
