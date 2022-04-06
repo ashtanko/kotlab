cc47cd78016e9f9c33e4ef4fc3f055e5fe11796f
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-06 19:57:11 +0300

    Solution of Minimum Time to Finish the Race problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MinimumFinishTime.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.Arrays
+import kotlin.math.min
+
+/**
+ * 2188. Minimum Time to Finish the Race
+ * @link https://leetcode.com/problems/minimum-time-to-finish-the-race/
+ */
+interface MinimumFinishTime {
+    fun perform(tires: Array<IntArray>, changeTime: Int, numLaps: Int): Int
+}
+
+class MinimumFinishTimeDP : MinimumFinishTime {
+    override fun perform(tires: Array<IntArray>, changeTime: Int, numLaps: Int): Int {
+        val minTime = IntArray(numLaps + 1)
+        Arrays.fill(minTime, Int.MAX_VALUE)
+        for (tire in tires) {
+            checkMinTime(tire, minTime, changeTime, numLaps)
+        }
+        for (i in 2..numLaps) {
+            for (j in 1 until i) {
+                val remain = i % j
+                // Greedy, in order to get the minimal runtime, we should repeat the same loop as much as possible.
+                val currMin: Int = if (remain != 0) {
+                    i / j * (minTime[j] + changeTime) + minTime[remain]
+                } else {
+                    // The last changeTime is not required if remain is 0
+                    i / j * (minTime[j] + changeTime) - changeTime
+                }
+                minTime[i] = min(minTime[i], currMin)
+            }
+        }
+
+        return minTime[numLaps]
+    }
+
+    private fun checkMinTime(tire: IntArray, minTime: IntArray, changeTime: Int, numLaps: Int) {
+        val base = tire[0]
+        var lap = 1
+        var curr = base
+        minTime[lap] = min(minTime[lap], curr)
+        var sum = base
+        // Greedy, if changeTime + base is smaller, the minimal runtime for the next lap
+        // will not be better than minTime[lap - 1] + changeTime + minTime[1]
+        while (curr * tire[1] - base <= changeTime && lap++ < numLaps) {
+            curr *= tire[1]
+            sum += curr
+            minTime[lap] = min(minTime[lap], sum)
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MinimumFinishTimeTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MinimumFinishTimeTest<out T : MinimumFinishTime>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(2, 3),
+                    intArrayOf(3, 4),
+                ),
+                5,
+                4,
+                21
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 10),
+                    intArrayOf(2, 2),
+                    intArrayOf(3, 4),
+                ),
+                6,
+                5,
+                25
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `minimum finish time test`(tires: Array<IntArray>, changeTime: Int, numLaps: Int, expected: Int) {
+        val actual = strategy.perform(tires, changeTime, numLaps)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MinimumFinishTimeDPTest : MinimumFinishTimeTest<MinimumFinishTime>(MinimumFinishTimeDP())
