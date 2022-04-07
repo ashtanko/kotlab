4fc496762c630f0dfd51c191fc8324f61ce65fa2
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-07 19:17:56 +0300

    Solution of Frequency of the Most Frequent Element problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MaxFrequency.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 1838. Frequency of the Most Frequent Element
+ * @link https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+ */
+interface MaxFrequency {
+    fun maxFrequency(nums: IntArray, k: Int): Int
+}
+
+class MaxFrequencySlidingWindow : MaxFrequency {
+    override fun maxFrequency(nums: IntArray, k: Int): Int {
+        val n = nums.size
+        nums.sort()
+        var ans = 0
+        var left = 0
+
+        var sum: Long = 0
+
+        for (right in 0 until n) {
+            sum += nums[right]
+            while (nums[right] * 1L * (right - left + 1) - sum > k) {
+                sum -= nums[left]
+                left++
+            }
+            ans = max(ans, right - left + 1)
+        }
+
+        return ans
+    }
+}
