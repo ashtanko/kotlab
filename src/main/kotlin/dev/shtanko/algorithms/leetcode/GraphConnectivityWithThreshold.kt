f07e0826474cde6abeb735c78a72c6913c767dfa
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-09 21:32:07 +0300

    Solution of Graph Connectivity With Threshold problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/GraphConnectivityWithThreshold.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1627. Graph Connectivity With Threshold
+ * @link https://leetcode.com/problems/graph-connectivity-with-threshold/
+ */
+interface GraphConnectivityWithThreshold {
+    fun areConnected(n: Int, threshold: Int, queries: Array<IntArray>): List<Boolean>
+}
+
+class GraphConnectivityWithThresholdUnion : GraphConnectivityWithThreshold {
+    override fun areConnected(n: Int, threshold: Int, queries: Array<IntArray>): List<Boolean> {
+        val uf = UnionFind(n + 1)
+        for (z in threshold + 1..n) {
+            var x = z * 2
+            while (x <= n) {
+                uf.union(z, x)
+                x += z
+            }
+        }
+
+        val ans: MutableList<Boolean> = ArrayList()
+        for (q in queries) {
+            ans.add(uf.find(q[0]) == uf.find(q[1]))
+        }
+        return ans
+    }
+
+    // Feel free to copy this class for later reuse!
+    internal class UnionFind(n: Int) {
+        var parent: IntArray
+        var size: IntArray
+
+        init {
+            parent = IntArray(n)
+            size = IntArray(n)
+            for (i in 0 until n) {
+                parent[i] = i
+                size[i] = 1
+            }
+        }
+
+        fun find(x: Int): Int {
+            return if (x == parent[x]) x else find(parent[x]).also {
+                parent[x] = it // Path compression
+            }
+        }
+
+        fun union(u: Int, v: Int): Boolean {
+            val pu = find(u)
+            val pv = find(v)
+            if (pu == pv) return false
+            if (size[pu] > size[pv]) { // Union by larger size
+                size[pu] += size[pv]
+                parent[pv] = pu
+            } else {
+                size[pv] += size[pu]
+                parent[pu] = pv
+            }
+            return true
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/GraphConnectivityWithThresholdTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class GraphConnectivityWithThresholdTest<out T : GraphConnectivityWithThreshold>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                6,
+                2,
+                arrayOf(
+                    intArrayOf(1, 4),
+                    intArrayOf(2, 5),
+                    intArrayOf(3, 6)
+                ),
+                listOf(false, false, true)
+            ),
+            Arguments.of(
+                6,
+                0,
+                arrayOf(
+                    intArrayOf(4, 5),
+                    intArrayOf(3, 4),
+                    intArrayOf(3, 2),
+                    intArrayOf(2, 6),
+                    intArrayOf(1, 3),
+                ),
+                listOf(true, true, true, true, true)
+            ),
+            Arguments.of(
+                5,
+                1,
+                arrayOf(
+                    intArrayOf(4, 5),
+                    intArrayOf(4, 5),
+                    intArrayOf(3, 2),
+                    intArrayOf(2, 3),
+                    intArrayOf(3, 4),
+                ),
+                listOf(false, false, false, false, false)
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `are connected test`(n: Int, threshold: Int, queries: Array<IntArray>, expected: List<Boolean>) {
+        val actual = strategy.areConnected(n, threshold, queries)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class GraphConnectivityWithThresholdUnionTest : GraphConnectivityWithThresholdTest<GraphConnectivityWithThreshold>(
+    GraphConnectivityWithThresholdUnion()
+)
