7d67d59a2b7a5e2f9411a6114600f022c9a30e54
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-09 21:07:47 +0300

    Solution of Smallest Sufficient Team problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/SmallestSufficientTeam.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1125. Smallest Sufficient Team
+ * @link https://leetcode.com/problems/smallest-sufficient-team/
+ */
+interface SmallestSufficientTeam {
+    fun perform(reqSkills: Array<String>, people: List<List<String>>): IntArray
+}
+
+class SmallestSufficientTeamDFS : SmallestSufficientTeam {
+    private var sol: MutableList<Int> = ArrayList()
+
+    override fun perform(reqSkills: Array<String>, people: List<List<String>>): IntArray {
+        val idx: MutableMap<String, Int> = HashMap()
+        var n = 0
+        for (s in reqSkills) idx[s] = n++ // skills are represented by 0, 1, 2....
+
+        val pe = IntArray(people.size)
+        for (i in pe.indices) {
+            for (p in people[i]) {
+                val skill = idx[p]!!
+                pe[i] += 1 shl skill
+            }
+        } // each person is transferred to a number, of which the bits of 1 means the guy has the skill
+
+        search(0, pe, ArrayList(), n)
+        val ans = IntArray(sol.size)
+        for (i in sol.indices) ans[i] = sol[i]
+        return ans
+    }
+
+    fun search(cur: Int, pe: IntArray, onesol: MutableList<Int>, n: Int) {
+        // when all bits are 1, all skills are covered
+        if (cur == (1 shl n) - 1) {
+            if (sol.size == 0 || onesol.size < sol.size) {
+                sol = ArrayList(onesol)
+            }
+            return
+        }
+        if (sol.size != 0 && onesol.size >= sol.size) {
+            return
+        }
+        var zeroBit = 0
+        while (cur shr zeroBit and 1 == 1) zeroBit++
+        for (i in pe.indices) {
+            val per = pe[i]
+            if (per shr zeroBit and 1 == 1) {
+                onesol.add(i) // when a person can cover a zero bit in the current number, we can add him
+                search(cur or per, pe, onesol, n)
+                onesol.removeAt(onesol.size - 1) // search in a backtracking way
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/SmallestSufficientTeamTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class SmallestSufficientTeamTest<out T : SmallestSufficientTeam>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf("java", "nodejs", "reactjs"),
+                listOf(
+                    listOf("java"),
+                    listOf("nodejs"),
+                    listOf("nodejs", "reactjs"),
+                ),
+                intArrayOf(0, 2)
+            ),
+            Arguments.of(
+                arrayOf("algorithms", "math", "java", "reactjs", "csharp", "aws"),
+                listOf(
+                    listOf("algorithms", "math", "java"),
+                    listOf("algorithms", "math", "reactjs"),
+                    listOf("java", "csharp", "aws"),
+                    listOf("reactjs", "csharp"),
+                    listOf("csharp", "math"),
+                    listOf("aws", "java")
+                ),
+                intArrayOf(1, 2)
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `smallest sufficient team test`(reqSkills: Array<String>, people: List<List<String>>, expected: IntArray) {
+        val actual = strategy.perform(reqSkills, people)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class SmallestSufficientTeamDFSTest : SmallestSufficientTeamTest<SmallestSufficientTeam>(SmallestSufficientTeamDFS())
