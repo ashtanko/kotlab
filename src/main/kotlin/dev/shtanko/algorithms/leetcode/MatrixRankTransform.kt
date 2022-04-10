59b1bc226ea70cb5919f7286d00682fcb9c86c51
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-10 13:57:28 +0300

    Solution of Rank Transform of a Matrix problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MatrixRankTransform.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.TreeMap
+import kotlin.math.max
+
+/**
+ * 1632. Rank Transform of a Matrix
+ * @link https://leetcode.com/problems/rank-transform-of-a-matrix/
+ */
+interface MatrixRankTransform {
+    fun perform(matrix: Array<IntArray>): Array<IntArray>
+}
+
+class MatrixRankTransformMap : MatrixRankTransform {
+    override fun perform(matrix: Array<IntArray>): Array<IntArray> {
+        val m: Int = matrix.size
+        val n: Int = matrix[0].size
+        val map = TreeMap<Int, MutableList<IntArray>>()
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                map.putIfAbsent(matrix[i][j], ArrayList())
+                map[matrix[i][j]]?.add(intArrayOf(i, j))
+            }
+        }
+
+        val rank = IntArray(m + n)
+        for ((_, value1) in map) {
+            val parentMap = HashMap<Int, Int>()
+            for (pair in value1) {
+                union(parentMap, pair[0], pair[1] + m)
+            }
+            val groups = HashMap<Int, MutableList<Int>>()
+            for (value in parentMap.keys) {
+                val parent = find(parentMap, value)
+                groups.putIfAbsent(parent, ArrayList())
+                groups[parent]?.add(value)
+            }
+            for ((_, v) in groups) {
+                var maxRank = 0
+                for (value in v) {
+                    maxRank = max(maxRank, rank[value])
+                }
+                for (value in v) {
+                    rank[value] = maxRank + 1
+                }
+            }
+            for (pair in value1) {
+                matrix[pair[0]][pair[1]] = rank[pair[0]]
+            }
+        }
+
+        return matrix
+    }
+
+    private fun find(parentMap: HashMap<Int, Int>, value: Int): Int {
+        if (parentMap[value] == value) {
+            return value
+        }
+        parentMap[value] = find(parentMap, parentMap.getOrDefault(value, 0))
+        return parentMap.getOrDefault(value, 0)
+    }
+
+    private fun union(parentMap: HashMap<Int, Int>, u: Int, v: Int) {
+        if (!parentMap.containsKey(u)) {
+            parentMap[u] = u
+        }
+        if (!parentMap.containsKey(v)) {
+            parentMap[v] = v
+        }
+        val pu = find(parentMap, u)
+        val pv = find(parentMap, v)
+        if (pu != pv) {
+            parentMap[pu] = pv
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MatrixRankTransformTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MatrixRankTransformTest<out T : MatrixRankTransform>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 2),
+                    intArrayOf(3, 4),
+                ),
+                arrayOf(
+                    intArrayOf(1, 2),
+                    intArrayOf(2, 3),
+                )
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(7, 7),
+                    intArrayOf(7, 7),
+                ),
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(1, 1),
+                )
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(20, -21, 14),
+                    intArrayOf(-19, 4, 19),
+                    intArrayOf(22, -47, 24),
+                    intArrayOf(-19, 4, 19),
+                ),
+                arrayOf(
+                    intArrayOf(4, 2, 3),
+                    intArrayOf(1, 3, 4),
+                    intArrayOf(5, 1, 6),
+                    intArrayOf(1, 3, 4)
+                )
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `matrix rank transform test`(matrix: Array<IntArray>, expected: Array<IntArray>) {
+        val actual = strategy.perform(matrix)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MatrixRankTransformMapTest : MatrixRankTransformTest<MatrixRankTransform>(MatrixRankTransformMap())
