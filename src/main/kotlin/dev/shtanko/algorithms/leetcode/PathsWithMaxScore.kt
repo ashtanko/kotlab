4bd61101546c7fa48394ec97db105abf883b9937
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-10 13:46:47 +0300

    Solution of Number of Paths with Max Score problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/PathsWithMaxScore.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1301. Number of Paths with Max Score
+ * @link https://leetcode.com/problems/number-of-paths-with-max-score/
+ */
+interface PathsWithMaxScore {
+    fun perform(board: List<String>): IntArray
+}
+
+class PathsWithMaxScoreDP : PathsWithMaxScore {
+    private val dirs = arrayOf(intArrayOf(0, -1), intArrayOf(-1, 0), intArrayOf(-1, -1))
+
+    override fun perform(board: List<String>): IntArray {
+        val m: Int = board.size
+        val n: Int = board[0].length
+        val dpSum = Array(m) { IntArray(n) }
+        val dpCnt = Array(m) { IntArray(n) }
+        dpCnt[m - 1][n - 1] = 1 // start at the bottom right square
+
+        for (r in m - 1 downTo 0) {
+            for (c in n - 1 downTo 0) {
+                if (dpCnt[r][c] == 0) {
+                    continue
+                }
+                for (dir in dirs) {
+                    val nr = r + dir[0]
+                    val nc = c + dir[1]
+                    if (nr >= 0 && nc >= 0 && board[nr][nc] != 'X') {
+                        var nsum = dpSum[r][c]
+                        if (board[nr][nc] != 'E') nsum += board[nr][nc] - '0'
+                        if (nsum > dpSum[nr][nc]) {
+                            dpCnt[nr][nc] = dpCnt[r][c]
+                            dpSum[nr][nc] = nsum
+                        } else if (nsum == dpSum[nr][nc]) {
+                            dpCnt[nr][nc] = (dpCnt[nr][nc] + dpCnt[r][c]) % MODULO
+                        }
+                    }
+                }
+            }
+        }
+        return intArrayOf(dpSum[0][0], dpCnt[0][0])
+    }
+
+    companion object {
+        private const val MODULO = 1000000007
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/PathsWithMaxScoreTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class PathsWithMaxScoreTest<out T : PathsWithMaxScore>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                listOf("E23", "2X2", "12S"),
+                intArrayOf(7, 1)
+            ),
+            Arguments.of(
+                listOf("E12", "1X1", "21S"),
+                intArrayOf(4, 2)
+            ),
+            Arguments.of(
+                listOf("E11", "XXX", "11S"),
+                intArrayOf(0, 0)
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `paths with max score test`(board: List<String>, expected: IntArray) {
+        val actual = strategy.perform(board)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class PathsWithMaxScoreDPTest : PathsWithMaxScoreTest<PathsWithMaxScore>(PathsWithMaxScoreDP())
