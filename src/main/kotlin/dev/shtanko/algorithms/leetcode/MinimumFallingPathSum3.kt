ca768d5e8fe27a8de5c9b4891bedba43975f6101
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-10 12:44:48 +0300

    Solution of Minimum Falling Path Sum II problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MinimumFallingPathSum3.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.min
+
+/**
+ * 1289. Minimum Falling Path Sum II
+ * @link https://leetcode.com/problems/minimum-falling-path-sum-ii/
+ */
+interface MinimumFallingPathSum3 {
+    fun minFallingPathSum(grid: Array<IntArray>): Int
+}
+
+class MinimumFallingPathSum3DP : MinimumFallingPathSum3 {
+    override fun minFallingPathSum(grid: Array<IntArray>): Int {
+        val n: Int = grid.size
+        val m: Int = grid[0].size
+        val dp = Array(n) { IntArray(m) }
+        var min = Int.MAX_VALUE
+        var minIndex = -1
+        var secondMin = Int.MAX_VALUE
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                dp[i][j] = Int.MAX_VALUE
+                if (i == 0) {
+                    dp[i][j] = grid[i][j]
+                } else {
+                    if (j == minIndex) {
+                        dp[i][j] = min(secondMin + grid[i][j], dp[i][j])
+                    } else {
+                        dp[i][j] = min(min + grid[i][j], dp[i][j])
+                    }
+                }
+            }
+            min = Int.MAX_VALUE
+            minIndex = -1
+            secondMin = Int.MAX_VALUE
+            for (j in 0 until m) {
+                if (min >= dp[i][j]) {
+                    secondMin = min
+                    min = dp[i][j]
+                    minIndex = j
+                } else if (secondMin > dp[i][j]) {
+                    secondMin = dp[i][j]
+                }
+            }
+        }
+        return min
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MinimumFallingPathSum3Test.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MinimumFallingPathSum3Test<out T : MinimumFallingPathSum3>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 2, 3),
+                    intArrayOf(4, 5, 6),
+                    intArrayOf(7, 8, 9),
+                ),
+                13
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(7)
+                ),
+                7
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `min falling path sum test`(grid: Array<IntArray>, expected: Int) {
+        val actual = strategy.minFallingPathSum(grid)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MinimumFallingPathSum3DPTest : MinimumFallingPathSum3Test<MinimumFallingPathSum3>(MinimumFallingPathSum3DP())
