680caaba7febe5d59fac1162dd18f10cce23aa97
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-13 19:28:22 +0300

    Add higher order function examples

---
+++ src/test/kotlin/dev/shtanko/kotlinlang/functions/higherorder/FoldTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.kotlinlang.functions.higherorder
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+class FoldTest {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                listOf(1, 2, 3, 4, 5),
+                0,
+                15
+            ),
+            Arguments.of(
+                listOf(1, 2, 3, 4, 5),
+                1,
+                16
+            ),
+            Arguments.of(
+                listOf(1, 2, 3, 4, 5),
+                2,
+                17
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `fold test`(items: List<Int>, initial: Int, expected: Int) {
+        val actual = items.fold(initial) { acc: Int, i: Int ->
+            val result = acc + i
+            result
+        }
+        assertThat(actual).isEqualTo(expected)
+    }
+}
