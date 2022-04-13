d8c919616997af419966386efea9ad19643b36ef
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-13 19:27:10 +0300

    Small refactor

--- src/main/kotlin/dev/shtanko/algorithms/dp/FibonacciSequence.kt
+++ src/main/kotlin/dev/shtanko/algorithms/dp/FibonacciSequence.kt
@@ -30,5 +30,5 @@ fun fibonacciAt(n: Int) = run {
 fun fibonacci(): Sequence<Int> {
     // fibonacci terms
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, ...
-    return generateSequence(Pair(0, 1), { it.second to it.first + it.second }).map { it.first }
+    return generateSequence(Pair(0, 1)) { it.second to it.first + it.second }.map { it.first }
 }
