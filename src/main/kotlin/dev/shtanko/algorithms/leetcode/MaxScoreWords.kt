e12012a554cd76255ec4f32bd2f34c13127310a3
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-13 19:26:18 +0300

    Solution of Maximum Score Words Formed by Letters problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MaxScoreWords.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 1255. Maximum Score Words Formed by Letters
+ * @link https://leetcode.com/problems/maximum-score-words-formed-by-letters/
+ */
+interface MaxScoreWords {
+    fun perform(words: Array<String>, letters: CharArray, score: IntArray): Int
+}
+
+class MaxScoreWordsBacktracking : MaxScoreWords {
+    override fun perform(words: Array<String>, letters: CharArray, score: IntArray): Int {
+        if (words.isEmpty() || letters.isEmpty() || score.isEmpty()) return 0
+        val count = IntArray(score.size)
+        for (ch in letters) {
+            count[ch.code - 'a'.code]++
+        }
+        return backtrack(words, count, score, 0)
+    }
+
+    private fun backtrack(words: Array<String>, count: IntArray, score: IntArray, index: Int): Int {
+        var max = 0
+        for (i in index until words.size) {
+            var res = 0
+            var isValid = true
+            for (ch in words[i].toCharArray()) {
+                count[ch.code - 'a'.code]--
+                res += score[ch.code - 'a'.code]
+                if (count[ch.code - 'a'.code] < 0) isValid = false
+            }
+            if (isValid) {
+                res += backtrack(words, count, score, i + 1)
+                max = max(res, max)
+            }
+            for (ch in words[i].toCharArray()) {
+                count[ch.code - 'a'.code]++
+            }
+        }
+        return max
+    }
+}
+
+class MaxScoreWordsDFS : MaxScoreWords {
+    override fun perform(words: Array<String>, letters: CharArray, score: IntArray): Int {
+        val memo = IntArray(ARR_SIZE)
+        for (l in letters) {
+            memo[l.code - 'a'.code]++
+        }
+        return dfs(0, memo, score, words)
+    }
+
+    fun dfs(index: Int, memo: IntArray, score: IntArray, words: Array<String>): Int {
+        if (index == words.size) {
+            return 0
+        }
+        var res = 0
+        val w = words[index]
+        var count = 0
+        var i = 0
+        val temp: IntArray = memo.copyOf(memo.size)
+        while (i < w.length) {
+            val c = w[i]
+            count += if (temp[c.code - 'a'.code] > 0) {
+                temp[c.code - 'a'.code]--
+                score[c.code - 'a'.code]
+            } else {
+                break
+            }
+            i++
+        }
+        if (i == w.length) {
+            res = max(res, count + dfs(index + 1, temp, score, words))
+        }
+        res = max(dfs(index + 1, memo, score, words), res)
+        return res
+    }
+
+    companion object {
+        private const val ARR_SIZE = 26
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MaxScoreWordsTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 Alexey Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MaxScoreWordsTest<out T : MaxScoreWords>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf("dog", "cat", "dad", "good"),
+                charArrayOf('a', 'a', 'c', 'd', 'd', 'd', 'g', 'o', 'o'),
+                intArrayOf(1, 0, 9, 5, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                23
+            ),
+            Arguments.of(
+                arrayOf("xxxz", "ax", "bx", "cx"),
+                charArrayOf('z', 'a', 'b', 'c', 'x', 'x', 'x'),
+                intArrayOf(4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10),
+                27
+            ),
+            Arguments.of(
+                arrayOf("leetcode"),
+                charArrayOf('l', 'e', 't', 'c', 'o', 'd'),
+                intArrayOf(0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0),
+                0
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `max score words test`(words: Array<String>, letters: CharArray, score: IntArray, expected: Int) {
+        val actual = strategy.perform(words, letters, score)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MaxScoreWordsBacktrackingTest : MaxScoreWordsTest<MaxScoreWords>(MaxScoreWordsBacktracking())
+class MaxScoreWordsDFSTest : MaxScoreWordsTest<MaxScoreWords>(MaxScoreWordsDFS())
