17d02c2a36ec38b017cbc853398287103120d573
Oleksii Shtanko <shtankopro@gmail.com>
2022-04-28 16:56:04 +0300

    Solution of Count Subtrees With Max Distance Between Cities problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CountSubgraphsForEachDiameter.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 1617. Count Subtrees With Max Distance Between Cities
+ * @link https://leetcode.com/problems/count-subtrees-with-max-distance-between-cities/
+ */
+interface CountSubgraphsForEachDiameter {
+    fun perform(n: Int, edges: Array<IntArray>): IntArray
+}
+
+class CountSubgraphsForEachDiameterDFS : CountSubgraphsForEachDiameter {
+
+    private var max = 0
+
+    override fun perform(n: Int, edges: Array<IntArray>): IntArray {
+        val graph: MutableMap<Int, MutableSet<Int>> = HashMap()
+        for (edge in edges) {
+            graph.computeIfAbsent(
+                edge[0]
+            ) { HashSet() }.add(edge[1])
+            graph.computeIfAbsent(
+                edge[1]
+            ) { HashSet() }.add(edge[0])
+        }
+        val ans = IntArray(n - 1)
+        for (bit in 0 until (1 shl n)) {
+            val subtree: MutableSet<Int?> = HashSet()
+            for (i in 0 until n) if (bit and (1 shl i) != 0) subtree.add(i + 1)
+            var edgeCount = 0
+            max = 0
+            for (edge in edges) {
+                if (subtree.contains(edge[0]) && subtree.contains(edge[1])) edgeCount++
+            }
+            if (edgeCount == 0 || edgeCount != subtree.size - 1) continue
+            dfs(subtree, graph, HashSet(), subtree.iterator().next())
+            ans[max - 1]++
+        }
+        return ans
+    }
+
+    private fun dfs(subtree: Set<Int?>, graph: Map<Int, Set<Int>>, visited: MutableSet<Int>, beg: Int?): Int {
+        var dist1 = 0
+        var dist2 = 0
+        beg?.let { visited.add(it) }
+        for (neighbor in graph[beg] ?: emptySet()) {
+            if (!visited.contains(neighbor) && subtree.contains(neighbor)) {
+                val d = dfs(subtree, graph, visited, neighbor)
+                if (d > dist1) {
+                    dist2 = dist1
+                    dist1 = d
+                } else if (d > dist2) dist2 = d
+            }
+        }
+        max = max(max, dist1 + dist2)
+        return 1 + dist1
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/CountSubgraphsForEachDiameterTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class CountSubgraphsForEachDiameterTest<out T : CountSubgraphsForEachDiameter>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                4,
+                arrayOf(
+                    intArrayOf(1, 2),
+                    intArrayOf(2, 3),
+                    intArrayOf(2, 4),
+                ),
+                intArrayOf(3, 4, 0)
+            ),
+            Arguments.of(
+                2,
+                arrayOf(
+                    intArrayOf(1, 2),
+                ),
+                intArrayOf(1)
+            ),
+            Arguments.of(
+                3,
+                arrayOf(
+                    intArrayOf(1, 2),
+                    intArrayOf(2, 3),
+                ),
+                intArrayOf(2, 1)
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `count subgraphs for each diameter test`(n: Int, edges: Array<IntArray>, expected: IntArray) {
+        val actual = strategy.perform(n, edges)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class CountSubgraphsForEachDiameterDFSTest :
+    CountSubgraphsForEachDiameterTest<CountSubgraphsForEachDiameter>(CountSubgraphsForEachDiameterDFS())
