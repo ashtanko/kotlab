bbba850e8cf4047d7f5d2984b692a35af5417b2e
Oleksii Shtanko <shtankopro@gmail.com>
2022-06-10 09:34:29 +0300

    Solution of 1553. Minimum Number of Days to Eat N Oranges problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ParallelCourses2.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.min
+
+/**
+ * 1494. Parallel Courses II
+ * @link https://leetcode.com/problems/parallel-courses-ii/
+ */
+interface ParallelCourses2 {
+    fun minNumberOfSemesters(n: Int, relations: Array<IntArray>, k: Int): Int
+}
+
+class ParallelCourses2DP : ParallelCourses2 {
+
+    override fun minNumberOfSemesters(n: Int, relations: Array<IntArray>, k: Int): Int {
+        val pres = IntArray(n)
+        for (r in relations) {
+            val prev = r[0] - 1
+            val next = r[1] - 1
+            pres[next] = pres[next] or (1 shl prev)
+        }
+        val dp = IntArray(1 shl n) { n }
+        dp[0] = 0
+        for (mask in dp.indices) {
+            var canTake = 0
+            for (i in 0 until n) {
+                // already taken
+                if (mask and (1 shl i) != 0) {
+                    continue
+                }
+                // satisfy all pres
+                if (mask and pres[i] == pres[i]) {
+                    canTake = canTake or (1 shl i)
+                }
+            }
+
+            // loop each sub-masks
+            var take = canTake
+            while (take > 0) {
+                if (Integer.bitCount(take) > k) {
+                    take = take - 1 and canTake
+                    continue
+                }
+                dp[take or mask] = min(dp[take or mask], dp[mask] + 1)
+                take = take - 1 and canTake
+            }
+        }
+        return dp[(1 shl n) - 1]
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/ParallelCourses2Test.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class ParallelCourses2Test<out T : ParallelCourses2>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                4,
+                arrayOf(
+                    intArrayOf(2, 1),
+                    intArrayOf(3, 1),
+                    intArrayOf(1, 4),
+                ),
+                2,
+                3
+            ),
+            Arguments.of(
+                5,
+                arrayOf(
+                    intArrayOf(2, 1),
+                    intArrayOf(3, 1),
+                    intArrayOf(4, 1),
+                    intArrayOf(1, 5),
+                ),
+                2,
+                4
+            ),
+            Arguments.of(
+                11,
+                arrayOf<IntArray>(),
+                2,
+                6
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `min number of semesters test`(n: Int, relations: Array<IntArray>, k: Int, expected: Int) {
+        val actual = strategy.minNumberOfSemesters(n, relations, k)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class ParallelCourses2DPTest : ParallelCourses2Test<ParallelCourses2>(ParallelCourses2DP())
