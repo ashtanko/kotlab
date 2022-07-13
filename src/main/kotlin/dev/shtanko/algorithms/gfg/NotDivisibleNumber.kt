b1032012bbc631902cc41267e6b5ab7d90ec191c
Oleksii Shtanko <shtankopro@gmail.com>
2022-07-13 14:41:38 +0300

    Solution of Number that are not divisible problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/gfg/NotDivisibleNumber.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.gfg
+
+/**
+ * Number that are not divisible
+ */
+interface NotDivisibleNumber {
+    fun perform(n: Long): Long
+}
+
+class NotDivisibleNumberBruteForce : NotDivisibleNumber {
+    override fun perform(n: Long): Long {
+
+        val primes = prime.sumOf { n / it }
+        val a = numbersA.sumOf { n / it }
+        val b = numbersB.sumOf { n / it } - n / LAST
+        return n - (primes - (a) + b)
+    }
+
+    companion object {
+        private val prime = listOf(2, 3, 5, 7)
+        private val numbersA = listOf(6, 10, 14, 15, 21, 35)
+        private val numbersB = listOf(30, 105, 70, 42)
+        private const val LAST = 210L
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/gfg/NotDivisibleNumberTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.gfg
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class NotDivisibleNumberTest<out T : NotDivisibleNumber>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(11L, 2L),
+            Arguments.of(2L, 1L),
+            Arguments.of(1245L, 285L),
+            Arguments.of(0L, 0L),
+            Arguments.of(1L, 1L),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `noOfNumbers test`(n: Long, expected: Long) {
+        val actual = strategy.perform(n)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class NotDivisibleNumberBruteForceTest : NotDivisibleNumberTest<NotDivisibleNumber>(NotDivisibleNumberBruteForce())
