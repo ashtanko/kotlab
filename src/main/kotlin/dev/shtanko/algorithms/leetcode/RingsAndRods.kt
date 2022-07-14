2a45c191b3ee367805aa0840a4fb5fe58e8d8492
Oleksii Shtanko <shtankopro@gmail.com>
2022-07-14 14:40:41 +0300

    Solution of Rings and Rods problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/RingsAndRods.kt
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 2103. Rings and Rods
+ * link https://leetcode.com/problems/rings-and-rods/
+ */
+interface RingsAndRods {
+    fun countPoints(rings: String): Int
+}
+
+class RingsAndRodsBF : RingsAndRods {
+    override fun countPoints(rings: String): Int {
+
+        val map = HashMap<String, MutableList<String>>()
+        rings.chunked(2).forEach {
+            val a = it[1].toString()
+            val b = it.first().toString()
+            if (map[a] == null) {
+                map[a] = mutableListOf()
+                map[a]?.add(b)
+            } else {
+                map[a]?.add(b)
+            }
+        }
+        var i = 0
+        map.forEach { (_, u) ->
+            if (u.containsAll(listOf("R", "G", "B"))) {
+                i++
+            }
+        }
+        return i
+    }
+}
+
+class RingsAndRodsBitmask : RingsAndRods {
+    override fun countPoints(rings: String): Int {
+        val rods = IntArray(10)
+        var i = 0
+        while (i < rings.length - 1) {
+            if (rings[i] == 'R') {
+                rods[rings[i + 1] - '0'] = rods[rings[i + 1] - '0'] or (1 shl 0)
+            }
+            if (rings[i] == 'G') {
+                rods[rings[i + 1] - '0'] = rods[rings[i + 1] - '0'] or (1 shl 1)
+            }
+            if (rings[i] == 'B') {
+                rods[rings[i + 1] - '0'] = rods[rings[i + 1] - '0'] or (1 shl 2)
+            }
+            i += 2
+        }
+        var total = 0
+        for (i0 in rods.indices) {
+            if (rods[i0] == 7) total++
+        }
+        return total
+    }
+}
+
+class RingsAndRodsArray : RingsAndRods {
+    class Ring {
+        var hasRead = false
+        var hasGreen = false
+        var hasBlue = false
+    }
+
+    override fun countPoints(rings: String): Int {
+        // Initialize
+        val arr = arrayOfNulls<Ring>(10)
+        for (i in 0..9) {
+            arr[i] = Ring()
+        }
+
+        // Check every pair of chars
+        var index = 0
+        val size: Int = rings.length
+        while (index < size) {
+            val target: Int = java.lang.String.valueOf(rings[index + 1]).toInt()
+            when (rings[index]) {
+                'R' -> {
+                    arr[target]?.hasRead = true
+                }
+
+                'G' -> {
+                    arr[target]?.hasGreen = true
+                }
+
+                else -> {
+                    arr[target]?.hasBlue = true
+                }
+            }
+            index += 2
+        }
+
+        // Count them
+        var count = 0
+        for (i in 0..9) {
+            if (arr[i]?.hasRead == true && arr[i]?.hasGreen == true && arr[i]?.hasBlue == true) {
+                count++
+            }
+        }
+        return count
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/RingsAndRodsTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class RingsAndRodsTest<out T : RingsAndRods>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "B0B6G0R6R0R6G9",
+                1
+            ),
+            Arguments.of(
+                "B0R0G0R9R0B0G0",
+                1
+            ),
+            Arguments.of(
+                "G4",
+                0
+            ),
+            Arguments.of(
+                "R0G0B0R1G1B1",
+                2
+            ),
+            Arguments.of(
+                "R0G0B0R1G1B1R2G2B2",
+                3
+            ),
+            Arguments.of(
+                "R0G0B0R1G1B1R2G2B2R3G3B3",
+                4
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `count points test`(rings: String, expected: Int) {
+        val actual = strategy.countPoints(rings)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class RingsAndRodsBFTest : RingsAndRodsTest<RingsAndRods>(RingsAndRodsBF())
+class RingsAndRodsBitmaskTest : RingsAndRodsTest<RingsAndRods>(RingsAndRodsBitmask())
+class RingsAndRodsArrayTest : RingsAndRodsTest<RingsAndRods>(RingsAndRodsArray())
