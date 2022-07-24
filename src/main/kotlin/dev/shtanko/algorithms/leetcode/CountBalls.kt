fac1eda5730f7ed6c90965d7dee5fabfc60c53d6
Oleksii Shtanko <shtankopro@gmail.com>
2022-07-24 14:47:11 +0300

    Solution of Maximum Number of Balls in a Box problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CountBalls.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 1742. Maximum Number of Balls in a Box
+ * link https://leetcode.com/problems/maximum-number-of-balls-in-a-box/
+ */
+interface CountBalls {
+    fun perform(lowLimit: Int, highLimit: Int): Int
+
+    companion object {
+        const val ARR_SIZE = 46
+    }
+}
+
+class CountBallsBruteforce : CountBalls {
+    override fun perform(lowLimit: Int, highLimit: Int): Int {
+        val cnt = IntArray(CountBalls.ARR_SIZE)
+        var max = 0
+        for (i in lowLimit..highLimit) {
+            var num = i
+            var sum = 0
+            while (num > 0) {
+                sum += num % DECIMAL
+                num /= DECIMAL
+            }
+            max = max(++cnt[sum], max)
+        }
+        return max
+    }
+}
+
+class CountBalls2 : CountBalls {
+    override fun perform(lowLimit: Int, highLimit: Int): Int {
+        val box = IntArray(CountBalls.ARR_SIZE)
+        var lo = lowLimit
+        var id = 0
+        while (lo > 0) { // compute box id for lowLimit.
+            id += lo % DECIMAL
+            lo /= DECIMAL
+        }
+        ++box[id]
+        for (i in lowLimit + 1..highLimit) { // compute all other box ids.
+            var digits = i
+            while (digits % DECIMAL == 0) { // for ball numbers with trailing 0's, decrease 9 for each 0.
+                id -= 9
+                digits /= DECIMAL
+            }
+            ++box[++id]
+        }
+        return box.max()
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/CountBallsTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class CountBallsTest<out T : CountBalls>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                1,
+                10,
+                2
+            ),
+            Arguments.of(
+                5,
+                15,
+                2
+            ),
+            Arguments.of(
+                19,
+                28,
+                2
+            ),
+        )
+    }
+
+    @ArgumentsSource(InputArgumentsProvider::class)
+    @ParameterizedTest
+    fun `count balls test`(lowLimit: Int, highLimit: Int, expected: Int) {
+        val actual = strategy.perform(lowLimit, highLimit)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class CountBallsBruteforceTest : CountBallsTest<CountBalls>(CountBallsBruteforce())
+class CountBalls2Test : CountBallsTest<CountBalls>(CountBalls2())
