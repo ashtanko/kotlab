51163d37a0d0d8a2ea9ae3c8ede69758e2a9c95d
Oleksii Shtanko <shtankopro@gmail.com>
2022-07-25 23:11:04 +0300

    Solution of Keep Multiplying Found Values by Two problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/FindFinalValue.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 2154. Keep Multiplying Found Values by Two
+ * link https://leetcode.com/problems/keep-multiplying-found-values-by-two/
+ */
+interface FindFinalValue {
+    fun perform(nums: IntArray, original: Int): Int
+}
+
+class FindFinalValueHashMap : FindFinalValue {
+    override fun perform(nums: IntArray, original: Int): Int {
+        var o = original
+        val set = HashSet<Int>()
+        for (i in nums) if (i >= o) set.add(i)
+        while (true) o *= if (set.contains(o)) 2 else break
+        return o
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/FindFinalValueTest.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class FindFinalValueTest<out T : FindFinalValue>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(5, 3, 6, 1, 12),
+                3,
+                24
+            ),
+            Arguments.of(
+                intArrayOf(2, 7, 9),
+                4,
+                4
+            ),
+            Arguments.of(
+                intArrayOf(),
+                0,
+                0
+            ),
+            Arguments.of(
+                intArrayOf(1),
+                1,
+                2
+            ),
+            Arguments.of(
+                intArrayOf(1, 2),
+                2,
+                4
+            ),
+            Arguments.of(
+                intArrayOf(12, 24),
+                12,
+                48
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `find final value test`(nums: IntArray, original: Int, expected: Int) {
+        val actual = strategy.perform(nums, original)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class FindFinalValueHashMapTest : FindFinalValueTest<FindFinalValue>(FindFinalValueHashMap())
