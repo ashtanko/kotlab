9206cc96fdfcdda67b4751b8b05a8c92e710dadb
Oleksii Shtanko <shtankopro@gmail.com>
2022-08-06 15:14:52 +0300

    Solution of Kth Distinct String in an Array problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/KTHDistinct.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 2053. Kth Distinct String in an Array
+ * link https://leetcode.com/problems/kth-distinct-string-in-an-array/
+ */
+interface KTHDistinct {
+    fun perform(arr: Array<String>, k: Int): String
+}
+
+class KTHDistinctImpl : KTHDistinct {
+    override fun perform(arr: Array<String>, k: Int): String {
+        val map: MutableMap<String, Boolean> = HashMap()
+        var k0 = k
+        for (s in arr) {
+            map[s] = !map.containsKey(s)
+        }
+        for (s in arr) {
+            if (map[s]!! && k0-- == 1) return s
+        }
+        return ""
+    }
+}
