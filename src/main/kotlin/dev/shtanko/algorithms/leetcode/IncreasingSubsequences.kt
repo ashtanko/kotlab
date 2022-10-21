f0f14e00f7e30eea0a9394be91398ec8af99a64c
Oleksii Shtanko <shtankopro@gmail.com>
2022-10-21 13:58:37 +0300

    Solution of Increasing Subsequences problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/IncreasingSubsequences.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.LinkedList
+
+/**
+ * 491. Increasing Subsequences
+ * @link https://leetcode.com/problems/increasing-subsequences/
+ */
+fun interface IncreasingSubsequences {
+    fun findSubsequences(nums: IntArray): List<List<Int>>
+}
+
+class IncreasingSubsequencesBacktracking : IncreasingSubsequences {
+    override fun findSubsequences(nums: IntArray): List<List<Int>> {
+        val res: MutableList<List<Int>> = LinkedList()
+        helper(LinkedList(), 0, nums, res)
+        return res
+    }
+
+    private fun helper(list: LinkedList<Int>, index: Int, nums: IntArray, res: MutableList<List<Int>>) {
+        if (list.size > 1) res.add(LinkedList(list))
+        val used: MutableSet<Int> = HashSet()
+        for (i in index until nums.size) {
+            if (used.contains(nums[i])) continue
+            if (list.size == 0 || nums[i] >= list.peekLast()) {
+                used.add(nums[i])
+                list.add(nums[i])
+                helper(list, i + 1, nums, res)
+                list.removeAt(list.size - 1)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/IncreasingSubsequencesTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class IncreasingSubsequencesTest<out T : IncreasingSubsequences>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(4, 6, 7, 7),
+                listOf(
+                    listOf(4, 6),
+                    listOf(4, 6, 7),
+                    listOf(4, 6, 7, 7),
+                    listOf(4, 7),
+                    listOf(4, 7, 7),
+                    listOf(6, 7),
+                    listOf(6, 7, 7),
+                    listOf(7, 7),
+                ),
+            ),
+            Arguments.of(
+                intArrayOf(4, 4, 3, 2, 1),
+                listOf(
+                    listOf(4, 4),
+                ),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `find subsequences test`(nums: IntArray, expected: List<List<Int>>) {
+        val actual = strategy.findSubsequences(nums)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class IncreasingSubsequencesBacktrackingTest :
+    IncreasingSubsequencesTest<IncreasingSubsequences>(IncreasingSubsequencesBacktracking())
