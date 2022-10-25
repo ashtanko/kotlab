91813c589e5fab33e51b9540eab188d6929cb404
Oleksii Shtanko <shtankopro@gmail.com>
2022-10-25 21:40:03 +0300

    Solution of Circular Permutation in Binary Representation problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CircularPermutation.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1238. Circular Permutation in Binary Representation
+ * @link https://leetcode.com/problems/circular-permutation-in-binary-representation/
+ */
+fun interface CircularPermutation {
+    fun invoke(n: Int, start: Int): List<Int>
+}
+
+class CircularPermutationGrayCode : CircularPermutation {
+    override fun invoke(n: Int, start: Int): List<Int> {
+        val res: MutableList<Int> = ArrayList()
+        for (i in 0 until (1 shl n)) {
+            res.add(start xor i xor (i shr 1))
+        }
+        return res
+    }
+}
+
+class OneBitDiffPermutation : CircularPermutation {
+    override fun invoke(n: Int, start: Int): List<Int> {
+        val res: MutableList<Int> = ArrayList()
+        val tmp = genOneBitDiffPermutation(n) // generate one bit diff permutations
+
+        // rotate the tmp list to make it starts from "start"
+        // before: {0, 1, ..., start, ..., end}
+        // after:  {start, ..., end, 0, 1, ...}
+        var i = 0
+        while (i < tmp.size) {
+            if (tmp[i] == start) {
+                break
+            }
+            i++
+        }
+
+        for (k in i until tmp.size) {
+            res.add(tmp[k])
+        }
+
+        for (q in 0 until i) {
+            res.add(tmp[q])
+        }
+
+        return res
+    }
+
+    /**
+     *  generate one bit diff permutations
+     * 0, 1
+     * 0, 1, 11, 10
+     * 000, 001, 011, 010, 110, 111, 101, 100
+     */
+    private fun genOneBitDiffPermutation(n: Int): List<Int> {
+        val tmp: MutableList<Int> = ArrayList()
+        tmp.add(0)
+        if (n == 0) {
+            return tmp
+        }
+        for (i in 0 until n) {
+            for (j in tmp.indices.reversed()) {
+                tmp.add(tmp[j] + (1 shl i))
+            }
+        }
+        return tmp
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/CircularPermutationTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class CircularPermutationTest<out T : CircularPermutation>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                2,
+                3,
+                listOf(3, 2, 0, 1),
+            ),
+            Arguments.of(
+                3,
+                2,
+                listOf(2, 6, 7, 5, 4, 0, 1, 3),
+            ),
+            Arguments.of(
+                0,
+                0,
+                listOf<Int>(),
+            ),
+            Arguments.of(
+                1,
+                0,
+                listOf<Int>(),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `circular permutation test`(n: Int, start: Int, expected: List<Int>) {
+        val actual = strategy.invoke(n, start)
+        assertThat(actual).containsAll(expected)
+    }
+}
+
+class CircularPermutationGrayCodeTest : CircularPermutationTest<CircularPermutation>(CircularPermutationGrayCode())
+class OneBitDiffPermutationTest : CircularPermutationTest<CircularPermutation>(OneBitDiffPermutation())
