40624c013c1bb1194fba0c6dd50c019a73ad17a3
Oleksii Shtanko <shtankopro@gmail.com>
2022-10-29 01:02:35 +0300

    Solution of Integer Replacement problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/IntegerReplacement.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 397. Integer Replacement
+ * @link https://leetcode.com/problems/integer-replacement/
+ */
+fun interface IntegerReplacement {
+    fun invoke(n: Int): Int
+}
+
+class IntegerReplacementBitCount : IntegerReplacement {
+    override fun invoke(n: Int): Int {
+        var n0 = n
+        var c = 0
+        while (n0 != 1) {
+            if (n0 and 1 == 0) {
+                n0 = n0 ushr 1
+            } else if (n == 3 || Integer.bitCount(n0 + 1) > Integer.bitCount(n0 - 1)) {
+                --n0
+            } else {
+                ++n0
+            }
+            ++c
+        }
+        return c
+    }
+}
+
+class IntegerReplacementBinary : IntegerReplacement {
+    override fun invoke(n: Int): Int {
+        var c = 0
+        var n0 = n
+        while (n0 != 1) {
+            if (n0 and 1 == 0) {
+                n0 = n0 ushr 1
+            } else if (n0 == 3 || n0 ushr 1 and 1 == 0) {
+                --n0
+            } else {
+                ++n0
+            }
+            ++c
+        }
+        return c
+    }
+}
