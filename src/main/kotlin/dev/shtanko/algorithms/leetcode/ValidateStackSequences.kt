2711f24b8b28d0023c00a360df0244b4e55d33f0
Oleksii Shtanko <shtankopro@gmail.com>
2022-10-31 20:17:47 +0200

    Solution of Validate Stack Sequences problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/ValidateStackSequences.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.Stack
+
+/**
+ * 946. Validate Stack Sequences
+ * @link https://leetcode.com/problems/validate-stack-sequences/
+ */
+fun interface ValidateStackSequences {
+    fun invoke(pushed: IntArray, popped: IntArray): Boolean
+}
+
+class ValidateStackSequencesGreedy : ValidateStackSequences {
+    override fun invoke(pushed: IntArray, popped: IntArray): Boolean {
+        val n: Int = pushed.size
+        val stack: Stack<Int> = Stack()
+
+        var j = 0
+        for (x in pushed) {
+            stack.push(x)
+            while (!stack.isEmpty() && j < n && stack.peek() == popped[j]) {
+                stack.pop()
+                j++
+            }
+        }
+        return j == n
+    }
+}
