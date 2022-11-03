61e4efc6caf79681f33dec848ade493847777a98
Oleksii Shtanko <shtankopro@gmail.com>
2022-11-03 16:14:10 +0200

    Solution of Car Pooling problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CarPooling.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.PriorityQueue
+import java.util.TreeMap
+
+/**
+ * 1094. Car Pooling
+ * @link https://leetcode.com/problems/car-pooling/
+ */
+fun interface CarPooling {
+    fun invoke(trips: Array<IntArray>, capacity: Int): Boolean
+}
+
+class ThousandOneStops : CarPooling {
+    companion object {
+        private const val STOPS = 1001
+    }
+
+    override fun invoke(trips: Array<IntArray>, capacity: Int): Boolean {
+        val nums = IntArray(STOPS)
+        for (trip in trips) {
+            if (trip.isEmpty()) return false
+            for (i in trip[1] until trip[2]) { // put load in index range [getUpStop, getOffStop)
+                nums[i] += trip[0]
+            }
+        }
+        for (num in nums) {
+            if (num > capacity) return false
+        }
+        return true
+    }
+}
+
+class CarPoolingMeetingRoom : CarPooling {
+    override fun invoke(trips: Array<IntArray>, capacity: Int): Boolean {
+        var c = capacity
+        val m: MutableMap<Int, Int> = TreeMap()
+        for (t in trips) {
+            if (t.isEmpty()) return false
+            m[t[1]] = (m[t[1]] ?: 0) + t[0]
+            m[t[2]] = (m[t[2]] ?: 0) - t[0]
+        }
+        for (v in m.values) {
+            c -= v
+            if (c < 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class CarPoolingInterval : CarPooling {
+    override fun invoke(trips: Array<IntArray>, capacity: Int): Boolean {
+        if (trips.isEmpty() || capacity == 0) return false
+        trips.sortWith(compareBy({ it[1] }, { it[2] }))
+        val pq = PriorityQueue<IntArray>(compareBy({ it.first() }, { it.last() })).apply {
+            trips.forEach {
+                offer(intArrayOf(it[2], -it[0]))
+                offer(intArrayOf(it[1], it[0]))
+            }
+        }
+        var maxCount = 0
+        while (pq.isNotEmpty()) {
+            maxCount += pq.poll().last()
+            if (maxCount > capacity) return false
+        }
+
+        return true
+    }
+}
+
+class CarPoolingStream : CarPooling {
+    companion object {
+        private const val STOPS = 1001
+    }
+
+    override fun invoke(trips: Array<IntArray>, capacity: Int): Boolean {
+        val count = IntArray(STOPS)
+        for (t in trips) {
+            if (t.isEmpty()) return false
+            for (i in t[1] until t[2]) {
+                count[i] += t[0]
+            }
+        }
+        return count.max() <= capacity
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/CarPoolingTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class CarPoolingTest<out T : CarPooling>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(2, 1, 5),
+                    intArrayOf(3, 3, 7),
+                ),
+                4,
+                false,
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(2, 1, 5),
+                    intArrayOf(3, 3, 7),
+                ),
+                5,
+                true,
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(),
+                ),
+                0,
+                false,
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `car pooling test`(trips: Array<IntArray>, capacity: Int, expected: Boolean) {
+        val actual = strategy.invoke(trips, capacity)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class ThousandOneStopsTest : CarPoolingTest<CarPooling>(ThousandOneStops())
+class CarPoolingMeetingRoomTest : CarPoolingTest<CarPooling>(CarPoolingMeetingRoom())
+class CarPoolingIntervalTest : CarPoolingTest<CarPooling>(CarPoolingInterval())
+class CarPoolingStreamTest : CarPoolingTest<CarPooling>(CarPoolingStream())
