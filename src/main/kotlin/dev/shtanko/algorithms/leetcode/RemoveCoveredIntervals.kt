30572c55c79e62cac42359772b2f8267bc945e3b
Oleksii Shtanko <shtankopro@gmail.com>
2022-11-08 09:41:50 +0200

    Solution of Remove Covered Intervals problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/RemoveCoveredIntervals.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import dev.shtanko.algorithms.extensions.second
+import java.util.Arrays
+import kotlin.math.max
+
+/**
+ * 1288. Remove Covered Intervals
+ * @link https://leetcode.com/problems/remove-covered-intervals/
+ */
+fun interface RemoveCoveredIntervals {
+    fun perform(intervals: Array<IntArray>): Int
+}
+
+class RemoveCoveredIntervalsSort : RemoveCoveredIntervals {
+    override fun perform(intervals: Array<IntArray>): Int {
+        var res = 0
+        var left = -1
+        var right = -1
+        Arrays.sort(intervals) { a, b -> a[0] - b[0] }
+        for (v in intervals) {
+            if (v[0] > left && v[1] > right) {
+                left = v[0]
+                ++res
+            }
+            right = max(right, v[1])
+        }
+        return res
+    }
+}
+
+class RemoveCoveredIntervalsSortLeft : RemoveCoveredIntervals {
+    override fun perform(intervals: Array<IntArray>): Int {
+        var res = 0
+        var right = 0
+        Arrays.sort(
+            intervals,
+        ) { a: IntArray, b: IntArray ->
+            if (a.first() != b.first()) {
+                a.first() - b.first()
+            } else {
+                b.second() - a.second()
+            }
+        }
+        for (v: IntArray in intervals) {
+            if (v.second() > right) {
+                ++res
+                right = v.second()
+            }
+        }
+        return res
+    }
+}
