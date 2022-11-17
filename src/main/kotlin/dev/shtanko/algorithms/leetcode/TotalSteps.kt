b4cb70aefbefa793e31ae69f2cde9e9114faa898
Oleksii Shtanko <shtankopro@gmail.com>
2022-11-17 10:27:12 +0200

    Solution of Steps to Make Array Non-decreasing problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/TotalSteps.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 2289. Steps to Make Array Non-decreasing
+ * @link https://leetcode.com/problems/steps-to-make-array-non-decreasing/
+ */
+interface TotalSteps {
+    fun perform(nums: IntArray): Int
+}
+
+class TotalStepsDp : TotalSteps {
+    override fun perform(nums: IntArray): Int {
+        val n: Int = nums.size
+        var res = 0
+        var j = -1
+        val dp = IntArray(n)
+        val stack = IntArray(n)
+        for (i in n - 1 downTo 0) {
+            while (j >= 0 && nums[i] > nums[stack[j]]) {
+                dp[i] = max(++dp[i], dp[stack[j--]])
+                res = max(res, dp[i])
+            }
+            stack[++j] = i
+        }
+        return res
+    }
+}
