aba17e584f17cf977863f1668eb3c46cbb6c0d35
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-05 11:50:44 +0200

    Solution of Height of Binary Tree After Subtree Removal Queries problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/TreeQueries.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.max
+
+/**
+ * 2458. Height of Binary Tree After Subtree Removal Queries
+ * @link https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries/
+ */
+interface TreeQueries {
+    fun invoke(root: TreeNode, queries: IntArray): IntArray
+}
+
+class TreeQueriesDP : TreeQueries {
+    private val cache: HashMap<Int, Int> = HashMap()
+    private val answer: IntArray = IntArray(SIZE)
+
+    companion object {
+        private const val SIZE = 100001
+    }
+
+    override fun invoke(root: TreeNode, queries: IntArray): IntArray {
+        dfs(root, 0, 0)
+        return queries.map { q -> answer[q] }.toIntArray()
+    }
+
+    private fun dfs(n: TreeNode?, d: Int, m: Int) {
+        if (n == null) {
+            return
+        }
+        answer[n.value] = m
+        dfs(n.left, d + 1, max(m, d + helper(n.right)))
+        dfs(n.right, d + 1, max(m, d + helper(n.left)))
+    }
+
+    private fun helper(n: TreeNode?): Int {
+        if (n == null) return 0
+        if (!cache.containsKey(n.value)) {
+            cache[n.value] = 1 + max(helper(n.left), helper(n.right))
+        }
+        return cache[n.value] ?: 0
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/TreeQueriesTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class TreeQueriesTest<out T : TreeQueries>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(3).apply {
+                        left = TreeNode(2)
+                    }
+                    right = TreeNode(4).apply {
+                        left = TreeNode(6)
+                        right = TreeNode(5).apply {
+                            right = TreeNode(7)
+                        }
+                    }
+                },
+                intArrayOf(4),
+                intArrayOf(2),
+            ),
+            Arguments.of(
+                TreeNode(5).apply {
+                    left = TreeNode(8).apply {
+                        left = TreeNode(2).apply {
+                            left = TreeNode(4)
+                            right = TreeNode(6)
+                        }
+                        right = TreeNode(1)
+                    }
+                    right = TreeNode(9).apply {
+                        left = TreeNode(3)
+                        right = TreeNode(7)
+                    }
+                },
+                intArrayOf(3, 2, 4, 8),
+                intArrayOf(3, 2, 3, 2),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `tree queries test`(root: TreeNode, queries: IntArray, expected: IntArray) {
+        val actual = strategy.invoke(root, queries)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class TreeQueriesDPTest : TreeQueriesTest<TreeQueries>(TreeQueriesDP())
