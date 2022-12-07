73836123395f0ff37e3ebf8b0afd267a36c31c1e
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-07 13:41:43 +0200

    Add pre-commit.sh

---
+++ config/git/pre-commit.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+echo "Running static analysis..."
+
+JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export JAVA_HOME
+
+OUTPUT="/tmp/analysis-result"
+./gradlew spotlessApply spotlessCheck spotlessKotlin detekt ktlintCheck --profile --daemon > ${OUTPUT}
+EXIT_CODE=$?
+if [ ${EXIT_CODE} -ne 0 ]; then
+    cat ${OUTPUT}
+    rm ${OUTPUT}
+    echo "*********************************************"
+    echo "            Static Analysis Failed           "
+    echo "Please fix the above issues before committing"
+    echo "*********************************************"
+    exit ${EXIT_CODE}
+else
+    rm ${OUTPUT}
+    echo "*********************************************"
+    echo "      Static analysis no problems found      "
+    echo "*********************************************"
+
+    echo "*********************************************"
+    echo "      Coping detekt report                  "
+    echo "*********************************************"
+
+    cat ../../build/reports/detekt/detekt.md >> README.md
+
+    echo "*********************************************"
+    echo "      All set                               "
+    echo "*********************************************"
+fi
