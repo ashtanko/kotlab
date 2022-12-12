379ce0714a0bf76cf6981581cd5936882f725e4d
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-12 09:48:43 +0200

    Solution of Maximum Difference Between Node and Ancestor problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MaxAncestorDiff.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * 1026. Maximum Difference Between Node and Ancestor
+ * @link https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/description/
+ */
+fun interface MaxAncestorDiff {
+    fun perform(root: TreeNode?): Int
+}
+
+/**
+ * Approach #1: Recursion
+ */
+class MaxAncestorDiffRecursion : MaxAncestorDiff {
+
+    // record the required maximum difference
+    var result = 0
+
+    override fun perform(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+        result = 0
+        helper(root, root.value, root.value)
+        return result
+    }
+
+    private fun helper(node: TreeNode?, curMax: Int, curMin: Int): Int {
+        var cMax = curMax
+        var cMin = curMin
+        if (node == null) {
+            return 0
+        }
+        val possibleResult: Int = max(abs(cMax - node.value), abs(cMin - node.value))
+        result = max(result, possibleResult)
+        cMax = max(cMax, node.value)
+        cMin = min(cMin, node.value)
+        helper(node.left, cMax, cMin)
+        helper(node.right, cMax, cMin)
+        return result
+    }
+}
+
+/**
+ * Approach #2: Maximum Minus Minimum
+ */
+class MaxAncestorDiffMM : MaxAncestorDiff {
+    override fun perform(root: TreeNode?): Int {
+        return if (root == null) {
+            0
+        } else {
+            helper(root, root.value, root.value)
+        }
+    }
+
+    fun helper(node: TreeNode?, curMax: Int, curMin: Int): Int {
+        // if encounter leaves, return the max-min along the path
+        var cMax = curMax
+        var cMin = curMin
+        if (node == null) {
+            return cMax - cMin
+        }
+        // else, update max and min
+        // and return the max of left and right subtrees
+        cMax = max(cMax, node.value)
+        cMin = min(cMin, node.value)
+        val left = helper(node.left, cMax, cMin)
+        val right = helper(node.right, cMax, cMin)
+        return max(left, right)
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MaxAncestorDiffTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MaxAncestorDiffTest<out T : MaxAncestorDiff>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(8).apply {
+                    left = TreeNode(3).apply {
+                        left = TreeNode(1)
+                        right = TreeNode(6).apply {
+                            left = TreeNode(4)
+                            right = TreeNode(7)
+                        }
+                    }
+                    right = TreeNode(10).apply {
+                        right = TreeNode(14).apply {
+                            left = TreeNode(13)
+                        }
+                    }
+                },
+                7,
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    right = TreeNode(2).apply {
+                        right = TreeNode(0).apply {
+                            left = TreeNode(3)
+                        }
+                    }
+                },
+                3,
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `max ancestor diff test`(root: TreeNode?, expected: Int) {
+        val actual = strategy.perform(root)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MaxAncestorDiffRecursionTest : MaxAncestorDiffTest<MaxAncestorDiff>(MaxAncestorDiffRecursion())
+class MaxAncestorDiffMMTest : MaxAncestorDiffTest<MaxAncestorDiff>(MaxAncestorDiffMM())
