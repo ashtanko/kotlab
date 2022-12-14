dc64967f9894fc15e627adf5db8743866473f748
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-14 14:09:59 +0200

    Add test cases

--- src/test/kotlin/dev/shtanko/algorithms/leetcode/RichestCustomerWealthTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/RichestCustomerWealthTest.kt
@@ -29,15 +29,42 @@ internal class RichestCustomerWealthTest {
     internal class InputArgumentsProvider : ArgumentsProvider {
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
             Arguments.of(
-                arrayOf(intArrayOf(1, 2, 3), intArrayOf(3, 2, 1)),
+                arrayOf<IntArray>(),
+                0,
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(),
+                ),
+                0,
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(0),
+                ),
+                0,
+            ),
+            Arguments.of(
+                arrayOf(
+                    intArrayOf(1, 2, 3),
+                    intArrayOf(3, 2, 1),
+                ),
                 6,
             ),
             Arguments.of(
-                arrayOf(intArrayOf(1, 5), intArrayOf(7, 3), intArrayOf(3, 5)),
+                arrayOf(
+                    intArrayOf(1, 5),
+                    intArrayOf(7, 3),
+                    intArrayOf(3, 5),
+                ),
                 10,
             ),
             Arguments.of(
-                arrayOf(intArrayOf(2, 8, 7), intArrayOf(7, 1, 3), intArrayOf(1, 9, 5)),
+                arrayOf(
+                    intArrayOf(2, 8, 7),
+                    intArrayOf(7, 1, 3),
+                    intArrayOf(1, 9, 5),
+                ),
                 17,
             ),
         )
