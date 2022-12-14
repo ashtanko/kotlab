78277bfeaee02498dc535ad4fe4f0748f3855e9f
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-14 13:55:57 +0200

    Add test cases

--- src/test/kotlin/dev/shtanko/algorithms/leetcode/RunningSum1DTest.kt
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/RunningSum1DTest.kt
@@ -16,38 +16,66 @@
 
 package dev.shtanko.algorithms.leetcode
 
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
 
 internal class RunningSum1DTest {
 
-    companion object {
-        @JvmStatic
-        fun dataProvider(): List<Pair<IntArray, IntArray>> {
-            return listOf(
-                intArrayOf() to intArrayOf(),
-                intArrayOf(1, 2, 3, 4) to intArrayOf(1, 3, 6, 10),
-                intArrayOf(1, 1, 1, 1, 1) to intArrayOf(1, 2, 3, 4, 5),
-                intArrayOf(3, 1, 2, 10, 1) to intArrayOf(3, 4, 6, 16, 17),
-                intArrayOf(-3, 1, -2, 10, -1) to intArrayOf(-3, -2, -4, 6, 5),
-            )
-        }
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(),
+                intArrayOf(),
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 3, 4),
+                intArrayOf(1, 3, 6, 10),
+            ),
+            Arguments.of(
+                intArrayOf(1, 1, 1, 1, 1),
+                intArrayOf(1, 2, 3, 4, 5),
+            ),
+            Arguments.of(
+                intArrayOf(3, 1, 2, 10, 1),
+                intArrayOf(3, 4, 6, 16, 17),
+            ),
+            Arguments.of(
+                intArrayOf(-3, 1, -2, 10, -1),
+                intArrayOf(-3, -2, -4, 6, 5),
+            ),
+            Arguments.of(
+                intArrayOf(1),
+                intArrayOf(),
+            ),
+            Arguments.of(
+                intArrayOf(1, 2),
+                intArrayOf(1, 3),
+            ),
+            Arguments.of(
+                intArrayOf(-1, -2),
+                intArrayOf(-1, -3),
+            ),
+        )
     }
 
     @Test
     internal fun `is empty test`() {
-        val arr = intArrayOf()
-        assertTrue(arr.runningSumNaive().isEmpty())
+        assertTrue(intArrayOf().runningSumNaive().isEmpty())
     }
 
     @ParameterizedTest
-    @MethodSource("dataProvider")
-    internal fun `running sum naive test`(testCase: Pair<IntArray, IntArray>) {
-        val (arr, expected) = testCase
+    @ArgumentsSource(InputArgumentsProvider::class)
+    internal fun `running sum naive test`(arr: IntArray, expected: IntArray) {
         val actual = arr.runningSumNaive()
+        assertThat(actual).isEqualTo(expected)
         assertArrayEquals(expected, actual)
     }
 }
