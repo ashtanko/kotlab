69f4b667213dd99d2d420974f3b4cce991cee010
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-22 13:45:25 +0200

    Solution of Largest Divisible Subset problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/LargestDivisibleSubset.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 368. Largest Divisible Subset
+ * @link https://leetcode.com/problems/largest-divisible-subset/
+ */
+interface LargestDivisibleSubset {
+    fun perform(nums: IntArray): List<Int>
+}
+
+class LargestDivisibleSubsetDFS : LargestDivisibleSubset {
+
+    private val mem: MutableMap<Int, List<Int>> = HashMap()
+
+    override fun perform(nums: IntArray): List<Int> {
+        nums.sort()
+        return helper(nums, 0)
+    }
+
+    private fun helper(nums: IntArray, i: Int): List<Int> {
+        if (mem.containsKey(i)) {
+            return mem[i] ?: emptyList()
+        }
+        var maxLenLst: List<Int> = ArrayList()
+        val div = if (i == 0) 1 else nums[i - 1]
+        for (k in i until nums.size) {
+            if (nums[k] % div == 0) {
+                // make a copy is crucial here, so that we won't modify the returned List reference
+                val lst: MutableList<Int> = ArrayList(helper(nums, k + 1))
+                lst.add(nums[k])
+                if (lst.size > maxLenLst.size) maxLenLst = lst
+            }
+        }
+        mem[i] = maxLenLst
+        return maxLenLst
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/LargestDivisibleSubsetTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class LargestDivisibleSubsetTest<out T : LargestDivisibleSubset>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                intArrayOf(1, 2, 3),
+                listOf(1, 2),
+            ),
+            Arguments.of(
+                intArrayOf(),
+                listOf<Int>(),
+            ),
+            Arguments.of(
+                intArrayOf(1, 2, 4, 8),
+                listOf(1, 2, 4, 8),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `largest divisible subset test`(nums: IntArray, expected: List<Int>) {
+        val actual = strategy.perform(nums)
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected)
+    }
+}
+
+class LargestDivisibleSubsetDFSTest : LargestDivisibleSubsetTest<LargestDivisibleSubset>(LargestDivisibleSubsetDFS())
