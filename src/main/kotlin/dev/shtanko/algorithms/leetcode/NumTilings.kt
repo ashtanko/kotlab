40f643f71aeb62b5c96151e0beda60f8e18884af
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-24 23:15:11 +0200

    Solution of Domino and Tromino Tiling problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/NumTilings.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 790. Domino and Tromino Tiling
+ * @link https://leetcode.com/problems/domino-and-tromino-tiling/
+ */
+interface NumTilings {
+    fun perform(n: Int): Int
+}
+
+class NumTilingsDP : NumTilings {
+    override fun perform(n: Int): Int {
+        val dp = LongArray(n + 2)
+        dp[0] = 1; dp[1] = 2
+        val dpa = LongArray(n + 2)
+        dpa[1] = 1
+        for (i in 2 until n) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dpa[i - 1] * 2) % MOD
+            dpa[i] = (dp[i - 2] + dpa[i - 1]) % MOD
+        }
+        return dp[n - 1].toInt()
+    }
+}
