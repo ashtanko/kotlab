e85e3455b5269aa07758f11d69e3950ea9dfeba3
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-26 08:57:12 +0200

    Small refactor

--- src/main/kotlin/dev/shtanko/algorithms/leetcode/CanJump.kt
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CanJump.kt
@@ -18,6 +18,7 @@ package dev.shtanko.algorithms.leetcode
 
 /**
  * Jump to game
+ * @link https://leetcode.com/problems/jump-game/description/
  * Given an array of non-negative integers, you are initially positioned at the first index of the array.
  * Each element in the array represents your maximum jump length at that position.
  * Determine if you are able to reach the last index.
