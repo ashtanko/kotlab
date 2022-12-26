aa4357e1304239e383c46378dfaec56f7c8c3c26
Oleksii Shtanko <shtankopro@gmail.com>
2022-12-26 09:25:04 +0200

    Solution of Longest Subsequence With Limited Sum problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/AnswerQueries.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.Arrays
+import kotlin.math.abs
+
+/**
+ * 2389. Longest Subsequence With Limited Sum
+ * @link https://leetcode.com/problems/longest-subsequence-with-limited-sum/
+ */
+interface AnswerQueries {
+    fun perform(nums: IntArray, queries: IntArray): IntArray
+}
+
+class AnswerQueriesBinarySearch : AnswerQueries {
+    override fun perform(nums: IntArray, queries: IntArray): IntArray {
+        nums.sort()
+        val n: Int = nums.size
+        val m: Int = queries.size
+        val res = IntArray(m)
+        for (i in 1 until n) {
+            nums[i] += nums[i - 1]
+        }
+        for (i in 0 until m) {
+            val j: Int = Arrays.binarySearch(nums, queries[i])
+            res[i] = abs(j + 1)
+        }
+        return res
+    }
+}
