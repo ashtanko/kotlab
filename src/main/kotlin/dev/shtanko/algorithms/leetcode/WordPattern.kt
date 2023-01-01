e613005e65df5083d1323c44f6ec40e74e0caebf
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-01 15:57:07 +0200

    Solution of Word Pattern problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/WordPattern.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 290. Word Pattern
+ * @link https://leetcode.com/problems/word-pattern/
+ */
+interface WordPattern {
+    fun perform(pattern: String, s: String): Boolean
+}
+
+class WordPatternSimple : WordPattern {
+    override fun perform(pattern: String, s: String): Boolean {
+        val words: Array<String> = s.split(" ").toTypedArray()
+        if (words.size != pattern.length) {
+            return false
+        }
+
+        val charToWord: MutableMap<Char, String> = HashMap()
+        val wordToChar: MutableMap<String, Char> = HashMap()
+
+        for (i in pattern.indices) {
+            val c: Char = pattern[i]
+            val word = words[i]
+            if (!charToWord.containsKey(c)) {
+                charToWord[c] = word
+            }
+            if (!wordToChar.containsKey(word)) {
+                wordToChar[word] = c
+            }
+            if (charToWord[c] != word || wordToChar[word] != c) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/WordPatternTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class WordPatternTest<out T : WordPattern>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "abba",
+                "dog cat cat dog",
+                true,
+            ),
+            Arguments.of(
+                "abba",
+                "dog cat cat fish",
+                false,
+            ),
+            Arguments.of(
+                "aaaa",
+                "dog cat cat dog",
+                false,
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `word pattern test`(pattern: String, s: String, expected: Boolean) {
+        val actual = strategy.perform(pattern, s)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class WordPatternSimpleTest : WordPatternTest<WordPattern>(WordPatternSimple())
