66d7256911e6ca060a7af318864cb27634a07bba
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-07 12:33:14 +0200

    Solution of Search in Rotated Sorted Array problem

--- README.md
+++ README.md
@@ -9,29 +9,29 @@
 
 ## Metrics
 
-* 7,645 number of properties
+* 7,659 number of properties
 
-* 5,315 number of functions
+* 5,324 number of functions
 
-* 4,448 number of classes
+* 4,458 number of classes
 
 * 127 number of packages
 
-* 1,874 number of kt files
+* 1,878 number of kt files
 
 ## Complexity Report
 
-* 123,367 lines of code (loc)
+* 123,580 lines of code (loc)
 
-* 75,595 source lines of code (sloc)
+* 75,717 source lines of code (sloc)
 
-* 53,826 logical lines of code (lloc)
+* 53,910 logical lines of code (lloc)
 
-* 33,951 comment lines of code (cloc)
+* 34,019 comment lines of code (cloc)
 
-* 13,091 cyclomatic complexity (mcc)
+* 13,111 cyclomatic complexity (mcc)
 
-* 11,332 cognitive complexity
+* 11,351 cognitive complexity
 
 * 0 number of total code smells
 
@@ -43,4 +43,4 @@
 
 ## Findings (0)
 
-generated with [detekt version 1.22.0](https://detekt.dev/) on 2023-01-06 21:56:40 UTC
+generated with [detekt version 1.22.0](https://detekt.dev/) on 2023-01-07 10:32:03 UTC
---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/SearchInRotatedSortedArray.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 33. Search in Rotated Sorted Array
+ * @link https://leetcode.com/problems/search-in-rotated-sorted-array/
+ */
+interface SearchInRotatedSortedArray {
+    fun search(nums: IntArray, target: Int): Int
+}
+
+class SearchInRotatedSortedArraySearch : SearchInRotatedSortedArray {
+    override fun search(nums: IntArray, target: Int): Int {
+        val minIdx = findMinIdx(nums)
+        if (target == nums[minIdx]) return minIdx
+        val m: Int = nums.size
+        var start = if (target <= nums[m - 1]) minIdx else 0
+        var end = if (target > nums[m - 1]) minIdx else m - 1
+
+        while (start <= end) {
+            val mid = start + (end - start) / 2
+            if (nums[mid] == target) {
+                return mid
+            } else if (target > nums[mid]) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+        return -1
+    }
+
+    private fun findMinIdx(nums: IntArray): Int {
+        var start = 0
+        var end = nums.size - 1
+        while (start < end) {
+            val mid = start + (end - start) / 2
+            if (nums[mid] > nums[end]) start = mid + 1 else end = mid
+        }
+        return start
+    }
+}
