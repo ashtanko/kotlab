23ee1faaa819bfe9b8595f49aac6ddd0899f74d0
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-08 12:09:39 +0200

    Solution of Different Ways to Add Parentheses problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DiffWaysToCompute.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 241. Different Ways to Add Parentheses
+ * @link https://leetcode.com/problems/different-ways-to-add-parentheses/
+ */
+interface DiffWaysToCompute {
+    fun compute(expression: String): List<Int>
+}
+
+class DiffWaysToComputeRecursive : DiffWaysToCompute {
+
+    // function to get the result of the operation
+    fun perform(x: Int, y: Int, op: Char): Int {
+        if (op == '+') return x + y
+        if (op == '-') return x - y
+        return if (op == '*') x * y else 0
+    }
+
+    override fun compute(expression: String): List<Int> {
+        val results: MutableList<Int> = ArrayList()
+        if (expression.isBlank()) return results
+        var isNumber = true
+        for (i in expression.indices) {
+            // check if current character is an operator
+            if (!Character.isDigit(expression[i])) {
+                // if current character is not a digit then
+                // exp is not purely a number
+                isNumber = false
+
+                // list of first operands
+                val left = compute(expression.substring(0, i))
+
+                // list of second operands
+                val right = compute(expression.substring(i + 1))
+
+                // performing operations
+                for (x in left) {
+                    for (y in right) {
+                        val value = perform(x, y, expression[i])
+                        results.add(value)
+                    }
+                }
+            }
+        }
+        if (isNumber) results.add(Integer.valueOf(expression))
+        return results
+    }
+}
+
+class DiffWaysToComputeDivideAndConquer : DiffWaysToCompute {
+    override fun compute(expression: String): List<Int> {
+        if (expression.isEmpty()) return emptyList()
+        return mutableListOf<Int>().apply {
+            expression.toCharArray().forEachIndexed { index, char ->
+                if (char in listOf('+', '-', '*')) {
+                    val left = expression.substring(0, index)
+                    val right = expression.substring(index + 1)
+
+                    val leftNums = compute(left)
+                    val rightNums = compute(right)
+
+                    leftNums.forEach { num1 ->
+                        rightNums.forEach { num2 ->
+                            add(char.calculate(num1, num2))
+                        }
+                    }
+                }
+            }
+            if (isEmpty()) add(expression.toInt())
+        }
+    }
+
+    private fun Char.calculate(num1: Int, num2: Int) = when (this) {
+        '+' -> num1 + num2
+        '-' -> num1 - num2
+        '*' -> num1 * num2
+        else -> throw IllegalArgumentException("Symbol is not allowed")
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/DiffWaysToComputeTest.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class DiffWaysToComputeTest<out T : DiffWaysToCompute>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "1",
+                listOf(1),
+            ),
+            Arguments.of(
+                "2-1-1",
+                listOf(0, 2),
+            ),
+            Arguments.of(
+                "2*3-4*5",
+                listOf(-34, -14, -10, -10, 10),
+            ),
+            Arguments.of(
+                "4+5*6-4-7+6",
+                listOf(
+                    79,
+                    19,
+                    -51,
+                    79,
+                    9,
+                    43,
+                    31,
+                    1,
+                    17,
+                    55,
+                    -15,
+                    43,
+                    13,
+                    29,
+                    135,
+                    27,
+                    -99,
+                    135,
+                    9,
+                    43,
+                    31,
+                    63,
+                    51,
+                    1,
+                    17,
+                    5,
+                    17,
+                    37,
+                    55,
+                    -15,
+                    43,
+                    13,
+                    29,
+                    87,
+                    -39,
+                    43,
+                    63,
+                    13,
+                    29,
+                    17,
+                    29,
+                    49,
+                ),
+            ),
+            Arguments.of(
+                "",
+                listOf<Int>(),
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `diff ways to compute test`(expression: String, expected: List<Int>) {
+        val actual = strategy.compute(expression)
+        assertThat(actual).containsExactlyInAnyOrder(*expected.toTypedArray())
+    }
+}
+
+class DiffWaysToComputeRecursiveTest : DiffWaysToComputeTest<DiffWaysToCompute>(DiffWaysToComputeRecursive())
+class DiffWaysToComputeDivideAndConquerTest :
+    DiffWaysToComputeTest<DiffWaysToCompute>(DiffWaysToComputeDivideAndConquer())
