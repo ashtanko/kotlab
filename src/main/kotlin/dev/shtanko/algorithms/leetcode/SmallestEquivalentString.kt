53563082899686586ef07a82e986e1d45b40bdea
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-14 21:30:17 +0200

    Solution of Lexicographically Smallest Equivalent String problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/SmallestEquivalentString.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1061. Lexicographically Smallest Equivalent String
+ * @link https://leetcode.com/problems/lexicographically-smallest-equivalent-string/
+ */
+interface SmallestEquivalentString {
+    fun perform(s1: String, s2: String, baseStr: String): String
+}
+
+class SmallestEquivalentStringUnion : SmallestEquivalentString {
+    override fun perform(s1: String, s2: String, baseStr: String): String {
+        val graph = IntArray(LIMIT)
+        for (i in 0 until LIMIT) {
+            graph[i] = i
+        }
+        for (i in s1.indices) {
+            val a: Int = s1[i] - 'a'
+            val b: Int = s2[i] - 'a'
+            val end1 = find(graph, b)
+            val end2 = find(graph, a)
+            if (end1 < end2) {
+                graph[end2] = end1
+            } else {
+                graph[end1] = end2
+            }
+        }
+        val sb = StringBuilder()
+        for (i in baseStr.indices) {
+            val c: Char = baseStr[i]
+            sb.append(('a'.code + find(graph, c.code - 'a'.code)).toChar())
+        }
+        return sb.toString()
+    }
+
+    private fun find(graph: IntArray, idx: Int): Int {
+        var i = idx
+        while (graph[i] != i) {
+            i = graph[i]
+        }
+        return i
+    }
+
+    private companion object {
+        private const val LIMIT = 26
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/SmallestEquivalentStringTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class SmallestEquivalentStringTest<out T : SmallestEquivalentString>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                "parker",
+                "morris",
+                "parser",
+                "makkek",
+            ),
+            Arguments.of(
+                "hello",
+                "world",
+                "hold",
+                "hdld",
+            ),
+            Arguments.of(
+                "leetcode",
+                "programs",
+                "sourcecode",
+                "aauaaaaada",
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `smallest equivalent string test`(s1: String, s2: String, baseStr: String, expected: String) {
+        val actual = strategy.perform(s1, s2, baseStr)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class SmallestEquivalentStringUnionTest :
+    SmallestEquivalentStringTest<SmallestEquivalentString>(SmallestEquivalentStringUnion())
