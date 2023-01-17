771753c05188a38c3a0d96a8eca4a197389be4d5
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-17 12:30:46 +0200

    Solution of Flip String to Monotone Increasing problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MinFlipsMonoIncr.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 926. Flip String to Monotone Increasing
+ * @link https://leetcode.com/problems/flip-string-to-monotone-increasing/
+ */
+interface MinFlipsMonoIncr {
+    fun perform(s: String): Int
+}
+
+class MinFlipsMonoIncrOnePass : MinFlipsMonoIncr {
+    override fun perform(s: String): Int {
+        if (s.isEmpty()) return 0
+
+        val sChars: CharArray = s.toCharArray()
+        var flipCount = 0
+        var onesCount = 0
+
+        for (i in sChars.indices) {
+            if (sChars[i] == '0') {
+                if (onesCount == 0) continue else flipCount++
+            } else {
+                onesCount++
+            }
+            if (flipCount > onesCount) {
+                flipCount = onesCount
+            }
+        }
+        return flipCount
+    }
+}
