f70c787cd76b246971e13ca5f32f82a45de23e85
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-29 17:19:00 +0200

    Solution of Data Stream as Disjoint Intervals problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/DisjointIntervals.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.TreeMap
+import kotlin.math.max
+
+/**
+ * 352. Data Stream as Disjoint Intervals
+ * @link https://leetcode.com/problems/data-stream-as-disjoint-intervals/
+ */
+class DisjointIntervals {
+
+    var map: TreeMap<Int, IntArray> = TreeMap()
+
+    fun addNum(value: Int) {
+        if (map.containsKey(value)) return
+        val low = map.lowerKey(value)
+        val high = map.higherKey(value)
+        if (low != null && high != null && value == map[low]!![1] + 1 && value == map[high]!![0] - 1) {
+            // low =1,val=3 high=4 and high has key =[4,5] and low has key [1,2]
+            // now 4 has no use as 1 2 3 4 5 is possible so [1,5]
+            map[low]!![1] = map[high]!![1]
+            map.remove(high)
+        } else if (low != null && value <= map[low]!![1] + 1) {
+            // for example if 2,8 exists and val is 7 nothing happens but if it is 9 then 2,9
+            // if i dont put <= then on 7  as  val,new value is added in the last else part which is incorrect
+            map[low]!![1] = max(value, map[low]!![1])
+        } else if (high != null && value == map[high]!![0] - 1) {
+            // if  5,8 exists and val is 4 so new=[4,8] and 5 is removed
+            map[value] = intArrayOf(value, map[high]!![1])
+            map.remove(high)
+        } else {
+            map[value] = intArrayOf(value, value)
+        }
+    }
+
+    fun getIntervals(): Array<IntArray> {
+        val res = Array(map.size) { IntArray(2) }
+        var i = 0
+        for (x in map.values) {
+            res[i++] = x
+        }
+        return res
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/DisjointIntervalsTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DisjointIntervalsTest {
+
+    @Test
+    fun `summary ranges test`() {
+        val disjointIntervals = DisjointIntervals()
+        disjointIntervals.addNum(1)
+        assertThat(disjointIntervals.getIntervals()).isEqualTo(arrayOf(intArrayOf(1, 1)))
+        disjointIntervals.addNum(3)
+        assertThat(disjointIntervals.getIntervals()).isEqualTo(arrayOf(intArrayOf(1, 1), intArrayOf(3, 3)))
+        disjointIntervals.addNum(7)
+        assertThat(disjointIntervals.getIntervals()).isEqualTo(
+            arrayOf(
+                intArrayOf(1, 1),
+                intArrayOf(3, 3),
+                intArrayOf(7, 7),
+            ),
+        )
+        disjointIntervals.addNum(2)
+        assertThat(disjointIntervals.getIntervals()).isEqualTo(arrayOf(intArrayOf(1, 3), intArrayOf(7, 7)))
+        disjointIntervals.addNum(6)
+        assertThat(disjointIntervals.getIntervals()).isEqualTo(arrayOf(intArrayOf(1, 3), intArrayOf(6, 7)))
+    }
+}
