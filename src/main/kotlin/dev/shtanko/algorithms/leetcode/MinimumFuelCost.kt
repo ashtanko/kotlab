51d5fccf5f384c033832ecb9d82f19ed0045afe7
Oleksii Shtanko <shtankopro@gmail.com>
2023-02-13 13:01:35 +0200

    Solution of Minimum Fuel Cost to Report to the Capital problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MinimumFuelCost.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 2477. Minimum Fuel Cost to Report to the Capital
+ * @link https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/
+ */
+interface MinimumFuelCost {
+    fun perform(roads: Array<IntArray>, seats: Int): Long
+}
+
+class MinimumFuelCostDFS : MinimumFuelCost {
+
+    private var ans: Long = 0
+    private var s: Int = 0
+
+    override fun perform(roads: Array<IntArray>, seats: Int): Long {
+        val graph: MutableList<MutableList<Int>> = ArrayList()
+        s = seats
+        for (i in 0 until roads.size + 1) {
+            graph.add(ArrayList())
+        }
+        for (r in roads) {
+            graph[r[0]].add(r[1])
+            graph[r[1]].add(r[0])
+        }
+        dfs(0, 0, graph)
+        return ans
+    }
+
+    private fun dfs(i: Int, prev: Int, graph: List<List<Int>>): Int {
+        var people = 1
+        for (x in graph[i]) {
+            if (x == prev) continue
+            people += dfs(x, i, graph)
+        }
+        if (i != 0) ans += (people + s - 1) / s
+        return people
+    }
+}
