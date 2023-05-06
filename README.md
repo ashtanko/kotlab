7e89ab9fc66a2c43220a0c94b3ece08e6f0effc4
Oleksii Shtanko <shtankopro@gmail.com>
2023-05-06 23:22:43 +0200

    Add solutions

--- README.md
+++ README.md
@@ -10,38 +10,38 @@
 
 ## Metrics
 
-* 8,720 number of properties
+* 8,764 number of properties
 
-* 6,035 number of functions
+* 6,054 number of functions
 
-* 5,186 number of classes
+* 5,209 number of classes
 
 * 133 number of packages
 
-* 2,145 number of kt files
+* 2,151 number of kt files
 
 ## Complexity Report
 
-* 140,895 lines of code (loc)
+* 141,453 lines of code (loc)
 
-* 86,286 source lines of code (sloc)
+* 86,628 source lines of code (sloc)
 
-* 61,419 logical lines of code (lloc)
+* 61,663 logical lines of code (lloc)
 
-* 39,050 comment lines of code (cloc)
+* 39,197 comment lines of code (cloc)
 
-* 14,917 cyclomatic complexity (mcc)
+* 14,986 cyclomatic complexity (mcc)
 
-* 13,040 cognitive complexity
+* 13,134 cognitive complexity
 
 * 0 number of total code smells
 
 * 45% comment source ratio
 
-* 242 mcc per 1,000 lloc
+* 243 mcc per 1,000 lloc
 
 * 0 code smells per 1,000 lloc
 
 ## Findings (0)
 
-generated with [detekt version 1.22.0](https://detekt.dev/) on 2023-05-03 20:20:01 UTC
+generated with [detekt version 1.22.0](https://detekt.dev/) on 2023-05-06 21:21:27 UTC
---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MaxVowels.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+/**
+ * 1456. Maximum Number of Vowels in a Substring of Given Length
+ * @link https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+ */
+interface MaxVowels {
+    fun perform(s: String, k: Int): Int
+}
+
+class MaxVowelsSlidingWindow : MaxVowels {
+    override fun perform(s: String, k: Int): Int {
+        val vowels = setOf('a', 'e', 'i', 'o', 'u')
+
+        // Build the window of size k, count the number of vowels it contains.
+        var count = 0
+        for (i in 0 until k) {
+            count += if (vowels.contains(s[i])) 1 else 0
+        }
+        var answer = count
+
+        // Slide the window to the right, focus on the added character and the
+        // removed character and update "count". Record the largest "count".
+        for (i in k until s.length) {
+            count += if (vowels.contains(s[i])) 1 else 0
+            count -= if (vowels.contains(s[i - k])) 1 else 0
+            answer = Math.max(answer, count)
+        }
+
+        return answer
+    }
+}
