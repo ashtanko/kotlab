5d7138953384691ec86f52bd17450d495c5ed22f
Oleksii Shtanko <shtankopro@gmail.com>
2023-06-15 00:03:27 +0200

    Solution of Minimum Absolute Difference in BST problem

---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/MinimumAbsoluteDifferenceBST.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import kotlin.math.min
+
+/**
+ * 530. Minimum Absolute Difference in BST
+ * @link https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+ */
+interface MinimumAbsoluteDifferenceBST {
+    fun getMinimumDifference(root: TreeNode?): Int
+}
+
+/**
+ * Approach 1: Depth First Search
+ */
+class MinimumAbsoluteDifferenceBSTDFS : MinimumAbsoluteDifferenceBST {
+
+    // List to store the node values.
+    private val nodeValues: MutableList<Int> = ArrayList()
+
+    override fun getMinimumDifference(root: TreeNode?): Int {
+        dfs(root)
+
+        nodeValues.sort()
+        var minDifference = Int.MAX_VALUE
+        // Find the diff between every two consecutive values in the list.
+        // Find the diff between every two consecutive values in the list.
+        for (i in 1 until nodeValues.size) {
+            minDifference = min(minDifference, nodeValues[i] - nodeValues[i - 1])
+        }
+
+        return minDifference
+    }
+
+    private fun dfs(node: TreeNode?) {
+        if (node == null) {
+            return
+        }
+        nodeValues.add(node.value)
+        dfs(node.left)
+        dfs(node.right)
+    }
+}
+
+/**
+ * Approach 2: In-order Traversal Using List
+ */
+class MinimumAbsoluteDifferenceBSTInOrder : MinimumAbsoluteDifferenceBST {
+    // List to store the tree nodes in the inorder traversal.
+    private val inorderNodes: MutableList<Int> = ArrayList()
+
+    override fun getMinimumDifference(root: TreeNode?): Int {
+        inorderTraversal(root)
+
+        var minDifference = Int.MAX_VALUE
+        // Find the diff between every two consecutive values in the list.
+        for (i in 1 until inorderNodes.size) {
+            minDifference = min(minDifference, inorderNodes[i] - inorderNodes[i - 1])
+        }
+
+        return minDifference
+    }
+
+    private fun inorderTraversal(node: TreeNode?) {
+        if (node == null) {
+            return
+        }
+        inorderTraversal(node.left)
+        // Store the nodes in the list.
+        inorderNodes.add(node.value)
+        inorderTraversal(node.right)
+    }
+}
+
+/**
+ * Approach 3: In-order Traversal Without List
+ */
+class MinimumAbsoluteDifferenceBSTInOrderOpt : MinimumAbsoluteDifferenceBST {
+    private var minDifference = Int.MAX_VALUE
+
+    // Initially, it will be null.
+    private var prevNode: TreeNode? = null
+
+    override fun getMinimumDifference(root: TreeNode?): Int {
+        inorderTraversal(root)
+        return minDifference
+    }
+
+    private fun inorderTraversal(node: TreeNode?) {
+        if (node == null) {
+            return
+        }
+        inorderTraversal(node.left)
+        // Find the difference with the previous value if it is there.
+        if (prevNode != null) {
+            minDifference = min(minDifference, node.value - prevNode!!.value)
+        }
+        prevNode = node
+        inorderTraversal(node.right)
+    }
+}
---
+++ src/test/kotlin/dev/shtanko/algorithms/leetcode/MinimumAbsoluteDifferenceBSTTest.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.stream.Stream
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+abstract class MinimumAbsoluteDifferenceBSTTest<out T : MinimumAbsoluteDifferenceBST>(private val strategy: T) {
+    private class InputArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+            Arguments.of(
+                TreeNode(4).apply {
+                    left = TreeNode(2).apply {
+                        left = TreeNode(1)
+                        right = TreeNode(3)
+                    }
+                    right = TreeNode(6)
+                },
+                1,
+            ),
+            Arguments.of(
+                TreeNode(1).apply {
+                    left = TreeNode(0)
+                    right = TreeNode(48).apply {
+                        left = TreeNode(12)
+                        right = TreeNode(49)
+                    }
+                },
+                1,
+            ),
+        )
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InputArgumentsProvider::class)
+    fun `get minimum difference test`(root: TreeNode, expected: Int) {
+        val actual = strategy.getMinimumDifference(root)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
+
+class MinimumAbsoluteDifferenceBSTDFSTest : MinimumAbsoluteDifferenceBSTTest<MinimumAbsoluteDifferenceBST>(
+    MinimumAbsoluteDifferenceBSTDFS(),
+)
+
+class MinimumAbsoluteDifferenceBSTInOrderTest : MinimumAbsoluteDifferenceBSTTest<MinimumAbsoluteDifferenceBST>(
+    MinimumAbsoluteDifferenceBSTInOrder(),
+)
+
+class MinimumAbsoluteDifferenceBSTInOrderOptTest : MinimumAbsoluteDifferenceBSTTest<MinimumAbsoluteDifferenceBST>(
+    MinimumAbsoluteDifferenceBSTInOrderOpt(),
+)
