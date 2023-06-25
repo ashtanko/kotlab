12f482d82539eb72e50d769c27f44bb1436ca446
Oleksii Shtanko <shtankopro@gmail.com>
2023-06-25 17:49:34 +0200

    Solution of Count All Possible Routes problem

--- README.md
+++ README.md
@@ -10,29 +10,29 @@
 
 ## Metrics
 
-* 9,093 number of properties
+* 9,099 number of properties
 
-* 6,275 number of functions
+* 6,281 number of functions
 
-* 5,428 number of classes
+* 5,435 number of classes
 
 * 135 number of packages
 
-* 2,221 number of kt files
+* 2,223 number of kt files
 
 ## Complexity Report
 
-* 146,879 lines of code (loc)
+* 147,033 lines of code (loc)
 
-* 89,674 source lines of code (sloc)
+* 89,773 source lines of code (sloc)
 
-* 63,795 logical lines of code (lloc)
+* 63,865 logical lines of code (lloc)
 
-* 40,946 comment lines of code (cloc)
+* 40,986 comment lines of code (cloc)
 
-* 15,448 cyclomatic complexity (mcc)
+* 15,466 cyclomatic complexity (mcc)
 
-* 13,483 cognitive complexity
+* 13,505 cognitive complexity
 
 * 0 number of total code smells
 
@@ -44,4 +44,4 @@
 
 ## Findings (0)
 
-generated with [detekt version 1.22.0](https://detekt.dev/) on 2023-06-24 12:44:31 UTC
+generated with [detekt version 1.22.0](https://detekt.dev/) on 2023-06-25 15:46:40 UTC
---
+++ src/main/kotlin/dev/shtanko/algorithms/leetcode/CountAllPossibleRoutes.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 Oleksii Shtanko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.shtanko.algorithms.leetcode
+
+import java.util.Arrays
+import kotlin.math.abs
+
+/**
+ * 1575. Count All Possible Routes
+ * @link https://leetcode.com/problems/count-all-possible-routes/
+ */
+interface CountAllPossibleRoutes {
+    fun countRoutes(locations: IntArray, start: Int, finish: Int, fuel: Int): Int
+}
+
+/**
+ * Approach 1: Recursive Dynamic Programming
+ */
+class CountAllPossibleRoutesRec : CountAllPossibleRoutes {
+    override fun countRoutes(locations: IntArray, start: Int, finish: Int, fuel: Int): Int {
+        val n: Int = locations.size
+        val memo = Array(n) { IntArray(fuel + 1) { -1 } }
+        return solve(locations, start, finish, fuel, memo)
+    }
+
+    private fun solve(locations: IntArray, currCity: Int, finish: Int, remainingFuel: Int, memo: Array<IntArray>): Int {
+        if (remainingFuel < 0) {
+            return 0
+        }
+        if (memo[currCity][remainingFuel] != -1) {
+            return memo[currCity][remainingFuel]
+        }
+        var ans = if (currCity == finish) 1 else 0
+        for (nextCity in locations.indices) {
+            if (nextCity != currCity) {
+                ans = ans.plus(
+                    solve(
+                        locations,
+                        nextCity,
+                        finish,
+                        remainingFuel - abs(locations[currCity] - locations[nextCity]),
+                        memo,
+                    ),
+                ) % MOD
+            }
+        }
+        return ans.also { memo[currCity][remainingFuel] = it }
+    }
+}
+
+/**
+ * Approach 2: Iterative Dynamic Programming
+ */
+class CountAllPossibleRoutesIter : CountAllPossibleRoutes {
+    override fun countRoutes(locations: IntArray, start: Int, finish: Int, fuel: Int): Int {
+        val n: Int = locations.size
+        val dp = Array(n) { IntArray(fuel + 1) }
+        Arrays.fill(dp[finish], 1)
+
+        for (j in 0..fuel) {
+            for (i in 0 until n) {
+                for (k in 0 until n) {
+                    if (k == i) {
+                        continue
+                    }
+                    if (abs(locations[i] - locations[k]) <= j) {
+                        dp[i][j] = (dp[i][j] + dp[k][j - abs(locations[i] - locations[k])]) % MOD
+                    }
+                }
+            }
+        }
+
+        return dp[start][fuel]
+    }
+}
